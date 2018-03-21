efaadeb447381cb215811a35b658a9bf5e8bae9d
cal02 <abc@def.com>
2018-03-21 21:30:58 +0900

    C#_MultiRenameApp_v0.1(v0.x終了)_ドラッグエンター処理追加

--- C#/Create/MultiRenameApp/WFA/Form1.Designer.cs
+++ C#/Create/MultiRenameApp/WFA/Form1.Designer.cs
@@ -1,35 +1,35 @@
 ﻿namespace WFA
 {
-    partial class Form1
+  partial class Form1
+  {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+    protected override void Dispose(bool disposing)
     {
-        /// <summary>
-        /// 必要なデザイナー変数です。
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
 
-        /// <summary>
-        /// 使用中のリソースをすべてクリーンアップします。
-        /// </summary>
-        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+    #region Windows フォーム デザイナーで生成されたコード
 
-        #region Windows フォーム デザイナーで生成されたコード
-
-        /// <summary>
-        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
-        /// コード エディターで変更しないでください。
-        /// </summary>
-        private void InitializeComponent()
-        {
-      Sgry.Azuki.FontInfo fontInfo3 = new Sgry.Azuki.FontInfo();
-      Sgry.Azuki.FontInfo fontInfo4 = new Sgry.Azuki.FontInfo();
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent()
+    {
+      Sgry.Azuki.FontInfo fontInfo1 = new Sgry.Azuki.FontInfo();
+      Sgry.Azuki.FontInfo fontInfo2 = new Sgry.Azuki.FontInfo();
       this.btCommit = new System.Windows.Forms.Button();
       this.azkTargetFileName = new Sgry.Azuki.WinForms.AzukiControl();
       this.azkChangedFileName = new Sgry.Azuki.WinForms.AzukiControl();
@@ -59,23 +59,24 @@
       // 
       // azkTargetFileName
       // 
-      this.azkTargetFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+      this.azkTargetFileName.AllowDrop = true;
+      this.azkTargetFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
       this.azkTargetFileName.BackColor = System.Drawing.Color.LightGray;
       this.azkTargetFileName.Cursor = System.Windows.Forms.Cursors.IBeam;
-      this.azkTargetFileName.DrawingOption = ((Sgry.Azuki.DrawingOption)(((((((Sgry.Azuki.DrawingOption.DrawsFullWidthSpace | Sgry.Azuki.DrawingOption.DrawsTab) 
-            | Sgry.Azuki.DrawingOption.DrawsEol) 
-            | Sgry.Azuki.DrawingOption.HighlightCurrentLine) 
-            | Sgry.Azuki.DrawingOption.ShowsLineNumber) 
-            | Sgry.Azuki.DrawingOption.ShowsDirtBar) 
+      this.azkTargetFileName.DrawingOption = ((Sgry.Azuki.DrawingOption)(((((((Sgry.Azuki.DrawingOption.DrawsFullWidthSpace | Sgry.Azuki.DrawingOption.DrawsTab)
+            | Sgry.Azuki.DrawingOption.DrawsEol)
+            | Sgry.Azuki.DrawingOption.HighlightCurrentLine)
+            | Sgry.Azuki.DrawingOption.ShowsLineNumber)
+            | Sgry.Azuki.DrawingOption.ShowsDirtBar)
             | Sgry.Azuki.DrawingOption.HighlightsMatchedBracket)));
       this.azkTargetFileName.FirstVisibleLine = 0;
-      this.azkTargetFileName.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-      fontInfo3.Name = "MS UI Gothic";
-      fontInfo3.Size = 14;
-      fontInfo3.Style = System.Drawing.FontStyle.Regular;
-      this.azkTargetFileName.FontInfo = fontInfo3;
+      this.azkTargetFileName.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+      fontInfo1.Name = "MS UI Gothic";
+      fontInfo1.Size = 14;
+      fontInfo1.Style = System.Drawing.FontStyle.Regular;
+      this.azkTargetFileName.FontInfo = fontInfo1;
       this.azkTargetFileName.ForeColor = System.Drawing.Color.Black;
       this.azkTargetFileName.IsReadOnly = true;
       this.azkTargetFileName.Location = new System.Drawing.Point(3, 3);
@@ -84,26 +85,29 @@
       this.azkTargetFileName.Size = new System.Drawing.Size(247, 428);
       this.azkTargetFileName.TabIndex = 4;
       this.azkTargetFileName.ViewWidth = 4142;
+      this.azkTargetFileName.DragDrop += new System.Windows.Forms.DragEventHandler(this.CommonDropDropEv);
+      this.azkTargetFileName.DragEnter += new System.Windows.Forms.DragEventHandler(this.CommonDropEnterEv);
       // 
       // azkChangedFileName
       // 
-      this.azkChangedFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+      this.azkChangedFileName.AllowDrop = true;
+      this.azkChangedFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
       this.azkChangedFileName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(240)))));
       this.azkChangedFileName.Cursor = System.Windows.Forms.Cursors.IBeam;
-      this.azkChangedFileName.DrawingOption = ((Sgry.Azuki.DrawingOption)(((((((Sgry.Azuki.DrawingOption.DrawsFullWidthSpace | Sgry.Azuki.DrawingOption.DrawsTab) 
-            | Sgry.Azuki.DrawingOption.DrawsEol) 
-            | Sgry.Azuki.DrawingOption.HighlightCurrentLine) 
-            | Sgry.Azuki.DrawingOption.ShowsLineNumber) 
-            | Sgry.Azuki.DrawingOption.ShowsDirtBar) 
+      this.azkChangedFileName.DrawingOption = ((Sgry.Azuki.DrawingOption)(((((((Sgry.Azuki.DrawingOption.DrawsFullWidthSpace | Sgry.Azuki.DrawingOption.DrawsTab)
+            | Sgry.Azuki.DrawingOption.DrawsEol)
+            | Sgry.Azuki.DrawingOption.HighlightCurrentLine)
+            | Sgry.Azuki.DrawingOption.ShowsLineNumber)
+            | Sgry.Azuki.DrawingOption.ShowsDirtBar)
             | Sgry.Azuki.DrawingOption.HighlightsMatchedBracket)));
       this.azkChangedFileName.FirstVisibleLine = 0;
-      this.azkChangedFileName.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-      fontInfo4.Name = "MS UI Gothic";
-      fontInfo4.Size = 15;
-      fontInfo4.Style = System.Drawing.FontStyle.Regular;
-      this.azkChangedFileName.FontInfo = fontInfo4;
+      this.azkChangedFileName.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+      fontInfo2.Name = "MS UI Gothic";
+      fontInfo2.Size = 15;
+      fontInfo2.Style = System.Drawing.FontStyle.Regular;
+      this.azkChangedFileName.FontInfo = fontInfo2;
       this.azkChangedFileName.ForeColor = System.Drawing.Color.Black;
       this.azkChangedFileName.Location = new System.Drawing.Point(3, 3);
       this.azkChangedFileName.Name = "azkChangedFileName";
@@ -111,11 +115,14 @@
       this.azkChangedFileName.Size = new System.Drawing.Size(355, 428);
       this.azkChangedFileName.TabIndex = 5;
       this.azkChangedFileName.ViewWidth = 4147;
+      this.azkChangedFileName.DragDrop += new System.Windows.Forms.DragEventHandler(this.CommonDropDropEv);
+      this.azkChangedFileName.DragEnter += new System.Windows.Forms.DragEventHandler(this.CommonDropEnterEv);
       // 
       // splitContainer1
       // 
-      this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+      this.splitContainer1.AllowDrop = true;
+      this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
       this.splitContainer1.Location = new System.Drawing.Point(12, 90);
       this.splitContainer1.Name = "splitContainer1";
@@ -130,6 +137,8 @@
       this.splitContainer1.Size = new System.Drawing.Size(615, 434);
       this.splitContainer1.SplitterDistance = 250;
       this.splitContainer1.TabIndex = 6;
+      this.splitContainer1.DragDrop += new System.Windows.Forms.DragEventHandler(this.CommonDropDropEv);
+      this.splitContainer1.DragEnter += new System.Windows.Forms.DragEventHandler(this.CommonDropEnterEv);
       // 
       // btOpenExplorer
       // 
@@ -143,7 +152,8 @@
       // 
       // lbTargetPath
       // 
-      this.lbTargetPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+      this.lbTargetPath.AllowDrop = true;
+      this.lbTargetPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
       this.lbTargetPath.BackColor = System.Drawing.SystemColors.Control;
       this.lbTargetPath.BorderStyle = System.Windows.Forms.BorderStyle.None;
@@ -154,9 +164,12 @@
       this.lbTargetPath.Size = new System.Drawing.Size(610, 23);
       this.lbTargetPath.TabIndex = 12;
       this.lbTargetPath.Text = "-";
+      this.lbTargetPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.CommonDropDropEv);
+      this.lbTargetPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.CommonDropEnterEv);
       // 
       // label12
       // 
+      this.label12.AllowDrop = true;
       this.label12.AutoSize = true;
       this.label12.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
       this.label12.Location = new System.Drawing.Point(8, 35);
@@ -164,6 +177,8 @@
       this.label12.Size = new System.Drawing.Size(100, 23);
       this.label12.TabIndex = 7;
       this.label12.Text = "対象フォルダ";
+      this.label12.DragDrop += new System.Windows.Forms.DragEventHandler(this.CommonDropDropEv);
+      this.label12.DragEnter += new System.Windows.Forms.DragEventHandler(this.CommonDropEnterEv);
       // 
       // btPathConfirm
       // 
@@ -189,15 +204,19 @@
       // 
       // tbTargetPathText
       // 
-      this.tbTargetPathText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+      this.tbTargetPathText.AllowDrop = true;
+      this.tbTargetPathText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
       this.tbTargetPathText.Location = new System.Drawing.Point(12, 12);
       this.tbTargetPathText.Name = "tbTargetPathText";
       this.tbTargetPathText.Size = new System.Drawing.Size(512, 19);
       this.tbTargetPathText.TabIndex = 13;
+      this.tbTargetPathText.DragDrop += new System.Windows.Forms.DragEventHandler(this.CommonDropDropEv);
+      this.tbTargetPathText.DragEnter += new System.Windows.Forms.DragEventHandler(this.CommonDropEnterEv);
       // 
       // Form1
       // 
+      this.AllowDrop = true;
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(639, 565);
@@ -211,6 +230,8 @@
       this.Controls.Add(this.btCommit);
       this.Name = "Form1";
       this.Text = "Form1";
+      this.DragDrop += new System.Windows.Forms.DragEventHandler(this.CommonDropDropEv);
+      this.DragEnter += new System.Windows.Forms.DragEventHandler(this.CommonDropEnterEv);
       this.splitContainer1.Panel1.ResumeLayout(false);
       this.splitContainer1.Panel2.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -218,20 +239,20 @@
       this.ResumeLayout(false);
       this.PerformLayout();
 
-        }
-
-        #endregion
-
-        private System.Windows.Forms.Button btCommit;
-        private Sgry.Azuki.WinForms.AzukiControl azkTargetFileName;
-        private Sgry.Azuki.WinForms.AzukiControl azkChangedFileName;
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.Button btOpenExplorer;
-        private System.Windows.Forms.TextBox lbTargetPath;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Button btPathConfirm;
-        private System.Windows.Forms.Button btReference;
-        private System.Windows.Forms.TextBox tbTargetPathText;
     }
+
+    #endregion
+
+    private System.Windows.Forms.Button btCommit;
+    private Sgry.Azuki.WinForms.AzukiControl azkTargetFileName;
+    private Sgry.Azuki.WinForms.AzukiControl azkChangedFileName;
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private System.Windows.Forms.Button btOpenExplorer;
+    private System.Windows.Forms.TextBox lbTargetPath;
+    private System.Windows.Forms.Label label12;
+    private System.Windows.Forms.Button btPathConfirm;
+    private System.Windows.Forms.Button btReference;
+    private System.Windows.Forms.TextBox tbTargetPathText;
+  }
 }
 
--- C#/Create/MultiRenameApp/WFA/Form1.cs
+++ C#/Create/MultiRenameApp/WFA/Form1.cs
@@ -35,12 +35,10 @@ namespace WFA
 
     #region プロパティ
 
-    //ターゲットフォルダ
-    string targetFolderPath { get; set; }
-
     //Azukiエディタのドキュメント型
     Sgry.Azuki.Document targetDoc { get; set; }
     Sgry.Azuki.Document changedDoc { get; set; }
+
     #endregion
 
 
@@ -55,6 +53,13 @@ namespace WFA
     #region 位置確定ボタン押下イベント
     private void btPathConfirm_Click(object sender, EventArgs e)
     {
+      //ねずみ返し_対象フォルダがない場合
+      if (Directory.Exists(lbTargetPath.Text))
+      {
+        MessageBox.Show("対象フォルダが存在しません");
+        return;
+      }
+
       //フォルダ内容取得メソッド使用
       GetDirectoryContents();
     }
@@ -69,8 +74,14 @@ namespace WFA
         MessageBox.Show("フォルダを指定してください");
         return;
       }
+      //ねずみ返し_対象フォルダがない場合
+      if (Directory.Exists(lbTargetPath.Text))
+      {
+        MessageBox.Show("対象フォルダが存在しません");
+        return;
+      }
 
-      //binパスラベルのフォルダをエクスプローラーで開く
+      //対象フォルダラベルのフォルダをエクスプローラーで開く
       Process.Start(lbTargetPath.Text);
     }
     #endregion
@@ -78,6 +89,16 @@ namespace WFA
     #region コミットボタン押下イベント
     private void btCommit_Click(object sender, EventArgs e)
     {
+      //ねずみ返し_対象フォルダがない場合
+      if (Directory.Exists(lbTargetPath.Text))
+      {
+        MessageBox.Show("対象フォルダが存在しません");
+        return;
+      }
+
+      //フォルダパスを取得
+      string targetFolderPath = lbTargetPath.Text + @"\";
+
       //対象Azukiエディタのドキュメント型を取得
       Sgry.Azuki.Document targetDoc = azkTargetFileName.Document;
       //変更後Azukiエディタのドキュメント型を取得
@@ -92,7 +113,7 @@ namespace WFA
 
       //実処理
       for (int i = 0; i < targetDoc.LineCount; i++)
-			{
+      {
         //置換え対象取得
         string target = targetFolderPath + targetDoc.GetLineContent(i);
         string changed = targetFolderPath + changedDoc.GetLineContent(i);
@@ -106,7 +127,7 @@ namespace WFA
         {
           File.Move(target, changed);
         }
-			}
+      }
     }
     #endregion
 
@@ -121,8 +142,6 @@ namespace WFA
       fbd.Description = fbd_Desc;
       //ルートフォルダを指定する
       fbd.RootFolder = Environment.SpecialFolder.Desktop;
-      ////最初に選択するフォルダを指定する
-      //fbd.SelectedPath = ConfigurationManager.AppSettings["ReferenceFolderDefaultPath"];
 
       //OKが押下された場合
       if (fbd.ShowDialog(this) == DialogResult.OK)
@@ -141,7 +160,7 @@ namespace WFA
     private void GetDirectoryContents()
     {
       //フォルダパスを取得
-      targetFolderPath = tbTargetPathText.Text + @"\";
+      string targetFolderPath = lbTargetPath.Text;
 
       //対象ディレクトリ内の一覧取得
       string[] targetFolderList = Directory.GetDirectories(targetFolderPath, "*", SearchOption.TopDirectoryOnly);
@@ -175,6 +194,55 @@ namespace WFA
 
       //対象フォルダ表示
       lbTargetPath.Text = targetFolderPath;
+    }
+    #endregion
+
+
+    #region 共通ドラッグエンターイベント
+    private void CommonDropEnterEv(object sender, DragEventArgs e)
+    {
+      //AllowDropプロパティの許可が必要
+
+      //ねずみ返し_マウスがファイルを持っていない場合、イベント・ハンドラを抜ける
+      if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+      {
+        return;
+      }
+
+      //ドラッグ中のファイルの取得
+      string[] drags = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+      //ねずみ返し_ファイルのみを条件とする
+      foreach (string d in drags)
+      {
+        //フォルダでもファイルでもない場合
+        if (!System.IO.Directory.Exists(d) && !System.IO.File.Exists(d))
+        {
+          return;
+        }
+      }
+
+      //マウスの表示を「+」に変更する
+      e.Effect = DragDropEffects.Copy;
+    }
+    #endregion
+
+    #region 共通ドラッグドロップイベント
+    private void CommonDropDropEv(object sender, DragEventArgs e)
+    {
+      //ドラッグ&ドロップされたファイルの一つ目を取得
+      string dropItem = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+
+      //フォルダかファイルか判断
+      if (Directory.Exists(dropItem))
+      {
+        tbTargetPathText.Text = dropItem;
+      }
+      else
+      {
+        //対象フォルダ入力欄に表示
+        tbTargetPathText.Text = System.IO.Path.GetDirectoryName(dropItem);
+      }
     }
     #endregion
 
