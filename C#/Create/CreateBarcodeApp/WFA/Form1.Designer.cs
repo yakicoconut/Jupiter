0f6e322b169b4e86683c10cdd191cad97d3d1797
ys <abc@def.com>
2018-03-26 16:55:20 +0900

    C#_CreateBarcodeApp_追加

---
+++ C#/Create/CreateBarcodeApp/WFA/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.button1 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button2 = new System.Windows.Forms.Button();
+      this.cbCodeType = new System.Windows.Forms.ComboBox();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.button1.Location = new System.Drawing.Point(12, 164);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBox1.Location = new System.Drawing.Point(12, 12);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.textBox1.Size = new System.Drawing.Size(324, 146);
+      this.textBox1.TabIndex = 2;
+      // 
+      // button2
+      // 
+      this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.button2.Location = new System.Drawing.Point(93, 164);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 3;
+      this.button2.Text = "button2";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // cbCodeType
+      // 
+      this.cbCodeType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.cbCodeType.FormattingEnabled = true;
+      this.cbCodeType.Items.AddRange(new object[] {
+            "AZTEC",
+            "CODABAR",
+            "CODE_39",
+            "CODE_93",
+            "CODE_128",
+            "DATA_MATRIX",
+            "EAN_8",
+            "EAN_13",
+            "ITF",
+            "MAXICODE",
+            "PDF_417",
+            "QR_CODE",
+            "RSS_14",
+            "RSS_EXPANDED",
+            "UPC_A",
+            "UPC_E",
+            "All_1D",
+            "UPC_EAN_EXTENSION",
+            "MSI",
+            "PLESSEY",
+            "IMB"});
+      this.cbCodeType.Location = new System.Drawing.Point(215, 167);
+      this.cbCodeType.Name = "cbCodeType";
+      this.cbCodeType.Size = new System.Drawing.Size(121, 20);
+      this.cbCodeType.TabIndex = 4;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(348, 199);
+      this.Controls.Add(this.cbCodeType);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cbCodeType;
+    }
+}
+
---
+++ C#/Create/CreateBarcodeApp/WFA/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Configuration;
+using ZXing;
+using System.Drawing.Imaging;
+
+namespace WFA
+{
+  /*
+   * バーコード関連パッケージZXing.Net(0.16.2)使用
+   * 以下からダウンロード
+   *   https://github.com/micjahn/ZXing.Net
+   */
+  public partial class Form1 : Form
+  {
+    #region コンストラクタ
+    public Form1()
+    {
+      InitializeComponent();
+
+      #region 【論理雛形】
+      WFAComLogic WFACL = new WFAComLogic();
+      // アプリ名設定
+      Text = WFACL.GetAppName();
+      #endregion
+
+      // コンフィグ取得メソッド使用
+      GetConfig();
+    }
+    #endregion
+
+    #region コンフィグ取得メソッド
+    public void GetConfig()
+    {
+      string hoge01 = ConfigurationManager.AppSettings["Hoge01"];
+    }
+    #endregion
+
+
+    #region フォームロードイベント
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      // 初期表示時に、先頭の項目を選択
+      this.cbCodeType.SelectedIndex = 0;
+    }
+    #endregion
+
+    #region ボタン1押下イベント
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // バーコード作成メソッド使用
+      CreateBarcode();
+    }
+    #endregion
+
+    #region ボタン2押下イベント
+    private void button2_Click(object sender, EventArgs e)
+    {
+      /* テキストを一行ずつ取得しタブ区切りに配列に入れる(バーコード値\tディスクリプション) */
+
+      //// 分割文字列(改行)の指定
+      //string[] separatorNewLine = new string[] { "\r\n" };
+      //// 文字列を分割
+      //string[] lineList = textBox1.Text.Split(separatorNewLine, StringSplitOptions.None);
+
+      //string[,] imputList;
+      //foreach (string x in lineList)
+      //{
+      //  // 分割文字列の指定
+      //  string[] separatorTab = new string[] { "\t" };
+      //  // 文字列を分割
+      //  string[] tempList = textBox1.Text.Split(separatorTab, StringSplitOptions.None);
+
+        
+      //  imputList = new string[,] { {"", "" } };
+      //}
+    }
+    #endregion
+
+
+    #region バーコード作成メソッド
+    public void CreateBarcode()
+    {
+      var bacodeWriter = new BarcodeWriter();
+
+      // コードタイプ変換メソッド使用
+      bacodeWriter.Format = SwitchCodeType();
+
+      // サイズ
+      bacodeWriter.Options.Height = 40;
+      bacodeWriter.Options.Width = 200;
+
+      // バーコード左右の余白
+      bacodeWriter.Options.Margin = 30;
+
+      // バーコードのみ表示するか
+      // falseにするとテキストも表示する
+      bacodeWriter.Options.PureBarcode = false;
+
+
+      // バーコードBitmapを作成
+      using (var bitmap = bacodeWriter.Write("0000000000000000"))
+      {
+        // 画像として保存
+        var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Barcode.png");
+        bitmap.Save(filePath, ImageFormat.Png);
+      }
+    }
+    #endregion
+
+    #region コードタイプ変換メソッド
+    public BarcodeFormat SwitchCodeType()
+    {
+      // 初期値設定
+      BarcodeFormat codeType = BarcodeFormat.AZTEC;
+
+      switch (cbCodeType.Text)
+      {
+        case "AZTEC":
+          codeType = BarcodeFormat.AZTEC;
+          break;
+        case "CODABAR":
+          codeType = BarcodeFormat.CODABAR;
+          break;
+        case "CODE_39":
+          codeType = BarcodeFormat.CODE_39;
+          break;
+        case "CODE_93":
+          codeType = BarcodeFormat.CODE_93;
+          break;
+        case "CODE_128":
+          codeType = BarcodeFormat.CODE_128;
+          break;
+        case "DATA_MATRIX":
+          codeType = BarcodeFormat.DATA_MATRIX;
+          break;
+        case "EAN_8":
+          codeType = BarcodeFormat.EAN_8;
+          break;
+        case "EAN_13":
+          codeType = BarcodeFormat.EAN_13;
+          break;
+        case "ITF":
+          codeType = BarcodeFormat.ITF;
+          break;
+        case "MAXICODE":
+          codeType = BarcodeFormat.MAXICODE;
+          break;
+        case "PDF_417":
+          codeType = BarcodeFormat.PDF_417;
+          break;
+        case "QR_CODE":
+          codeType = BarcodeFormat.QR_CODE;
+          break;
+        case "RSS_14":
+          codeType = BarcodeFormat.RSS_14;
+          break;
+        case "RSS_EXPANDED":
+          codeType = BarcodeFormat.RSS_EXPANDED;
+          break;
+        case "UPC_A":
+          codeType = BarcodeFormat.UPC_A;
+          break;
+        case "UPC_E":
+          codeType = BarcodeFormat.UPC_E;
+          break;
+        case "All_1D":
+          codeType = BarcodeFormat.All_1D;
+          break;
+        case "UPC_EAN_EXTENSION":
+          codeType = BarcodeFormat.UPC_EAN_EXTENSION;
+          break;
+        case "MSI":
+          codeType = BarcodeFormat.MSI;
+          break;
+        case "PLESSEY":
+          codeType = BarcodeFormat.PLESSEY;
+          break;
+        case "IMB":
+          codeType = BarcodeFormat.IMB;
+          break;
+        default:
+          break;
+      }
+
+      return codeType;
+    }
+    #endregion
+
+
+    #region 雛形メソッド
+    public void template()
+    {
+
+    }
+    #endregion
+  }
+}
