25f014d125c113900f66ef0d0f737c952dd1124b
ys <abc@def.com>
2018-03-27 11:16:51 +0900

    C#_CorkboardApp2_Txt_v0.06(v0.xx終了)_コミット漏れ

---
+++ C#/Create/CorkboardApp2/CorkboardApp2_Txt/WFA/Form2.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WFA
+{
+  partial class Form2
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.listView1 = new System.Windows.Forms.ListView();
+      this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+      this.不透明度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.上げToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.下げToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.閉じるToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.contextMenuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // listView1
+      // 
+      this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.listView1.CheckBoxes = true;
+      this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+      this.listView1.Location = new System.Drawing.Point(12, 12);
+      this.listView1.Name = "listView1";
+      this.listView1.Size = new System.Drawing.Size(149, 285);
+      this.listView1.TabIndex = 0;
+      this.listView1.UseCompatibleStateImageBehavior = false;
+      this.listView1.View = System.Windows.Forms.View.List;
+      this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+      this.listView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDown);
+      // 
+      // contextMenuStrip1
+      // 
+      this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.不透明度ToolStripMenuItem,
+            this.閉じるToolStripMenuItem});
+      this.contextMenuStrip1.Name = "contextMenuStrip1";
+      this.contextMenuStrip1.Size = new System.Drawing.Size(125, 48);
+      // 
+      // 不透明度ToolStripMenuItem
+      // 
+      this.不透明度ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.上げToolStripMenuItem,
+            this.下げToolStripMenuItem});
+      this.不透明度ToolStripMenuItem.Name = "不透明度ToolStripMenuItem";
+      this.不透明度ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+      this.不透明度ToolStripMenuItem.Text = "不透明度";
+      this.不透明度ToolStripMenuItem.Click += new System.EventHandler(this.不透明度ToolStripMenuItem_Click);
+      // 
+      // 上げToolStripMenuItem
+      // 
+      this.上げToolStripMenuItem.Name = "上げToolStripMenuItem";
+      this.上げToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+      this.上げToolStripMenuItem.Text = "上げ";
+      this.上げToolStripMenuItem.Click += new System.EventHandler(this.上げToolStripMenuItem_Click);
+      // 
+      // 下げToolStripMenuItem
+      // 
+      this.下げToolStripMenuItem.Name = "下げToolStripMenuItem";
+      this.下げToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+      this.下げToolStripMenuItem.Text = "下げ";
+      this.下げToolStripMenuItem.Click += new System.EventHandler(this.下げToolStripMenuItem_Click);
+      // 
+      // 閉じるToolStripMenuItem
+      // 
+      this.閉じるToolStripMenuItem.Name = "閉じるToolStripMenuItem";
+      this.閉じるToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+      this.閉じるToolStripMenuItem.Text = "閉じる";
+      this.閉じるToolStripMenuItem.Click += new System.EventHandler(this.閉じるToolStripMenuItem_Click);
+      // 
+      // Form2
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+      this.ClientSize = new System.Drawing.Size(173, 309);
+      this.Controls.Add(this.listView1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "Form2";
+      this.Opacity = 0.8D;
+      this.Text = "Form2";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+      this.Load += new System.EventHandler(this.Form2_Load);
+      this.contextMenuStrip1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListView listView1;
+    private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem 不透明度ToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem 上げToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem 下げToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem 閉じるToolStripMenuItem;
+  }
+}
---
+++ C#/Create/CorkboardApp2/CorkboardApp2_Txt/WFA/Form2.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WFA
+{
+  public partial class Form2 : Form
+  {
+    #region コンストラクタ
+    public Form2()
+    {
+      InitializeComponent();
+
+      //リストビューをプロパティに格納
+      lvCtrl = listView1;
+    }
+    #endregion
+
+
+    #region プロパティ
+
+    public Form1 form1 { get; set; }
+    public ListViewItem lvItem { get; set; }
+    public ListView lvCtrl { get; set; }
+
+    #endregion
+
+
+    #region フォームロードイベント
+    private void Form2_Load(object sender, EventArgs e)
+    {
+
+    }
+    #endregion
+
+    #region リストビューダブルクリックイベント
+    private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+    {
+      try
+      {
+        //選択された基底パネル名を取得
+        string targetCtrlName = listView1.SelectedItems[0].SubItems[1].Text;
+        ////【テスト】選択された基底パネル名を表示
+        //MessageBox.Show(targetCtrlName);
+
+        //選択した基底パネルを表示し最前面化
+        form1.Controls[targetCtrlName].Visible = true;
+        form1.Controls[targetCtrlName].BringToFront();
+      }
+      catch
+      {
+
+      }
+    }
+    #endregion
+
+    #region リストビューマウスダウンイベント
+    private void listView1_MouseDown(object sender, MouseEventArgs e)
+    {
+      //ねずみ返し_ワンクリックの場合
+      if (e.Clicks == 1)
+      {
+        return;
+      }
+
+      //選択アイテムのチェックを変更しない
+      if (listView1.SelectedItems[0].Checked)
+      {
+        listView1.SelectedItems[0].Checked = false;
+      }
+      else
+      {
+        listView1.SelectedItems[0].Checked = true;
+      }
+    }
+    #endregion
+
+
+    #region コンテキストイベント一覧
+
+    #region コンテキスト_不透明度押下イベント
+    private void 不透明度ToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      //デフォルトに戻す
+      this.Opacity = 0.8;
+    }
+    #endregion
+
+    #region コンテキスト_上げ押下イベント
+    private void 上げToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      //不透明度を上げる
+      this.Opacity += 0.2;
+    }
+    #endregion
+
+    #region コンテキスト_下げ押下イベント
+    private void 下げToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      //不透明度を下げる
+      this.Opacity -= 0.2;
+    }
+    #endregion
+
+    #region コンテキスト_閉じる押下イベント
+    private void 閉じるToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        //チェックしたアイテム
+        foreach (ListViewItem x in listView1.CheckedItems)
+        {
+          //対象コントロール情報取得
+          Control targetCtrl = form1.Controls[x.SubItems[1].Text];
+          string lumpingNum = targetCtrl.Name.Substring(targetCtrl.Name.Length - 4, 4);
+
+          //上書き確認メソッド使用
+          if (!form1.ConfirmationOverwrite(targetCtrl, lumpingNum))
+            //キャンセルの場合
+            continue;
+
+          //基底パネル削除メソッド使用
+          form1.CloseBasePanel(targetCtrl);
+        }
+      }
+      catch
+      {
+
+      }
+    }
+    #endregion
+
+    #endregion
+
+
+    #region フォームクロージングイベント
+    private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      //クローズキャンセル
+      if (e.CloseReason == CloseReason.UserClosing)
+        e.Cancel = true;
+    }
+    #endregion
+  }
+}
