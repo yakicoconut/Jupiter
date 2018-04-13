caf96d37eb6baed5e747e7ae6a899305ddefe8f9
ys <abc@def.com>
2018-04-13 21:51:47 +0900

    C#_LogicTemp_OptionForm_追加

---
+++ C#/LogicTemp/Form/OptionForm/WFA/FrmOption.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WFA
+{
+  partial class FrmOption
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+      this.不透明度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.上げToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.下げToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.contextMenuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // contextMenuStrip1
+      // 
+      this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.不透明度ToolStripMenuItem});
+      this.contextMenuStrip1.Name = "contextMenuStrip1";
+      this.contextMenuStrip1.Size = new System.Drawing.Size(125, 48);
+      // 
+      // 不透明度ToolStripMenuItem
+      // 
+      this.不透明度ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.上げToolStripMenuItem,
+            this.下げToolStripMenuItem});
+      this.不透明度ToolStripMenuItem.Name = "不透明度ToolStripMenuItem";
+      this.不透明度ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+      this.不透明度ToolStripMenuItem.Text = "不透明度";
+      this.不透明度ToolStripMenuItem.Click += new System.EventHandler(this.不透明度ToolStripMenuItem_Click);
+      // 
+      // 上げToolStripMenuItem
+      // 
+      this.上げToolStripMenuItem.Name = "上げToolStripMenuItem";
+      this.上げToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+      this.上げToolStripMenuItem.Text = "上げ";
+      this.上げToolStripMenuItem.Click += new System.EventHandler(this.上げToolStripMenuItem_Click);
+      // 
+      // 下げToolStripMenuItem
+      // 
+      this.下げToolStripMenuItem.Name = "下げToolStripMenuItem";
+      this.下げToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+      this.下げToolStripMenuItem.Text = "下げ";
+      this.下げToolStripMenuItem.Click += new System.EventHandler(this.下げToolStripMenuItem_Click);
+      // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.SystemColors.Window;
+      this.panel1.Location = new System.Drawing.Point(10, 10);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(150, 285);
+      this.panel1.TabIndex = 1;
+      // 
+      // FrmOption
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+      this.ClientSize = new System.Drawing.Size(170, 305);
+      this.ContextMenuStrip = this.contextMenuStrip1;
+      this.Controls.Add(this.panel1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "FrmOption";
+      this.Opacity = 0.8D;
+      this.Text = "Form2";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+      this.Load += new System.EventHandler(this.Form2_Load);
+      this.contextMenuStrip1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem 不透明度ToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem 上げToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem 下げToolStripMenuItem;
+    private System.Windows.Forms.Panel panel1;
+  }
+}
