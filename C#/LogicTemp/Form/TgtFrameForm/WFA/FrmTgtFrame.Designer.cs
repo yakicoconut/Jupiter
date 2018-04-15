5509e49b36823de54cf9eea87fde9c7cb5964d72
cal01 <abc@def.com>
2018-04-15 23:04:19 +0900

    C#_LogicTemp_TgtFrameForm_追加

---
+++ C#/LogicTemp/Form/TgtFrameForm/WFA/FrmTgtFrame.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WFA
+{
+  partial class FrmTgtFrame
+  {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows フォーム デザイナーで生成されたコード
+
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.rbLeftTop = new System.Windows.Forms.RadioButton();
+      this.rbRightBottom = new System.Windows.Forms.RadioButton();
+      this.lbHorizonDist = new System.Windows.Forms.Label();
+      this.lbVerticalDist = new System.Windows.Forms.Label();
+      this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+      this.toolStripMenuItemOpacity = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripMenuItemOpacityGain = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripMenuItemOpacityDec = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripMenuItemClose = new System.Windows.Forms.ToolStripMenuItem();
+      this.ToolStripMenuItemPointTest = new System.Windows.Forms.ToolStripMenuItem();
+      this.contextMenuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // rbLeftTop
+      // 
+      this.rbLeftTop.AutoSize = true;
+      this.rbLeftTop.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+      this.rbLeftTop.Location = new System.Drawing.Point(12, 12);
+      this.rbLeftTop.Name = "rbLeftTop";
+      this.rbLeftTop.Size = new System.Drawing.Size(14, 13);
+      this.rbLeftTop.TabIndex = 0;
+      this.rbLeftTop.TabStop = true;
+      this.rbLeftTop.UseVisualStyleBackColor = true;
+      // 
+      // rbRightBottom
+      // 
+      this.rbRightBottom.AutoSize = true;
+      this.rbRightBottom.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+      this.rbRightBottom.Location = new System.Drawing.Point(12, 31);
+      this.rbRightBottom.Name = "rbRightBottom";
+      this.rbRightBottom.Size = new System.Drawing.Size(14, 13);
+      this.rbRightBottom.TabIndex = 1;
+      this.rbRightBottom.TabStop = true;
+      this.rbRightBottom.UseVisualStyleBackColor = true;
+      // 
+      // lbHorizonDist
+      // 
+      this.lbHorizonDist.AutoSize = true;
+      this.lbHorizonDist.Location = new System.Drawing.Point(42, 55);
+      this.lbHorizonDist.Name = "lbHorizonDist";
+      this.lbHorizonDist.Size = new System.Drawing.Size(35, 12);
+      this.lbHorizonDist.TabIndex = 2;
+      this.lbHorizonDist.Text = "label1";
+      // 
+      // lbVerticalDist
+      // 
+      this.lbVerticalDist.AutoSize = true;
+      this.lbVerticalDist.Location = new System.Drawing.Point(12, 67);
+      this.lbVerticalDist.Name = "lbVerticalDist";
+      this.lbVerticalDist.Size = new System.Drawing.Size(35, 12);
+      this.lbVerticalDist.TabIndex = 3;
+      this.lbVerticalDist.Text = "label1";
+      // 
+      // contextMenuStrip1
+      // 
+      this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemOpacity,
+            this.ToolStripMenuItemPointTest,
+            this.toolStripMenuItemClose});
+      this.contextMenuStrip1.Name = "contextMenuStrip1";
+      this.contextMenuStrip1.Size = new System.Drawing.Size(153, 92);
+      // 
+      // toolStripMenuItemOpacity
+      // 
+      this.toolStripMenuItemOpacity.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemOpacityGain,
+            this.toolStripMenuItemOpacityDec});
+      this.toolStripMenuItemOpacity.Name = "toolStripMenuItemOpacity";
+      this.toolStripMenuItemOpacity.Size = new System.Drawing.Size(122, 22);
+      this.toolStripMenuItemOpacity.Text = "不透明度";
+      this.toolStripMenuItemOpacity.Click += new System.EventHandler(this.toolStripMenuItemOpacity_Click);
+      // 
+      // toolStripMenuItemOpacityGain
+      // 
+      this.toolStripMenuItemOpacityGain.Name = "toolStripMenuItemOpacityGain";
+      this.toolStripMenuItemOpacityGain.Size = new System.Drawing.Size(96, 22);
+      this.toolStripMenuItemOpacityGain.Text = "上げ";
+      this.toolStripMenuItemOpacityGain.Click += new System.EventHandler(this.toolStripMenuItemOpacityGain_Click);
+      // 
+      // toolStripMenuItemOpacityDec
+      // 
+      this.toolStripMenuItemOpacityDec.Name = "toolStripMenuItemOpacityDec";
+      this.toolStripMenuItemOpacityDec.Size = new System.Drawing.Size(96, 22);
+      this.toolStripMenuItemOpacityDec.Text = "下げ";
+      this.toolStripMenuItemOpacityDec.Click += new System.EventHandler(this.toolStripMenuItemOpacityDec_Click);
+      // 
+      // toolStripMenuItemClose
+      // 
+      this.toolStripMenuItemClose.Name = "toolStripMenuItemClose";
+      this.toolStripMenuItemClose.Size = new System.Drawing.Size(122, 22);
+      this.toolStripMenuItemClose.Text = "閉じる";
+      this.toolStripMenuItemClose.Click += new System.EventHandler(this.toolStripMenuItemClose_Click);
+      // 
+      // ポイントテストToolStripMenuItem
+      // 
+      this.ToolStripMenuItemPointTest.Name = "ポイントテストToolStripMenuItem";
+      this.ToolStripMenuItemPointTest.Size = new System.Drawing.Size(152, 22);
+      this.ToolStripMenuItemPointTest.Text = "ポイントテスト";
+      this.ToolStripMenuItemPointTest.Click += new System.EventHandler(this.ToolStripMenuItemPointTest_Click);
+      // 
+      // FrmTgtFrame
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.ContextMenuStrip = this.contextMenuStrip1;
+      this.Controls.Add(this.lbVerticalDist);
+      this.Controls.Add(this.lbHorizonDist);
+      this.Controls.Add(this.rbRightBottom);
+      this.Controls.Add(this.rbLeftTop);
+      this.Name = "FrmTgtFrame";
+      this.Text = "FrmTgtFrame";
+      this.Load += new System.EventHandler(this.FrmTgtFrame_Load);
+      this.Shown += new System.EventHandler(this.FrmTgtFrame_Shown);
+      this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.FrmTgtFrame_MouseDoubleClick);
+      this.contextMenuStrip1.ResumeLayout(false);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.RadioButton rbLeftTop;
+    private System.Windows.Forms.RadioButton rbRightBottom;
+    private System.Windows.Forms.Label lbHorizonDist;
+    private System.Windows.Forms.Label lbVerticalDist;
+    private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpacity;
+    private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpacityGain;
+    private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpacityDec;
+    private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemClose;
+    private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemPointTest;
+  }
+}
---
+++ C#/LogicTemp/Form/TgtFrameForm/WFA/FrmTgtFrame.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WFA
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public partial class FrmTgtFrame : Form
+  {
+    #region コンストラクタ
+    public FrmTgtFrame()
+    {
+      InitializeComponent();
+    }
+    #endregion
+
+    #region 宣言
+
+    ////調整フォーム宣言
+    //Form3 DrawRegulationFormClass;
+
+    // 不透明度退避
+    double evacuateOpacity;
+
+    // 親フォーム
+    public Form1 form1 { get; set; }
+
+    // 開始/終了座標
+    int leftTopX { get; set; }
+    int leftTopY { get; set; }
+    int rightBottomX { get; set; }
+    int rightBottomY { get; set; }
+
+    #endregion
+
+
+    #region フォームロードイベント
+    private void FrmTgtFrame_Load(object sender, EventArgs e)
+    {
+      // フォームの大きさを最大に固定
+      this.WindowState = FormWindowState.Maximized;
+      // フォーム不透明度設定
+      this.Opacity = 0.6;
+      // タイトルバー非表示
+      this.FormBorderStyle = FormBorderStyle.None;
+
+      leftTopX = 2;
+      leftTopY = 2;
+
+      rightBottomX = 10;
+      rightBottomY = 10;
+    }
+    #endregion
+
+    #region ロームショウイベント
+    private void FrmTgtFrame_Shown(object sender, EventArgs e)
+    {
+      // 対象正方形描画メソッド使用
+      DrawSquare();
+    }
+    #endregion
+
+    #region マウスダブルクリックイベント
+    private void FrmTgtFrame_MouseDoubleClick(object sender, MouseEventArgs e)
+    {
+      // 座標取得メソッド使用
+      GetPosition();
+    }
+    #endregion
+
+
+    #region 座標取得メソッド
+    private void GetPosition()
+    {
+      #region 【要調査】
+
+      /*
+       * 微妙に左上にズレる
+       */
+      //// クリック位置の座標取得
+      //int x = Cursor.Position.X;
+      //int y = Cursor.Position.Y;
+
+      #endregion
+
+      // フォーム上の座標でマウスポインタの位置を取得する
+      Point sp = Cursor.Position;
+      // 画面座標をクライアント座標に変換する
+      Point cp = this.PointToClient(sp);
+
+      //// クリック位置の座標取得
+      int x = cp.X;
+      int y = cp.Y;
+
+      // 背景が黒の場合
+      if (this.BackColor == Color.Black)
+      {
+        // ポイントテスト描画メソッド使用
+        DrawPointTest(x, y);
+
+        return;
+      }
+
+      // ラジオボタンで始点か終点を判断
+      if (rbLeftTop.Checked)
+      {
+        // プロパティ設定
+        leftTopX = x;
+        leftTopY = y;
+
+        // ラジオボタンに始点座標を表示
+        rbLeftTop.Text = x.ToString() + "×" + y.ToString();
+
+        // 右下ラジオボタンにチェック
+        rbRightBottom.Checked = true;
+      }
+      else if (rbRightBottom.Checked)
+      {
+        rightBottomX = x;
+        rightBottomY = y;
+
+        rbRightBottom.Text = x.ToString() + "×" + y.ToString();
+
+        rbLeftTop.Checked = true;
+
+        // 対象正方形描画メソッド使用
+        // 描画処理は終点のみで行う
+        DrawSquare();
+      }
+
+      return;
+    }
+    #endregion
+
+    #region ポイントテスト描画メソッド
+    private void DrawPointTest(int x, int y)
+    {
+      // 対象点作成
+      Point targetPoint = new Point(x, y);
+
+      // Graphicsオブジェクトの作成
+      using (Graphics g = this.CreateGraphics())
+      {
+        // 白点画像を作成
+        Bitmap p = new Bitmap(1, 1);
+        p.SetPixel(0, 0, Color.White);
+
+        // 点描画
+        g.DrawImageUnscaled(p, targetPoint);
+
+        return;
+      }
+    }
+    #endregion
+
+    #region 対象正方形描画メソッド
+    private void DrawSquare()
+    {
+      // Graphicsオブジェクトの作成
+      using (Graphics g = this.CreateGraphics())
+      {
+        // 色を初期化
+        g.Clear(SystemColors.Control);
+        // 直線型のインスタンス生成
+        Pen frontCoverPen = new Pen(Color.Green, 1);
+
+        // 表紙lineの始点と終点を設定
+        Point leftTop = new Point(leftTopX, leftTopY);
+        Point rightButtom = new Point(rightBottomX, rightBottomY);
+        // 右上と左下は始点と終点の値をそのまま使用できる
+        Point rightTop = new Point(rightBottomX, leftTopY);
+        Point leftButtom = new Point(leftTopX, rightBottomY);
+
+        // 表紙lineを描画
+        g.DrawLine(frontCoverPen, leftTop, rightTop);
+        g.DrawLine(frontCoverPen, leftTop, leftButtom);
+        g.DrawLine(frontCoverPen, rightButtom, rightTop);
+        g.DrawLine(frontCoverPen, rightButtom, leftButtom);
+
+        return;
+      }
+    }
+    #endregion
+
+
+    #region コンテキスト_不透明度押下イベント
+    private void toolStripMenuItemOpacity_Click(object sender, EventArgs e)
+    {
+      // デフォルトに戻す
+      this.Opacity = 0.8;
+    }
+    #endregion
+
+    #region コンテキスト_上げ押下イベント
+    private void toolStripMenuItemOpacityGain_Click(object sender, EventArgs e)
+    {
+      // 不透明度を上げる
+      this.Opacity += 0.2;
+    }
+    #endregion
+
+    #region コンテキスト_下げ押下イベント
+    private void toolStripMenuItemOpacityDec_Click(object sender, EventArgs e)
+    {
+      // 不透明度を下げる
+      this.Opacity -= 0.2;
+    }
+    #endregion
+
+    #region コンテキスト_ポイントテスト押下イベント
+    private void ToolStripMenuItemPointTest_Click(object sender, EventArgs e)
+    {
+      // 標準色の場合
+      if (this.BackColor == SystemColors.Control)
+      {
+        // 色を黒にする
+        this.BackColor = Color.Black;
+
+        // 不透明度退避
+        evacuateOpacity = this.Opacity;
+        // 不透明度を最大に上げる
+        this.Opacity = 0.8;
+      }
+      else if (this.BackColor == Color.Black)
+      {
+        // 色を元に戻す
+        this.BackColor = SystemColors.Control;
+
+        // 不透明度を戻す
+        this.Opacity = evacuateOpacity;
+      }
+    }
+    #endregion
+
+    #region コンテキスト_閉じる押下イベント
+    private void toolStripMenuItemClose_Click(object sender, EventArgs e)
+    {
+      // フォーム閉じる
+      this.Close();
+    }
+    #endregion
+  }
+}
