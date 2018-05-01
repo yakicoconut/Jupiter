54ed30cce9b368d50f49502607e7e287665df08e
ys <abc@def.com>
2018-05-01 09:37:39 +0900

    C#_ImgViewerApp_修正_ファイルリストフォーム(一つ前のリビジョンのコミットし忘れ)

---
+++ C#/Create/ImgViewerApp/WFA/ComForm/FrmFileList.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WFA
+{
+  partial class FrmFileList
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+      this.toolStripMenuItemOpacity = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripMenuItemOpacityGain = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripMenuItemOpacityDec = new System.Windows.Forms.ToolStripMenuItem();
+      this.lvFileList = new System.Windows.Forms.ListView();
+      this.contextMenuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // contextMenuStrip1
+      // 
+      this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemOpacity});
+      this.contextMenuStrip1.Name = "contextMenuStrip1";
+      this.contextMenuStrip1.Size = new System.Drawing.Size(125, 26);
+      // 
+      // toolStripMenuItemOpacity
+      // 
+      this.toolStripMenuItemOpacity.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemOpacityGain,
+            this.toolStripMenuItemOpacityDec});
+      this.toolStripMenuItemOpacity.Name = "toolStripMenuItemOpacity";
+      this.toolStripMenuItemOpacity.Size = new System.Drawing.Size(124, 22);
+      this.toolStripMenuItemOpacity.Text = "不透明度";
+      this.toolStripMenuItemOpacity.Click += new System.EventHandler(this.toolStripMenuItemOpacity_Click);
+      // 
+      // toolStripMenuItemOpacityGain
+      // 
+      this.toolStripMenuItemOpacityGain.Name = "toolStripMenuItemOpacityGain";
+      this.toolStripMenuItemOpacityGain.Size = new System.Drawing.Size(100, 22);
+      this.toolStripMenuItemOpacityGain.Text = "上げ";
+      this.toolStripMenuItemOpacityGain.Click += new System.EventHandler(this.toolStripMenuItemOpacityGain_Click);
+      // 
+      // toolStripMenuItemOpacityDec
+      // 
+      this.toolStripMenuItemOpacityDec.Name = "toolStripMenuItemOpacityDec";
+      this.toolStripMenuItemOpacityDec.Size = new System.Drawing.Size(100, 22);
+      this.toolStripMenuItemOpacityDec.Text = "下げ";
+      this.toolStripMenuItemOpacityDec.Click += new System.EventHandler(this.toolStripMenuItemOpacityDec_Click);
+      // 
+      // lvFileList
+      // 
+      this.lvFileList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.lvFileList.CheckBoxes = true;
+      this.lvFileList.ContextMenuStrip = this.contextMenuStrip1;
+      this.lvFileList.Location = new System.Drawing.Point(10, 10);
+      this.lvFileList.Name = "lvFileList";
+      this.lvFileList.Size = new System.Drawing.Size(150, 285);
+      this.lvFileList.TabIndex = 0;
+      this.lvFileList.UseCompatibleStateImageBehavior = false;
+      this.lvFileList.View = System.Windows.Forms.View.List;
+      this.lvFileList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvFileList_MouseDoubleClick);
+      this.lvFileList.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lvFileList_MouseDown);
+      // 
+      // FrmFileList
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+      this.ClientSize = new System.Drawing.Size(170, 305);
+      this.ContextMenuStrip = this.contextMenuStrip1;
+      this.Controls.Add(this.lvFileList);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "FrmFileList";
+      this.Opacity = 0.8D;
+      this.Text = "Form2";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+      this.Load += new System.EventHandler(this.Form2_Load);
+      this.contextMenuStrip1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpacity;
+    private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpacityGain;
+    private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpacityDec;
+    public System.Windows.Forms.ListView lvFileList;
+  }
+}
---
+++ C#/Create/ImgViewerApp/WFA/ComForm/FrmFileList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WFA
+{
+  public partial class FrmFileList : Form
+  {
+    #region コンストラクタ
+    public FrmFileList()
+    {
+      InitializeComponent();
+
+      // タイトル設定
+      this.Text = "FileList";
+    }
+    #endregion
+
+
+    #region 宣言
+
+    // 親フォーム
+    public Form1 form1 { get; set; }
+
+    #endregion
+
+
+    #region フォームロードイベント
+    private void Form2_Load(object sender, EventArgs e)
+    {
+      this.Text = "Option";
+    }
+    #endregion
+
+
+    #region リストビューダブルクリックイベント
+    private void lvFileList_MouseDoubleClick(object sender, MouseEventArgs e)
+    {
+
+    }
+    #endregion
+
+    #region リストビューマウスダウンイベント
+    private void lvFileList_MouseDown(object sender, MouseEventArgs e)
+    {
+
+    }
+    #endregion
+
+
+    #region コンテキスト_不透明度押下イベント
+    private void toolStripMenuItemOpacity_Click(object sender, EventArgs e)
+    {
+      //デフォルトに戻す
+      this.Opacity = 0.8;
+    }
+    #endregion
+
+    #region コンテキスト_上げ押下イベント
+    private void toolStripMenuItemOpacityGain_Click(object sender, EventArgs e)
+    {
+      //不透明度を上げる
+      this.Opacity += 0.2;
+    }
+    #endregion
+
+    #region コンテキスト_下げ押下イベント
+    private void toolStripMenuItemOpacityDec_Click(object sender, EventArgs e)
+    {
+      //不透明度を下げる
+      this.Opacity -= 0.2;
+    }
+    #endregion
+
+
+    #region フォームクロージングイベント
+    private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      // クローズキャンセル
+      if (e.CloseReason == CloseReason.UserClosing)
+        e.Cancel = true;
+    }
+    #endregion
+  }
+}
