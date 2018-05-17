d285e49cd0d48f93e4e5abf0c945999bd79e2158
ys <abc@def.com>
2018-05-17 20:16:03 +0900

    Python_Template_テンプレート追加

---
+++ Python/Template/TemplateProj/Main.py
@@ -0,0 +1,12 @@
+# # プロジェクトテンプレート # #
+# # ディスクリプション
+
+
+# # 共通関数フォルダの相対的指定
+import sys
+import os
+sys.path.append(__file__.rsplit("/", 2)[0] + r"/OwnLib")  # 二階層上の共通関数フォルダを指定
+# from OL_Template import *  # 共通関数ファイルの読み込み例
+
+
+# print(fn_template)  # 関数使用例
