19c64d32551147a23bea5449de047bad07d974f6
cal02 <abc@def.com>
2018-07-15 21:52:09 +0900

    C#_ImgConvApp_追加

---
+++ C#/Create/ImgConvApp/WFA/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WFA
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows フォーム デザイナーで生成されたコード
+
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btConvert = new System.Windows.Forms.Button();
+      this.tbTargetFolder = new System.Windows.Forms.TextBox();
+      this.cbExtention = new System.Windows.Forms.ComboBox();
+      this.SuspendLayout();
+      // 
+      // btConvert
+      // 
+      this.btConvert.Location = new System.Drawing.Point(303, 43);
+      this.btConvert.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+      this.btConvert.Name = "btConvert";
+      this.btConvert.Size = new System.Drawing.Size(125, 34);
+      this.btConvert.TabIndex = 1;
+      this.btConvert.Text = "変換";
+      this.btConvert.UseVisualStyleBackColor = true;
+      this.btConvert.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // tbTargetFolder
+      // 
+      this.tbTargetFolder.Location = new System.Drawing.Point(14, 13);
+      this.tbTargetFolder.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+      this.tbTargetFolder.Name = "tbTargetFolder";
+      this.tbTargetFolder.Size = new System.Drawing.Size(414, 25);
+      this.tbTargetFolder.TabIndex = 2;
+      // 
+      // cbExtention
+      // 
+      this.cbExtention.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.cbExtention.FormattingEnabled = true;
+      this.cbExtention.Location = new System.Drawing.Point(14, 51);
+      this.cbExtention.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+      this.cbExtention.Name = "cbExtention";
+      this.cbExtention.Size = new System.Drawing.Size(122, 26);
+      this.cbExtention.TabIndex = 3;
+      this.cbExtention.Validated += new System.EventHandler(this.comboBox1_Validated);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(445, 97);
+      this.Controls.Add(this.cbExtention);
+      this.Controls.Add(this.tbTargetFolder);
+      this.Controls.Add(this.btConvert);
+      this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btConvert;
+    private System.Windows.Forms.TextBox tbTargetFolder;
+    private System.Windows.Forms.ComboBox cbExtention;
+  }
+}
+
---
+++ C#/Create/ImgConvApp/WFA/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Configuration;
+using System.Drawing.Imaging;
+
+namespace WFA
+{
+  /// <summary>
+  /// メインフォーム
+  /// </summary>
+  public partial class Form1 : Form
+  {
+    #region コンストラクタ
+    public Form1()
+    {
+      InitializeComponent();
+
+      #region 【論理雛形】
+      WFAComLogic WFACL = new WFAComLogic();
+      // アプリ名設定
+      Text = WFACL.GetAppName();
+      #endregion
+
+      // コンフィグ取得メソッド使用
+      GetConfig();
+    }
+    #endregion
+
+    #region コンフィグ取得メソッド
+    public void GetConfig()
+    {
+      // 変換先拡張子
+      imgExtention = ConfigurationManager.AppSettings["ImgExtention"].Split(',');
+
+    }
+    #endregion
+
+
+    #region 宣言
+
+    // 共通ロジッククラスインスタンス
+    MCSComLogic _comLogic = new MCSComLogic();
+
+    // 変換先拡張子
+    string[] imgExtention;
+
+    // 画像フォーマット変数
+    ImageFormat imageFormat;
+
+    #endregion
+
+
+    #region メインフォーム初期設定メソッド
+    private void MainFormInitSeting()
+    {
+      // 変換先拡張子コンボボックス設定
+      cbExtention.DataSource = imgExtention;
+    }
+    #endregion
+
+
+    #region ロードイベント
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      // メインフォーム初期設定メソッド使用
+      MainFormInitSeting();
+    }
+    #endregion
+
+    #region コンボボックス値変更イベント
+    private void comboBox1_Validated(object sender, EventArgs e)
+    {
+      // 拡張子コンボボックスからフォーマットを選判定する
+      switch (cbExtention.Text)
+      {
+        case "jpg":
+          imageFormat = ImageFormat.Jpeg;
+          break;
+
+        case "bmp":
+          imageFormat = ImageFormat.Bmp;
+          break;
+
+        case "gif":
+          imageFormat = ImageFormat.Gif;
+          break;
+
+        case "png":
+          imageFormat = ImageFormat.Png;
+          break;
+      }
+    }
+    #endregion
+
+    #region ボタン1押下イベント
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // 画像変換メソッド使用
+      ImgConversion();
+    }
+    #endregion
+
+    #region ボタン2押下イベント
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+    #endregion
+
+
+    #region 画像変換メソッド
+    public void ImgConversion()
+    {
+      // ターゲットフォルダを取得
+      string targetFolder = tbTargetFolder.Text;
+      // 対象フォルダ内のファイルを取得
+      string[] files = Directory.GetFiles(targetFolder, "*", SearchOption.AllDirectories);
+      // ねずみ返し_対象ファイルがない場合
+      if (files.Length == 0)
+      {
+        MessageBox.Show("対象のファイルがありません");
+        return;
+      }
+
+      // 全てのファイルを処理
+      foreach (string x in files)
+      {
+        try
+        {
+          // ビットマップに変換
+          using (Bitmap img = (Bitmap)Bitmap.FromFile(x))
+          {
+            // ファイル名取得(拡張子なし)
+            string fileName = Path.GetFileNameWithoutExtension(x);
+            // フォーマットを指定して保存
+            img.Save(fileName + "." + cbExtention.Text, imageFormat);
+          }
+        }
+        catch
+        {
+          continue;
+        }
+      }
+      MessageBox.Show("完了しました");
+    }
+    #endregion
+
+
+    #region 雛形メソッド
+    public void template()
+    {
+
+    }
+    #endregion
+  }
+}
