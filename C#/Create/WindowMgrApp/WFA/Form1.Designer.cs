9beb94f4e7ec1164751efc0765578a92809c1d22
ys <abc@def.com>
2018-08-09 10:47:07 +0900

    C#_WindowMgrApp_追加

---
+++ C#/Create/WindowMgrApp/WFA/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.btRefresh = new System.Windows.Forms.Button();
+      this.lvProcessList = new System.Windows.Forms.ListView();
+      this.SuspendLayout();
+      // 
+      // btRefresh
+      // 
+      this.btRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.btRefresh.Location = new System.Drawing.Point(12, 899);
+      this.btRefresh.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+      this.btRefresh.Name = "btRefresh";
+      this.btRefresh.Size = new System.Drawing.Size(125, 34);
+      this.btRefresh.TabIndex = 1;
+      this.btRefresh.Text = "更新";
+      this.btRefresh.UseVisualStyleBackColor = true;
+      this.btRefresh.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // lvProcessList
+      // 
+      this.lvProcessList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.lvProcessList.CheckBoxes = true;
+      this.lvProcessList.Location = new System.Drawing.Point(12, 12);
+      this.lvProcessList.Name = "lvProcessList";
+      this.lvProcessList.Size = new System.Drawing.Size(378, 880);
+      this.lvProcessList.TabIndex = 4;
+      this.lvProcessList.UseCompatibleStateImageBehavior = false;
+      this.lvProcessList.View = System.Windows.Forms.View.List;
+      this.lvProcessList.SelectedIndexChanged += new System.EventHandler(this.lvProcessList_SelectedIndexChanged);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(402, 961);
+      this.Controls.Add(this.lvProcessList);
+      this.Controls.Add(this.btRefresh);
+      this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btRefresh;
+    private System.Windows.Forms.ListView lvProcessList;
+  }
+}
+
---
+++ C#/Create/WindowMgrApp/WFA/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Configuration;
+using System.Runtime.InteropServices;
+
+/*
+ * 画面上のすべてのウィンドウとそのタイトルを列挙する - .NET Tips (VB.NET,C#...)
+ * 	https://dobon.net/vb/dotnet/process/enumwindows.html
+ * 外部アプリケーションのウィンドウをアクティブにする - .NET Tips (VB.NET,C#...)
+ * 	https://dobon.net/vb/dotnet/process/appactivate.html
+ */
+namespace WFA
+{
+  /// <summary>
+  /// メインフォーム
+  /// </summary>
+  public partial class Form1 : Form
+  {
+    #region コンストラクタ
+    public Form1()
+    {
+      InitializeComponent();
+
+      #region 【論理雛形】
+      WFAComLogic WFACL = new WFAComLogic();
+      // アプリ名設定
+      Text = WFACL.GetAppName();
+      #endregion
+
+      // コンフィグ取得メソッド使用
+      GetConfig();
+    }
+    #endregion
+
+    #region コンフィグ取得メソッド
+    public void GetConfig()
+    {
+      // 除外ウィンドウ名
+      omitTitle = _comLogic.GetConfigValue("OmitTitle", "DefaultValue").Split(',');
+    }
+    #endregion
+
+
+    #region 宣言
+
+    // 共通ロジッククラスインスタンス
+    MCSComLogic _comLogic = new MCSComLogic();
+
+    // 画像パスディクショナリ
+    public Dictionary<int, Process> dicProcess = new Dictionary<int, Process>();
+
+    // 除外ウィンドウ名
+    string[] omitTitle;
+
+    #endregion
+
+    #region DllImport
+
+    // 外部プロセスのメイン・ウィンドウを起動するためのWin32 API
+    [DllImport("user32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+    [DllImport("user32.dll")]
+    private static extern bool IsIconic(IntPtr hWnd);
+    // ShowWindowAsync関数のパラメータに渡す定義値
+    // 画面を元の大きさに戻す
+    private const int SW_RESTORE = 9;
+
+    #endregion
+
+
+    #region フォームロードイベント
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      // リストビューマルチセレクト禁止
+      lvProcessList.MultiSelect = false;
+
+      // リストビュー初期化メソッド使用
+      InitListView();
+    }
+    #endregion
+
+    #region リストビュー選択項目変更イベント
+    private void lvProcessList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      // 選択中アイテム取得
+      var windowTitle = lvProcessList.SelectedItems;
+
+      // ねずみ返し_選択中アイテムがない場合
+      if (windowTitle.Count <= 0)
+        return;
+
+      // ねずみ返し_選択中アイテムにチェックがある場合
+      if (windowTitle[0].Checked)
+        return;
+
+      #region コメント_VBウィンドウアクティブメソッド使用
+
+      //// VBウィンドウアクティブメソッド使用
+      //// ウィンドウタイトルでアクティブ化
+      //ActiveWindowVB(windowTitle[0].Text);
+
+      #endregion
+
+      // ディクショナリからウィンドウプロセス取得
+      Process windowProc = dicProcess[windowTitle[0].Index];
+      // User32ウィンドウアクティブメソッド使用
+      ActiveWindowUser32(windowProc.MainWindowHandle);
+
+      // 自身をアクティブ化
+      this.Activate();
+    }
+    #endregion
+
+    #region 更新ボタン押下イベント
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // リストビュー初期化メソッド使用
+      InitListView();
+    }
+    #endregion
+
+
+    #region リストビュー初期化メソッド
+    /// <summary>
+    /// リストビュー初期化メソッド
+    /// </summary>
+    public void InitListView()
+    {
+      // リストビュー初期化
+      lvProcessList.Items.Clear();
+
+      // 全プロセスをループ処理
+      int i = 0;
+      foreach (Process x in Process.GetProcesses())
+      {
+        // ねずみ返し_タイトルがない場合
+        if (x.MainWindowTitle == string.Empty)
+          continue;
+
+        // ウィンドウタイトル取得
+        string windowTitle = Path.GetFileName(x.MainWindowTitle);
+
+        // リストビューにプロセス名追加
+        lvProcessList.Items.Add(windowTitle);
+
+        // 除外ウィンドウ名の場合
+        if (omitTitle.Contains(windowTitle))
+          // チェックをつける
+          lvProcessList.Items[i].Checked = true;
+
+        // ディクショナリにも追加
+        dicProcess.Add(i, x);
+        i += 1;
+      }
+    }
+    #endregion
+
+
+    #region VBウィンドウアクティブメソッド
+    public void ActiveWindowVB(string windowTitle)
+    {
+      // VB.dll使用
+      Interaction.AppActivate(windowTitle);
+    }
+    #endregion
+
+    #region User32ウィンドウアクティブメソッド
+    public void ActiveWindowUser32(IntPtr windowHandle)
+    {
+      //// 単純なアクティブ化
+      //SetForegroundWindow(windowHandle);
+
+      // ウィンドウが最小化されていれば元に戻す
+      if (IsIconic(windowHandle))
+      {
+        ShowWindowAsync(windowHandle, SW_RESTORE);
+      }
+
+      // ウィンドウを最前面に表示する
+      SetForegroundWindow(windowHandle);
+    }
+    #endregion
+
+
+    #region 雛形メソッド
+    public void template()
+    {
+
+    }
+    #endregion
+  }
+}
