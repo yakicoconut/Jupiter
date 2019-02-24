e3df4d38320199f95c6805c7fb6cc7ca7dd20b8a
cal01 <abc@def.com>
2019-02-24 12:17:55 +0900

    Batch_Sample_Exec_ChkTimeFormat_追加

---
+++ Batch/Sample/Exec_ChkTimeFormat.bat
@@ -0,0 +1,147 @@
+@echo off
+title %~nx0
+echo 時刻判定バッチの使用例
+
+
+: 参照バッチ
+  rem 時刻判定バッチ
+  set call_ChkTimeFormat="..\OwnLib\ChkTimeFormat.bat"
+
+
+: 処理
+  : 複数パターン
+    set target=123
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 文字複合パターン
+    set target=1a2b3c
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 文字パターン
+    set target=def
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「m;s」パターン
+    set target=1:0
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「mm;s」パターン
+    set target=10:5
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「m:ss」パターン
+    set target=0:50
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「mm:ss」パターン
+    set target=12:51
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「h:m;s」パターン
+    set target=4:5:4
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「h:mm;s」パターン
+    set target=3:10:2
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「h:m:ss」パターン
+    set target=5:4:20
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「h:mm:ss」パターン
+    set target=6:10:50
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「hh:m;s」パターン
+    set target=14:1:5
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「hh:mm;s」パターン
+    set target=21:10:0
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「hh:m:ss」パターン
+    set target=11:1:50
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+  : 「hh:mm:ss」パターン
+    set target=09:10:50
+
+    call %call_ChkTimeFormat% %target%
+    echo %target%
+    echo %return_ChkTimeFormat%
+    echo ---------------
+
+
+pause
