d87859b2182a651807d55a8443a0d21bee5fbd5c
ys <abc@def.com>
2019-05-09 19:22:34 +0900

    C#_Function_Cast_追加

---
+++ C#/Function/Cast/001/WCA/Program.cs
@@ -0,0 +1,98 @@
+﻿#define ERROR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * キャスト
+ *   アップキャスト
+ *     基底クラスの変数型に派生クラスのインスタンスを代入することは
+ *     常に可能(明示的なキャストが必要ない)
+ *   ダウンキャスト
+ *     派生クラスの変数型に基底クラスのインスタンスを代入するには
+ *     明示的なキャストが必要
+ *   ・アップキャストされたインスタンスが格納されている変数は
+ *     型が一緒でも中身のインスタンスが別のクラスという可能性があるため
+ *     文法的なダウンキャストができていて(コンパイルが通って)も実行時に実際に
+ *     変数代入が行われたときにエラーとなるパターンがある
+ *   
+ *     クラス定義
+ *       基底クラス →継承→ 派生クラス1
+ *                  →継承→ 派生クラス2
+ *   
+ *     インスタンス代入
+ *       基底クラス型  変数①  = 派生クラス1インスタンス        ⇒ ○:アップキャスト
+ *       基底クラス型  変数②  = 派生クラス2インスタンス        ⇒ ○:アップキャスト
+ *       派生クラス1型 変数③  = (キャスト:派生クラス1型)変数① ⇒ ○:ダウンキャスト(明示的な変換必須)
+ *       派生クラス2型 変数④  = (キャスト:派生クラス2型)変数② ⇒ ○:ダウンキャスト(明示的な変換必須)
+ *       
+ *       派生クラス2型 変数⑤  = (キャスト:派生クラス2型)変数① ⇒ △:コンパイルは通るが実行時エラー
+ *                                                                    あくまで派生クラス型の変数に基底クラス型を
+ *                                                                    代入しようとするため明示的なキャストをすれば
+ *                                                                    コンパイルは通る(文法的にはエラーにならない)
+ */
+namespace WCA
+{
+  /// <summary>
+  /// メインプログラム
+  /// </summary>
+  class Program
+  {
+    #region 宣言
+
+    // 共通ロジッククラスインスタンス
+    static MCSComLogic _comLogic = new MCSComLogic();
+
+    #endregion
+
+
+    // クラス定義
+    class Base { }
+    class Derived1 : Base { }
+    class Derived2 : Base { }
+
+
+    static void Main(string[] args)
+    {      
+      // 通常のインスタンス生成
+      Derived1 dynamicD1 = new Derived1();
+      Derived2 dynamicD2 = new Derived2();
+      Base dynamicB = new Base();
+      
+
+      // 単純なアップキャスト
+      // 「Base」クラス型の変数に
+      // 「Derived1」クラスのインスタンスを入れる
+      Base staticB1 = new Derived1();
+      // 「Derived1」クラス型の変数に
+      // 「Base」クラス型の「Derived1」クラスインスタンスを入れるが
+      // ダウンキャストとなるため明示的なキャストが必須
+      Derived1 staticD1 = (Derived1)staticB1;
+
+
+      // 単純なアップキャスト
+      Base staticB2 = new Derived1();
+      // 「Derived2」クラス型の変数に
+      // 「Base」クラス型の「Derived1」クラスインスタンスを入れる
+      // →「Base」クラス型の変数のためキャストは可能だが
+      //   インスタンスとして入っているのは「Derived1」クラスのため実行時にエラーとなる
+      Derived2 staticD2 = (Derived2)staticB2;
+
+      // 派生クラスに基底クラスのインスタンスは入れられない
+      Derived1 staticD1Error = (Derived1)new Base();
+
+
+      #region パターン_エラー
+#if ERROR
+
+
+
+#endif
+      #endregion
+
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ C#/Function/Cast/002/WCA/Program.cs
@@ -0,0 +1,124 @@
+﻿//#define ERROR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * as・is
+ *   ・アップキャストした変数では派生クラスのメンバに
+ *     アクセスすることが出来ないためダウンキャストする必要があるが
+ *     間違った派生クラス型に対してキャストを行うとコンパイル上は問題ないが
+ *     処理実行時にエラーとなってしまう
+ *   ・as演算子とis演算子でインスタンスとキャスト対象インスタンスの関係性を
+ *     調べることが出来、上記のエラーを防ぐことが可能
+ *   is
+ *     ・該当インスタンスが該当型に対してキャスト可能かどうかを返す
+ *     ・あくまでキャスト可能かどうかを判断するため厳密に同じ型かどうかは判断できない
+ *   as
+ *     ・キャスト可能な場合はキャストを実行しキャストできない場合はnullを返す
+ *       通常のキャストとの違いはエラーを返すかどうか
+ */
+namespace WCA
+{
+  /// <summary>
+  /// メインプログラム
+  /// </summary>
+  class Program
+  {
+    #region 宣言
+
+    // 共通ロジッククラスインスタンス
+    static MCSComLogic _comLogic = new MCSComLogic();
+
+    #endregion
+
+
+    // クラス定義
+    class Base
+    {
+      public string var01 = "abc";
+    }
+
+    class Derived1 : Base
+    {
+      public string var02 = "def";
+    }
+
+    class Derived2 : Base
+    {
+      public string var03 = "ghi";
+    }
+
+
+    static void Main(string[] args)
+    {      
+      Derived1 dynamicD1;
+      Derived2 dynamicD2;
+      Base dynamicB1;
+      Base dynamicB2;
+
+      // 通常アップキャスト
+      dynamicB1 = new Derived1();
+      dynamicB2 = new Derived2();
+      // 通常ダウンキャスト
+      dynamicD1 = (Derived1)dynamicB1;
+      dynamicD2 = (Derived2)dynamicB2;
+
+
+      #region パターン_エラー
+#if ERROR
+
+      // テレコしたダウンキャスト
+      dynamicD1 = (Derived1)dynamicB2;
+      dynamicD2 = (Derived2)dynamicB1;
+
+#endif
+      #endregion
+
+
+      /* is */
+      Derived1 varD1 = new Derived1();
+      // 通常インスタンスは当然trueとなる
+      Console.WriteLine(varD1 is Derived1);
+      // アップキャストされた変数は元の型には戻せる
+      Console.WriteLine(dynamicB1 is Derived1);
+      // アップキャストされた変数は別の型には戻せない
+      Console.WriteLine(dynamicB2 is Derived1);
+      // キャスト可能か判断
+      if (dynamicB1 is Derived1)
+      {
+        // ダウンキャスト前は基底クラスのメンバしか使用できない
+        Console.WriteLine(dynamicB1.var01);
+
+        // 可能な場合はダウンキャスト
+        dynamicD1 = (Derived1)dynamicB1;
+
+        // ダウンキャスト後は元のクラスのメンバも使用できる
+        Console.WriteLine(dynamicD1.var02);
+      }
+
+      // キャスト可能かどうかを判断するため
+      // 厳密に同じ型かどうかは判断できない
+      Console.WriteLine(dynamicB1 is Derived1);
+      Console.WriteLine(dynamicB1 is Base);
+      // 厳密に判断したい場合は以下のようにする
+      Console.WriteLine(dynamicB1.GetType() == typeof(Derived1));
+      Console.WriteLine(dynamicB1.GetType() == typeof(Base));
+
+
+      /* as */
+      // キャストできないためnullを格納する
+      Derived1 varD2 = dynamicB2 as Derived1;
+      Console.WriteLine(varD2 == null);
+      // キャストできる
+      varD1 = dynamicB1 as Derived1;
+      Console.WriteLine(varD1.var02);
+      // isで書き換えると以下となる
+      Derived1 varD1_1 = dynamicB1 is Derived1 ? (Derived1)dynamicB1 : null;
+
+
+      Console.ReadKey();
+    }
+  }
+}
