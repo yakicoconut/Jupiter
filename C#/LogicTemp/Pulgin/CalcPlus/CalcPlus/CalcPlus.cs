0fa2b0e7134702f1e70a346f1c5fc4aaf9ad8da3
ys <abc@def.com>
2019-05-17 18:11:42 +0900

    C#_LogicTemp_Pligin_追加

---
+++ C#/LogicTemp/Pulgin/CalcPlus/CalcPlus/CalcPlus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICalcPlugin;
+
+namespace CalcPlus
+{
+  /// <summary>
+  /// 足し算プラグインクラス
+  /// </summary>
+  public class Plus : ICalcClass
+  {
+    #region ICalcClassメンバ
+
+    #region プロパティ
+
+    /// <summary>　
+    /// 計算記号
+    /// </summary>
+    public string Symbol { get; set; }
+
+    #endregion
+
+
+    #region 計算メソッド
+    /// <summary>
+    /// 計算メソッド
+    /// </summary>
+    /// <param name="luftElem">左辺</param>
+    /// <param name="rightElem">右辺</param>
+    /// <returns>計算結果</returns>
+    public int Calc(int leftElem, int rightElem)
+    {
+      // 計算記号プロパティ設定
+      Symbol = "+";
+
+      // 計算
+      return leftElem + rightElem;
+    }
+    #endregion 
+
+    #endregion
+  }
+}
