53899c5dd1316449b3dddb70e54a8b5753e35a58
ys <abc@def.com>
2019-05-24 15:04:50 +0900

    Batch_AppendTxt_修正_ユーザ入力共通バッチ化

--- Batch/Create/AppendTxt.bat
+++ Batch/Create/AppendTxt.bat
@@ -3,47 +3,43 @@ title %~nx0
 echo 同フォルダ内のファイルを一つのテキストファイルにまとめる
 
 
+: 参照バッチ
+  rem ユーザ入力バッチ
+  set call_UserInput="..\OwnLib\UserInput.bat"
+
+
 rem 出力ファイル名入力
 :INPUTOUTPUTFILENAME
-  echo;
-  echo 出力ファイル名入力
-
-  rem 変数初期化
-  set USR=
-
-  set /P USR="入力してください:"
-  set outptFileName=%USR%
-  : ねずみ返し_空白の場合
-    if "%outptFileName%"=="" (
-      echo;
-      echo 入力がありません
-      echo 終了します
-      goto :EOF
-    )
+  rem ユーザ入力バッチ使用
+  call %call_UserInput% 出力ファイル名入力※要拡張子 FALSE STR
+  if %return_UserInput2%==0 (
+    echo;
+    echo 入力がありません
+    echo 終了します
+    pause
+    goto :EOF
+  )
+  rem 入力地引継ぎ
+  set outptFileName=%return_UserInput1%
 
 
 rem 対象ファイルパス入力
 :INPUTTARGETFILEPATH
   echo;
   echo 対象ファイルパス入力
-  echo 出力ファイル名を変更する場合は空白でエンター
-
-  rem 変数初期化
-  set USR=
-
-  set /P USR="入力してください:"
-  set targetFilePath=%USR%
-  : ねずみ返し_空白の場合
-    if "%targetFilePath%"=="" (
+  rem ユーザ入力バッチ使用
+  call %call_UserInput% 出力ファイル名を変更する場合は空白でエンター FALSE STR
+  if %return_UserInput2%==0 (
       echo;
-
       goto :INPUTOUTPUTFILENAME
-    )
+  )
+  rem 入力地引継ぎ
+  set targetFilePath=%return_UserInput1%
 
 
 : ファイル出力
   rem ファイル名出力
-  echo ===============%targetFilePath%===============>>%outptFileName%
+  echo ===============%targetFilePath:"=%===============>>%outptFileName%
   rem ファイル内容出力
   type %targetFilePath%>>%outptFileName%
   rem 改行出力
