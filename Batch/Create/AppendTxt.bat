35084f00b307680ce4727096205ee59baae59a57
ys <abc@def.com>
2019-05-24 20:00:20 +0900

    Batch_AppendTxt_修正 ・出力ファイル名入力時の不正入力ループ有効 ・対象ファイルパス入力時の入力オプション最適化

--- Batch/Create/AppendTxt.bat
+++ Batch/Create/AppendTxt.bat
@@ -8,32 +8,20 @@ echo 同フォルダ内のファイルを一つのテキストファイルにまとめる
   set call_UserInput="..\OwnLib\UserInput.bat"
 
 
-rem 出力ファイル名入力
-:INPUTOUTPUTFILENAME
+: 出力ファイル名入力
+  echo 出力ファイル名入力(要拡張子)
   rem ユーザ入力バッチ使用
-  call %call_UserInput% 出力ファイル名入力※要拡張子 FALSE STR
-  if %return_UserInput2%==0 (
-    echo;
-    echo 入力がありません
-    echo 終了します
-    pause
-    goto :EOF
-  )
-  rem 入力地引継ぎ
+  call %call_UserInput% "" TRUE STR
+  rem 入力値引継ぎ
   set outptFileName=%return_UserInput1%
 
 
 rem 対象ファイルパス入力
 :INPUTTARGETFILEPATH
   echo;
-  echo 対象ファイルパス入力
   rem ユーザ入力バッチ使用
-  call %call_UserInput% 出力ファイル名を変更する場合は空白でエンター FALSE STR
-  if %return_UserInput2%==0 (
-      echo;
-      goto :INPUTOUTPUTFILENAME
-  )
-  rem 入力地引継ぎ
+  call %call_UserInput% 対象ファイルパス入力 TRUE PATH
+  rem 入力値引継ぎ
   set targetFilePath=%return_UserInput1%
 
 
