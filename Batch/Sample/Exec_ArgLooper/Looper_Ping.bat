19e9dcfffad79e8c18e9e243091a2d59689c95ab
ys <abc@def.com>
2019-08-02 16:38:02 +0900

    Batch_Sample_Exec_ArgLooper_追加

---
+++ Batch/Sample/Exec_ArgLooper/Looper_Ping.bat
@@ -0,0 +1,39 @@
+@echo off
+title %~nx0
+echo 実行コマンドファイル
+echo;
+: 引数01:ループ開始時の数値のみ年月日時分秒ミリ
+: 引数02:ループカウンタ
+: 引数03:オプション
+: 引数04:対象引数
+: 戻値  :なし
+
+
+SETLOCAL
+  : 引数判定
+    rem ねずみ返し_引数がない場合
+    if "%~1"=="" (
+      echo ※本ファイルは単体で実行しないでください
+      echo 終了します
+      pause
+      exit
+    )
+
+  : 引数
+    set datetime=%~1
+    set counter=%~2
+    set option=%~3
+    set arg=%~4
+
+  : コマンド実行
+    echo %datetime%
+    echo %counter%
+    echo %option%
+    echo %arg%
+
+    ping %option% %arg%
+    echo;
+    echo;
+
+ENDLOCAL
+exit /b
