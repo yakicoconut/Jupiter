c8c6d8d84e2da14315d00f7894d9e1dc88c6ea18
ys <abc@def.com>
2019-11-01 21:16:33 +0900

    Batch_Sample_Exec_ZeroPadding_修正_下駄モード対応

--- Batch/Sample/Exec_ZeroPadding.bat
+++ Batch/Sample/Exec_ZeroPadding.bat
@@ -8,15 +8,25 @@ echo ゼロ埋めバッチの使用例
   set call_ZeroPadding="..\OwnLib\ZeroPadding.bat"
 
 
-: 4桁パターン
+: ゼロ埋めモード_4桁パターン
   rem ゼロ埋めバッチ使用
   call %call_ZeroPadding% 123 4
   echo %return_ZeroPadding%
 
-
-: 10桁パターン
+: ゼロ埋めモード_10桁パターン
   rem ゼロ埋めバッチ使用
   call %call_ZeroPadding% 123 10
+  echo %return_ZeroPadding%
+
+
+: 下駄モード_5桁パターン
+  rem ゼロ埋めバッチ使用
+  call %call_ZeroPadding% 4 -5
+  echo %return_ZeroPadding%
+
+: 下駄モード_10桁パターン
+  rem ゼロ埋めバッチ使用
+  call %call_ZeroPadding% 56789 -10
   echo %return_ZeroPadding%
 
 
