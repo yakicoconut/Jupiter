35ff0fb639600e4d2447b3e9275075d612a64e1c
ys <abc@def.com>
2019-11-01 21:19:56 +0900

    Batch_Sample_Exec_ElapsedTime_修正_汎用時刻フォーマット対応(未完)

--- Batch/Sample/Exec_ElapsedTime.bat
+++ Batch/Sample/Exec_ElapsedTime.bat
@@ -8,54 +8,92 @@ echo 経過時間計算バッチの使用例
   set call_ElapsedTime="..\OwnLib\ElapsedTime.bat"
 
 
-: 実行時間パターン
-  rem 処理前時刻取得
-  set beforeTime=%time%
-  rem 二文字目取得
-  set hourColon=%beforeTime:~1,1%
-  rem 二文字目が「:」(時間が一桁)の場合
-  if %hourColon% == : (
-    rem 0埋めする
-    set beforeTime=0%beforeTime%
-  )
-
-  rem タイムアウト処理
-  timeout 4
-
-  rem 処理後時刻取得
-  set afterTime=%time%
-  set hourColon=%afterTime:~1,1%
-  if %hourColon% == : (
-    set afterTime=0%afterTime%
-  )
-
+: 差分計算_「mm:ss」パターン
+  echo;
+  set beforeTime=12:51
+  set  afterTime=24:10
   rem 経過時間計算バッチ使用
   call %call_ElapsedTime% %beforeTime% %afterTime%
-  echo %beforeTime% - %afterTime% → %return_ElapsedTime%
+  echo      %beforeTime%
+  echo -    %afterTime%
+  echo   ------------
+  echo   %return_ElapsedTime%
+  echo   00:11:19 ←期待結果
+
+
+: 差分計算_「h:mm:ss」パターン
+  echo;
+  set beforeTime=6:10:50
+  set  afterTime=8:00:00
+  rem 経過時間計算バッチ使用
+  call %call_ElapsedTime% %beforeTime% %afterTime%
+  echo    %beforeTime%
+  echo -  %afterTime%
+  echo   ------------
+  echo   %return_ElapsedTime%
+  echo   00:49:10 ←期待結果
+
+: 差分計算_「h:mm:ss.f」パターン
+  echo;
+  set beforeTime=6:10:50.4
+  set  afterTime=6:10:50.5
+  rem 経過時間計算バッチ使用
+  call %call_ElapsedTime% %beforeTime% %afterTime%
+  echo    %beforeTime%
+  echo -  %afterTime%
+  echo   ------------
+  echo   %return_ElapsedTime%
+  echo   00:00:00.1 ←期待結果
+
+: 差分計算_「h:mm:ss.ff」パターン
+  echo;
+  set beforeTime=6:10:50.45
+  set  afterTime=9:00:00.99
+  rem 経過時間計算バッチ使用
+  call %call_ElapsedTime% %beforeTime% %afterTime%
+  echo    %beforeTime%
+  echo -  %afterTime%
+  echo   ------------
+  echo   %return_ElapsedTime%
+  echo   02:49:10.54 ←期待結果
+
+: 差分計算_「h:mm:ss.fff」パターン
+  echo;
+  set beforeTime=1:23:45.670
+  set  afterTime=9:54:32.100
+  rem 経過時間計算バッチ使用
+  call %call_ElapsedTime% %beforeTime% %afterTime%
+  echo    %beforeTime%
+  echo -  %afterTime%
+  echo   ------------
+  echo   %return_ElapsedTime%
+  echo   08:30:46.430 ←期待結果
 
 
 : 差分計算_コンマ秒パターン
-  rem 開始時刻
+  echo;
   set beforeTime=01:59:19.40
-
-  rem 終了時刻
-  set  afterTime=01:00:00.39
-
+  set  afterTime=02:00:00.39
   rem 経過時間計算バッチ使用
   call %call_ElapsedTime% %beforeTime% %afterTime%
-  echo %beforeTime% - %afterTime% → %return_ElapsedTime%
+  echo   %beforeTime%
+  echo - %afterTime%
+  echo   ------------
+  echo   %return_ElapsedTime%
+  echo   00:00:40.99 ←期待結果
 
 
 : 差分計算パターン
-  rem 開始時刻
+  echo;
   set beforeTime=00:13:30
-
-  rem 終了時刻
   set  afterTime=00:14:00
-
   rem 経過時間計算バッチ使用
   call %call_ElapsedTime% %beforeTime% %afterTime%
-  echo %beforeTime%    - %afterTime%    → %return_ElapsedTime%
+  echo   %beforeTime%
+  echo - %afterTime%
+  echo   ------------
+  echo   %return_ElapsedTime%
+  echo   00:00:30 ←期待結果
 
 
 pause
