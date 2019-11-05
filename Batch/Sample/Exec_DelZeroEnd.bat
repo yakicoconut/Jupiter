46831ee659675b77d3c755837b484c1cda538c79
ys <abc@def.com>
2019-11-05 14:02:19 +0900

    Batch_Sample_Exec_DelZeroEnd_追加

---
+++ Batch/Sample/Exec_DelZeroEnd.bat
@@ -0,0 +1,36 @@
+@echo off
+title %~nx0
+echo ゼロ末尾削除バッチの使用例
+
+
+: 参照バッチ
+  rem 数値判定バッチ
+  set call_DelZeroEnd="..\OwnLib\DelZeroEnd.bat"
+
+
+: 処理
+  : 二桁
+    rem 対象値設定
+    set target=10
+
+    rem 数値判定バッチ使用
+    call %call_DelZeroEnd% %target%
+    echo %target%
+    echo %return_DelZeroEnd%
+    echo ---------------
+
+  : 三桁
+    set target=200
+    call %call_DelZeroEnd% %target%
+    echo %target%
+    echo %return_DelZeroEnd%
+    echo ---------------
+
+  : ゼロなし
+    set target=345
+    call %call_DelZeroEnd% %target%
+    echo %target%
+    echo %return_DelZeroEnd%
+    echo ---------------
+
+pause
