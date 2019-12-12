72f37c4cda39b39a734f11bbebf4fa9b6062fb32
cal01 <abc@def.com>
2019-12-13 00:52:39 +0900

    Batch_ExecFfmpeg_CapScreen_修正 ・ミリ秒対応 ・フレームレート確認追加

--- Batch/Create/ExecFfmpeg/CapScreen.bat
+++ Batch/Create/ExecFfmpeg/CapScreen.bat
@@ -3,6 +3,8 @@ title %~nx0
 echo ffmpegで画像取得
 : ffmpegで動画から静止画を抜き出す - Qiita
 : 	https://qiita.com/matoken/items/664e7a7e8f31e8a46a6:
+: ffmpegデフォルト出力フレームレート
+: 	https://avp.stackovernet.com/ja/q/6007
 
 
 : 参照バッチ
@@ -10,6 +12,8 @@ echo ffmpegで画像取得
   set call_UserInput="..\..\OwnLib\UserInput.bat"
   rem 経過時間計算バッチ
   set call_ElapsedTime="..\..\OwnLib\ElapsedTime.bat"
+  rem 動画情報取得バッチ
+  set call_GetMpegInfo="GetMpegInfo.bat"
 
 
 : ユーザ入力処理
@@ -19,6 +23,16 @@ echo ffmpegで画像取得
     call %call_UserInput% 対象ファイルパス入力 TRUE PATH
     rem 入力値引継ぎ
     set sourcePath=%return_UserInput1%
+
+    echo;
+    echo 対象fps
+    echo r_frame_rate  :全タイムスタンプを正確に表すことができる
+    echo                最低のフレームレート(ストリーム内のすべての
+    echo                フレームレートの最小公倍数)
+    echo avg_frame_rate:平均フレームレート
+    rem 動画情報取得バッチ使用
+    rem 対象動画fpsのみ取得オプション
+    call %call_GetMpegInfo% "-v error -select_streams v -show_entries stream=r_frame_rate -show_entries stream=avg_frame_rate" %sourcePath%
 
   : 開始時間
     echo;
@@ -32,7 +46,7 @@ echo ffmpegで画像取得
 
     rem 時刻解体サブルーチン使用
     call :DISMANTLE_TIME %start% %targetTimeFormat%
-    set start=%ret_DISMANTLE_TIME01%
+    set start=%ret_DISMANTLE_TIME01:"=%
     set startMilli=%ret_DISMANTLE_TIME02%
 
   : 分割時間
@@ -57,7 +71,36 @@ echo ffmpegで画像取得
     set rate=%return_UserInput1%
 
 
-: 秒数変換
+: 開始時間秒数変換
+    rem 文字列として分割
+    set   strHour=%start:~0,2%
+    set strMinute=%start:~3,2%
+    set strSecond=%start:~6,2%
+
+    rem 二桁目が「0」の場合
+    if %strHour:~0,1%==0 (
+      rem 数値型に格納するとエラーとなるため、一桁目のみ取得
+      set strHour=%strHour:~1,1%
+    )
+    if %strMinute:~0,1%==0 (
+      set strMinute=%strMinute:~1,1%
+    )
+    if %strSecond:~0,1%==0 (
+      set strSecond=%strSecond:~1,1%
+    )
+
+    rem 数値変換
+    set /a   hour=%strHour%
+    set /a minute=%strMinute%
+    set /a second=%strSecond%
+
+    rem 秒数変換
+    set /a   secHour=%hour%*3600
+    set /a secMinute=%minute%*60
+    set /a  startSec=%secHour%+%secMinute%+%second%
+
+
+: 分割時間秒数変換
   rem 経過時間計算バッチ使用
   call %call_ElapsedTime% %start:"=% %dist:"=%
   set elapsed=%return_ElapsedTime%
@@ -98,7 +141,7 @@ echo ffmpegで画像取得
   : -r :1秒あたり何枚抜き出すか
   :     fps(フレームレート)の確認は「ffprobe.exe 対象動画」
   : -f :「image2 %%06d.jpg」指定で「000001.jpg」から連番出力指定
-  ffmpeg\win32\ffmpeg.exe -i %sourcePath% -ss %start% -t %length% -r %rate% -f image2 Img_%%06d.png
+  ffmpeg\win32\ffmpeg.exe -i %sourcePath% -ss %startSec%%startMilli% -t %length%%distMilli% -r %rate% -f image2 Img_%%06d.png
   pause
 
 
