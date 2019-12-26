009f0c0aef3395c308a20023d58066b76f6a55af
ys <abc>
2019-12-26 18:06:15 +0900

    PowerShell_Function_Func_修正 ・引数型設定 ・返り値あり ・複数返り値

--- PowerShell/Function/Func/Func01/Main.ps1
+++ PowerShell/Function/Func/Func01/Main.ps1
@@ -15,7 +15,7 @@ Write-Host
     # エラー内容
     Write-Host "エラーパターン"
     Write-Host "※関数宣言前に呼び出し記述を行うとエラーとなる"
-   Write-Host "  $error[0]"
+    Write-Host "  $error[0]"
     Write-Host
   }
 
@@ -23,39 +23,71 @@ Write-Host
 <# 関数宣言 #>
   # # テスト関数01
   #   引数01:
-  #   返り値:
+  #   返り値:なし
   function Test01
   {
     # 引数設定
     param ($arg01)
 
-    Write-Host "テスト関数01"
+    Write-Host "テスト関数01_通常"
     Write-Host $arg01
     Write-Host
   }
 
-  # テスト関数02
+  # # テスト関数02
   #   引数01:
-  #   返り値:
+  #   返り値:なし
   function Test02 ($arg01)
   {
-    # 引数設定を「param」キーワードなしで行う
-    Write-Host "テスト関数02"
+    Write-Host "テスト関数02_「param」引数設定"
     Write-Host $arg01
     Write-Host
   }
 
   # # テスト関数03
-  # #   引数01:
-  # #   返り値:
-  # function Test03 ([string]$arg01)
-  # {
-  #   # 引数に型を設定
-  #   $calc = $arg01 / 2
-  #   Write-Host "テスト関数02"
-  #   Write-Host $calc
-  #   Write-Host
-  # }
+  #   引数01:int16型
+  #   返り値:なし
+  function Test03 ([int16]$arg01)
+  {
+    Write-Host "テスト関数02_引数型設定"
+    Write-Host $arg01
+    Write-Host
+  }
+
+  # # テスト関数04
+  #   引数01:
+  #   引数02:
+  #   返り値:引数を計算した値
+  function Test04 ($arg01, $arg02)
+  {
+    # 値計算
+    $calc = $arg01 + $arg02
+    Write-Host "テスト関数04_返り値あり"
+    Write-Host "$arg01 + $arg02"
+
+    return $calc
+  }
+
+  # # テスト関数05
+  #   引数01:
+  #   引数02:
+  #   返り値:引数を計算した配列
+  function Test05 ($arg01, $arg02)
+  {
+    # 値計算
+    $plus = $arg01 + $arg02
+    $minus = $arg01 - $arg02
+    Write-Host "テスト関数05_複数返り値"
+    Write-Host "$arg01 + $arg02"
+    Write-Host "$arg01 - $arg02"
+
+    # 返却配列追加
+    $ret = @()
+    $ret += $plus
+    $ret += $minus
+
+    return $ret
+  }
 
 
 <# 関数使用 #>
@@ -63,6 +95,28 @@ Write-Host
   Test01 "def"
   # テスト関数02使用
   Test02 "ghi"
-  # # テスト関数03使用
-  # Test03 "4"
-  # Test03 4
+  # テスト関数03使用
+  try
+  {
+    Test03 "jkl"
+  }
+  catch
+  {
+    # エラー内容
+    Write-Host "エラーパターン"
+    Write-Host "※設定されている型に変換できない値を渡すとエラーとなる"
+    Write-Host "  $error[0]"
+    Write-Host
+  }
+  # テスト関数04使用
+  $test04Answer = Test04 1 2
+  Write-Host "テスト関数04の結果:$test04Answer"
+  Write-Host
+  # テスト関数05使用
+  $test05Answer = Test05 1 2
+  Write-Host "テスト関数05の結果:"
+  foreach ($x in $test05Answer)
+  {
+    Write-Host "  "$x
+  }
+  Write-Host
