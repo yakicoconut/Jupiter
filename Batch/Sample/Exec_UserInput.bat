f872339c616f768b1df558d467dc51e43bd7b34c
cal03 <ghi>
2020-03-20 17:16:05 +0900

    Batch_Sample_Exec_UserInput_表示改善

--- Batch/Sample/Exec_UserInput.bat
+++ Batch/Sample/Exec_UserInput.bat
@@ -9,7 +9,8 @@ echo ユーザ入力バッチの使用例
 
 
 : パターン1
-  echo --------------------------------
+  echo;
+  echo ================================
   echo 文字列入力例1_文字入力
   echo   設定
   echo     表示文言      :任意
@@ -19,12 +20,14 @@ echo ユーザ入力バッチの使用例
   echo     無入力  ⇒入力ループ
   echo     任意入力⇒戻値1:入力文字列
   echo               戻値2:1
+  echo --------------------------------
   call %call_UserInput% 文字入力パターン TRUE STR
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン2
-  echo --------------------------------
+  echo;
+  echo ================================
   echo 文字列入力例2_表示文言なし
   echo   設定
   echo     表示文言      :""
@@ -34,12 +37,14 @@ echo ユーザ入力バッチの使用例
   echo     無入力  ⇒入力ループ
   echo     任意入力⇒戻値1:入力文字列
   echo               戻値2:1
+  echo --------------------------------
   call %call_UserInput% "" TRUE STR
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン3
-  echo --------------------------------
+  echo;
+  echo ================================
   echo 文字列入力例3_文字入力 - 無効入力ループ無効
   echo   設定
   echo     表示文言      :任意
@@ -50,12 +55,14 @@ echo ユーザ入力バッチの使用例
   echo               戻値2:0
   echo     任意入力⇒戻値1:入力文字列
   echo               戻値2:1
+  echo --------------------------------
   call %call_UserInput% "文字入力 - 無効入力ループ無効パターン" FALSE STR
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン4
-  echo --------------------------------
+  echo;
+  echo ================================
   echo パス入力例1_パス入力
   echo   設定
   echo     表示文言      :任意
@@ -65,12 +72,14 @@ echo ユーザ入力バッチの使用例
   echo     無効入力⇒入力ループ
   echo     有効入力⇒戻値1:入力文字列
   echo               戻値2:1
+  echo --------------------------------
   call %call_UserInput% パス入力パターン TRUE PATH
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン5
-  echo --------------------------------
+  echo;
+  echo ================================
   echo パス入力例2_パス入力 - 無効入力ループ無効
   echo   設定
   echo     表示文言      :任意
@@ -81,12 +90,14 @@ echo ユーザ入力バッチの使用例
   echo               戻値2:0
   echo     有効入力⇒戻値1:入力文字列
   echo               戻値2:1
+  echo --------------------------------
   call %call_UserInput% "パス入力 - 無効入力ループ無効パターン" FALSE PATH
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン6
-  echo --------------------------------
+  echo;
+  echo ================================
   echo 数値入力例1_数値入力
   echo   設定
   echo     表示文言      :任意
@@ -96,12 +107,14 @@ echo ユーザ入力バッチの使用例
   echo     無効入力⇒入力ループ
   echo     有効入力⇒戻値1:入力文字列
   echo               戻値2:1
+  echo --------------------------------
   call %call_UserInput% 数値入力パターン TRUE NUM
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン7
-  echo --------------------------------
+  echo;
+  echo ================================
   echo 数値入力例2_数値入力 - 無効入力ループ無効
   echo   設定
   echo     表示文言      :任意
@@ -112,12 +125,14 @@ echo ユーザ入力バッチの使用例
   echo               戻値2:0
   echo     有効入力⇒戻値1:入力文字列
   echo               戻値2:1
+  echo --------------------------------
   call %call_UserInput% "数値入力 - 無効入力ループ無効パターン" FALSE NUM
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン8
-  echo --------------------------------
+  echo;
+  echo ================================
   echo 日付入力例1_日付入力
   echo   設定
   echo     表示文言      :任意
@@ -127,12 +142,14 @@ echo ユーザ入力バッチの使用例
   echo     無効入力⇒入力ループ
   echo     有効入力⇒戻値1:入力文字列
   echo               戻値2:有効パターン(yyyy/mm/dd、yy/mm/dd、mm/dd)
+  echo --------------------------------
   call %call_UserInput% 日付入力パターン TRUE DATE
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン9
-  echo --------------------------------
+  echo;
+  echo ================================
   echo 日付入力例2_日付入力 - 無効入力ループ無効
   echo   設定
   echo     表示文言      :任意
@@ -143,12 +160,14 @@ echo ユーザ入力バッチの使用例
   echo               戻値2:0
   echo     有効入力⇒戻値1:入力文字列
   echo               戻値2:有効パターン(yyyy/mm/dd、yy/mm/dd、mm/dd)
+  echo --------------------------------
   call %call_UserInput% "日付入力 - 無効入力ループ無効パターン" FALSE DATE
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン10
-  echo --------------------------------
+  echo;
+  echo ================================
   echo 時刻入力例1_時刻入力
   echo   設定
   echo     表示文言      :任意
@@ -158,12 +177,14 @@ echo ユーザ入力バッチの使用例
   echo     無効入力⇒入力ループ
   echo     有効入力⇒戻値1:入力文字列
   echo               戻値2:有効パターン(mm:ss、h:mm:ss、hh:mm:ss、hh:mm:ss.ff)
+  echo --------------------------------
   call %call_UserInput% 時刻入力パターン TRUE TIME
   echo %return_UserInput1%
   echo %return_UserInput2%
 
 : パターン11
-  echo --------------------------------
+  echo;
+  echo ================================
   echo 時刻入力例2_時刻入力 - 無効入力ループ無効
   echo   設定
   echo     表示文言      :任意
@@ -174,6 +195,7 @@ echo ユーザ入力バッチの使用例
   echo               戻値2:0
   echo     有効入力⇒戻値1:入力文字列
   echo               戻値2:有効パターン(mm:ss、h:mm:ss、hh:mm:ss、hh:mm:ss.ff)
+  echo --------------------------------
   call %call_UserInput% "時刻入力 - 無効入力ループ無効パターン" FALSE TIME
   echo %return_UserInput1%
   echo %return_UserInput2%
