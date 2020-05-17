ff3128adec512a0917fda7406daee0d4224040f8
cal01 <abc@def.com>
2020-05-18 01:16:15 +0900

    TypeScript_Function_ScriptLoad_追加

---
+++ TypeScript/Function/ScriptLoad/TSA/app.ts
@@ -0,0 +1,18 @@
+﻿/*
+ * html内ロード順
+ * 【JavaScript入門】onloadイベントの使い方とハマりやすい注意点とは | 侍エンジニア塾ブログ（Samurai Blog） - プログラミング入門者向けサイト
+ * https://www.sejuku.net/blog/19754
+ * ・タグ内容は上から順にロードされる
+ * ・headはbodyの前にロードされる
+ * ・scriptはソースファイルの指定と
+ * 直接記述が可能
+ */
+
+
+/*
+ * ボタンクリック関数
+ */
+function button1_click() {
+    // ウィンドウ表示
+    alert("HelloWorld3");
+}
---
+++ TypeScript/Function/ScriptLoad/TSA/app2.ts
@@ -0,0 +1,16 @@
+﻿// 「app.ts」で実装している関数と同名のためビルドエラー
+///*
+// * ボタンクリック関数
+// */
+//function button1_click() {
+//    // ウィンドウ表示
+//    alert("HelloWorld4");
+//}
+
+/*
+ * ボタンクリック関数
+ */
+function button2_click() {
+    // ウィンドウ表示
+    alert("HelloWorld5");
+}
