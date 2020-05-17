7017ed2156554669f20ea0f4aee7e12d58ec8e15
cal01 <abc@def.com>
2020-05-18 01:10:56 +0900

    TypeScript_Function_Class_追加

---
+++ TypeScript/Function/Class/TSA/app.js
@@ -0,0 +1,16 @@
+/*
+ * クラスサンプル
+ */
+var SamnpleCalass = /** @class */ (function () {
+    function SamnpleCalass() {
+    }
+    return SamnpleCalass;
+}());
+/*
+ * ボタンクリック関数
+ */
+function button1_click() {
+    // ウィンドウ表示
+    alert("HelloWorld");
+}
+//# sourceMappingURL=app.js.map
---
+++ TypeScript/Function/Class/TSA/app.ts
@@ -0,0 +1,30 @@
+﻿/*
+ * クラスサンプル
+ */
+class SamnpleCalass {
+    // コンストラクタは省略可
+    ///*
+    // * コンストラクタ
+    // * @param displayText
+    // *   表示文字列
+    // */
+    //constructor() { }
+
+    // クラス内にfunctionは不可
+    ///*
+    // * ボタンクリック関数
+    // */
+    //function button1_click() {
+    //    // ウィンドウ表示
+    //    alert("HelloWorld");
+    //}
+}
+
+
+/*
+ * ボタンクリック関数
+ */
+function button1_click() {
+    // ウィンドウ表示
+    alert("HelloWorld");
+}
