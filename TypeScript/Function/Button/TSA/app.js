cec3d8a251110c7fe23433f8273458576328d2ac
cal01 <abc@def.com>
2020-05-18 01:10:26 +0900

    TypeScript_Function_Button_追加

---
+++ TypeScript/Function/Button/TSA/app.js
@@ -0,0 +1,23 @@
+/*
+ * HelloWorldクラス
+ *   TypeScript HelloWorld - Qiita
+ *    https://qiita.com/Chrowa3/items/92833a36a12f3d0450db
+ */
+var Button = /** @class */ (function () {
+    /*
+     * コンストラクタ
+     * @param displayText
+     *   表示文字列
+     */
+    function Button() {
+    }
+    return Button;
+}());
+/*
+ * ボタンクリック関数
+ */
+function button1_click() {
+    // ウィンドウ表示
+    alert("HelloWorld");
+}
+//# sourceMappingURL=app.js.map
---
+++ TypeScript/Function/Button/TSA/app.ts
@@ -0,0 +1,22 @@
+﻿/*
+ * HelloWorldクラス
+ *   TypeScript HelloWorld - Qiita
+ *    https://qiita.com/Chrowa3/items/92833a36a12f3d0450db
+ */
+class Button {
+    /*
+     * コンストラクタ
+     * @param displayText
+     *   表示文字列
+     */
+    constructor() { }
+}
+
+
+/*
+ * ボタンクリック関数
+ */
+function button1_click() {
+    // ウィンドウ表示
+    alert("HelloWorld");
+}
