d4f8ccd23ffd8762806ace722ada25ef1aac68af
cal01 <abc@def.com>
2020-05-18 01:20:45 +0900

    TypeScript_LogicTemp_TimeGet_追加

---
+++ TypeScript/LogicTemp/TimeGet/TSA/app.js
@@ -0,0 +1,14 @@
+/*
+ * ボタンクリック関数
+ */
+function button1_click() {
+    // 現在時刻取得
+    var now = new Date();
+    // 時分秒変数設定
+    var hour = now.getHours();
+    var min = now.getMinutes();
+    var sec = now.getSeconds();
+    // ウィンドウ表示
+    alert(hour + ":" + min + ":" + sec);
+}
+//# sourceMappingURL=app.js.map
