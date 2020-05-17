63158a8d2b2b2953b8ca2004fee2f0cd76680492
cal01 <abc@def.com>
2020-05-18 01:14:45 +0900

    TypeScript_Function_OnLoad_追加

---
+++ TypeScript/Function/OnLoad/TSA/app.ts
@@ -0,0 +1,18 @@
+﻿/*
+ * 【JavaScript入門】onloadイベントの使い方とハマりやすい注意点とは | 侍エンジニア塾ブログ（Samurai Blog） - プログラミング入門者向けサイト
+ *  https://www.sejuku.net/blog/19754
+ * 読み込み時の処理！JavaScriptでonloadを使う方法 | TechAcademyマガジン
+ *  https://techacademy.jp/magazine/15558
+-->
+
+
+/*
+ * onload関数
+ */
+window.onload = () => {
+    // HTML内の「DateTimeDisp」ID取得
+    var target = document.getElementById("Disp");
+
+    // 文字列表示
+    target.innerHTML = "テスト";
+}
