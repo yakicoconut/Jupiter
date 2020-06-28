540728dac64837ddd380eacda8b258f310e3edd1
cal01 <abc@def.com>
2020-06-28 19:19:46 +0900

    Batch_ExecFfmpeg_ImgCompo_修正_引数対応

--- Batch/Create/ExecFfmpeg/ImgCompo.bat
+++ Batch/Create/ExecFfmpeg/ImgCompo.bat
@@ -7,25 +7,53 @@ echo ffmpegで動画に画像を合成
 
 : 参照バッチ
   rem ユーザ入力バッチ
-  set call_UserInput="..\..\OwnLib\UserInput.bat"
+  set call_UserInput=%~dp0"..\..\OwnLib\UserInput.bat"
   rem 経過時間計算バッチ
-  set call_ElapsedTime="..\..\OwnLib\ElapsedTime.bat"
+  set call_ElapsedTime=%~dp0"..\..\OwnLib\ElapsedTime.bat"
+  rem 引数型判定バッチ
+  set call_ChkArgDataType=%~dp0"..\..\OwnLib\ChkArgDataType.bat"
 
 
-: ユーザ入力処理
-  : 背景動画ファイルパス
+: 引数チェック
+  rem 引数カウント
+  set argc=0
+  for %%a in ( %* ) do set /a argc+=1
+
+  rem 引数がない場合、ユーザ入力へ
+  if %argc%==0 goto :USER_INPUT
+  rem 引数が定義通りの場合、引数判定へ
+  if %argc%==6 goto :CHK_ARG
+
+  echo 引数の数が定義と異なるため、終了します
+  echo 引数:%argc%
+  echo 定義:6
+  pause
+  exit /b
+
+
+rem ユーザ入力処理
+:USER_INPUT
+  : 対象ファイルパス
     echo;
     rem ユーザ入力バッチ使用
-    call %call_UserInput% 背景動画ファイルパス入力 TRUE PATH
+    call %call_UserInput% 対象ファイルパス入力 TRUE PATH
     rem 入力値引継ぎ
-    set sourcePath=%return_UserInput1%
+    set srcPath=%return_UserInput1%
 
-  : 合成動画ファイルパス
+  : 合成ファイルパス
     echo;
     rem ユーザ入力バッチ使用
-    call %call_UserInput% 合成動画ファイルパス入力 TRUE PATH
+    call %call_UserInput% 合成ファイルパス入力 TRUE PATH
     rem 入力値引継ぎ
     set compPath=%return_UserInput1%
+
+  : コーデック
+    echo;
+    echo コーデック入力(-c:v 動画Codec -c:a 音声Codec)
+    rem ユーザ入力バッチ使用
+    call %call_UserInput% "" FALSE STR
+    rem 入力値引継ぎ
+    set codec=%return_UserInput1:"=%
 
   : 1秒あたり何枚
     echo;
@@ -50,54 +78,61 @@ echo ffmpegで動画に画像を合成
     rem 入力値引継ぎ
     set outPath=%return_UserInput1%
 
-
-: 画像合成実行
-  : 
-  : 
-  : 
-  : 
-  : 
-  : 
-  ffmpeg\win32\ffmpeg.exe -i %sourcePath% -i %compPath% -filter_complex overlay %codec% -r %rate% -video_track_timescale %tbn% %outPath%
-  pause
+    rem 本処理へ
+    goto :RUN
 
 
-exit
+rem 引数判定
+:CHK_ARG
+  rem 引数型判定バッチ使用
+  call %call_ChkArgDataType% "PATH PATH STR NUM NUM STR" %1 %2 %3 %4 %5 %6
+  rem 判定結果が失敗の場合、終了へ
+  if %ret_ChkArgDataType1%==0 goto :EOF
+
+  : 引数引継ぎ
+    set  srcPath=%1
+    set compPath=%2
+    set    codec=%3
+    set     rate=%4
+    set      tbn=%5
+    set  outPath=%6
 
 
-rem 時刻解体サブルーチン
-:DISMANTLE_TIME
-SETLOCAL
-  : 引数
-    rem 入力時刻
-    set inpTime=%1
-    rem 時刻フォーマット
-    set format=%2
+rem 本処理
+:RUN
+  : 実行
+    rem ログフォルダ作成
+    if not exist %~dp0Log ( mkdir %~dp0Log )
+    rem ファイル名でログファイルパス設定
+    set logPath=%~dp0Log\%~n0.log
+    rem 実行前ログ出力
+    echo %date% %time%>>%logPath%
+    echo;>>%logPath%
 
-  : ミリ秒が存在する場合
-    if %format:~-2,2%==.f (
-      set milli=%inpTime:~-3,2%
-    )
-    if %format:~-3,3%==.ff (
-      set milli=%inpTime:~-4,3%
-    )
-    if %format:~-4,4%==.fff (
-      set milli=%inpTime:~-5,4%
-    )
+    rem 画像合成実行
+      : -y      :上書き
+      : -i      :対象ファイル
+      : -filter~:overlay
+      :            
+      : -c:v    :動画コーデック
+      : -c:a    :音声コーデック
+      : -r      :フレームレート
+      : -video~ :tbn設定
+    %~dp0ffmpeg\win32\ffmpeg.exe -y -i %srcPath% -i %compPath% -filter_complex overlay %codec:"=% -r %rate% -video_track_timescale %tbn% %outPath%
 
-  : 先頭「m」、「h」、「hh」判断
-    if %format:~0,2%==mm (
-      set tms="00:%inpTime:~1,5%"
-    )
-    if %format:~0,2%==h: (
-      set tms="0%inpTime:~1,7%"
-    )
-    if %format:~0,3%==hh: (
-      rem 「hh」の場合もミリ秒を抜く
-      set tms="%inpTime:~1,8%"
-    )
 
-rem 返り値1:時分秒
-rem 返り値2:ミリ秒
-ENDLOCAL && set ret_DISMANTLE_TIME01=%tms%&& set ret_DISMANTLE_TIME02=%milli%
-exit /b
+:END
+  rem ログ出力
+  echo %srcPath:"=%>>%logPath%
+  echo %compPath:"=%>>%logPath%
+  echo %codec:"=%>>%logPath%
+  echo %rate:"=%>>%logPath%
+  echo %tbn:"=%>>%logPath%
+  echo %outPath:"=%>>%logPath%
+  echo;>>%logPath%
+  echo %date% %time%>>%logPath%
+  echo;>>%logPath%
+  echo;>>%logPath%
+
+  rem 引数がない(ユーザ入力で実行した)場合、ポーズ
+  if %argc%==0 pause
