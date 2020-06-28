ebcd37c3706aaf4cf46a82be088feda48c9a0cc7
cal01 <abc@def.com>
2020-06-28 19:20:31 +0900

    Batch_ExecFfmpeg_ImgToVideo_修正_引数対応

--- Batch/Create/ExecFfmpeg/ImgToVideo.bat
+++ Batch/Create/ExecFfmpeg/ImgToVideo.bat
@@ -3,22 +3,43 @@ title %~nx0
 echo ffmpegで画像から動画生成
 : FFMPEGで1枚の静止画から動画を作成する | 技術的特異点
 : 	http://tecsingularity.com/ffmpeg/make-movie/
-
+: 動画操作ソフト ffmpeg のエラー "width not divisible by 2" への対処 - Qiita
+: 	https://qiita.com/genchi-jin/items/90078b6ec751fdacbc9e
 
 : 参照バッチ
   rem ユーザ入力バッチ
-  set call_UserInput="..\..\OwnLib\UserInput.bat"
+  set call_UserInput=%~dp0"..\..\OwnLib\UserInput.bat"
   rem 経過時間計算バッチ
-  set call_ElapsedTime="..\..\OwnLib\ElapsedTime.bat"
+  set call_ElapsedTime=%~dp0"..\..\OwnLib\ElapsedTime.bat"
+  rem 引数型判定バッチ
+  set call_ChkArgDataType=%~dp0"..\..\OwnLib\ChkArgDataType.bat"
 
 
-: ユーザ入力処理
-  : 対象画像ファイルパス
+: 引数チェック
+  rem 引数カウント
+  set argc=0
+  for %%a in ( %* ) do set /a argc+=1
+
+  rem 引数がない場合、ユーザ入力へ
+  if %argc%==0 goto :USER_INPUT
+  rem 引数が定義通りの場合、引数判定へ
+  if %argc%==6 goto :CHK_ARG
+
+  echo 引数の数が定義と異なるため、終了します
+  echo 引数:%argc%
+  echo 定義:6
+  pause
+  exit /b
+
+
+rem ユーザ入力処理
+:USER_INPUT
+  : 対象ファイルパス
     echo;
     rem ユーザ入力バッチ使用
-    call %call_UserInput% 対象画像ファイルパス入力 TRUE PATH
+    call %call_UserInput% 対象ファイルパス入力 TRUE PATH
     rem 入力値引継ぎ
-    set sourcePath=%return_UserInput1%
+    set srcPath=%return_UserInput1%
 
   : 動画時間
     echo;
@@ -27,13 +48,16 @@ echo ffmpegで画像から動画生成
     rem ユーザ入力バッチ使用
     call %call_UserInput% "" TRUE TIME
     rem 入力値引継ぎ
-    set start=%return_UserInput1%
-    set targetTimeFormat=%return_UserInput2%
+    set movieTime=%return_UserInput1%
+    set movieTimeFmt=%return_UserInput2%
 
-    rem 時刻解体サブルーチン使用
-    call :DISMANTLE_TIME %start% %targetTimeFormat%
-    set start=%ret_DISMANTLE_TIME01:"=%
-    set startMilli=%ret_DISMANTLE_TIME02%
+  : コーデック
+    echo;
+    echo コーデック入力(-c:v 動画Codec -c:a 音声Codec)
+    rem ユーザ入力バッチ使用
+    call %call_UserInput% "" FALSE STR
+    rem 入力値引継ぎ
+    set codec=%return_UserInput1:"=%
 
   : 1秒あたり何枚
     echo;
@@ -58,12 +82,43 @@ echo ffmpegで画像から動画生成
     rem 入力値引継ぎ
     set outPath=%return_UserInput1%
 
+    rem 本処理へ
+    goto :RUN
 
-: 開始時間秒数変換
+
+rem 引数判定
+:CHK_ARG
+  rem 引数型判定バッチ使用
+  call %call_ChkArgDataType% "PATH TIME STR NUM NUM STR" %1 %2 %3 %4 %5 %6
+  rem 判定結果が失敗の場合、終了
+  if %ret_ChkArgDataType1%==0 goto :EOF
+  rem 型判定結果引継ぎ
+  for /f "tokens=2,3" %%a in (%ret_ChkArgDataType2%) do (
+    rem 時刻フォーマット取得
+    set movieTimeFmt=%%a
+  )
+
+  : 引数引継ぎ
+    set   srcPath=%1
+    set movieTime="%2"
+    set     codec=%3
+    set      rate=%4
+    set       tbn=%5
+    set   outPath=%6
+
+
+rem 本処理
+:RUN
+  : 動画時間秒数変換
+    rem 時刻解体サブルーチン使用
+    call :DISMANTLE_TIME %movieTime% %movieTimeFmt%
+    set movieTime=%ret_DISMANTLE_TIME01:"=%
+    set movieTimeMilli=%ret_DISMANTLE_TIME02%
+
     rem 文字列として分割
-    set   strHour=%start:~0,2%
-    set strMinute=%start:~3,2%
-    set strSecond=%start:~6,2%
+    set   strHour=%movieTime:~0,2%
+    set strMinute=%movieTime:~3,2%
+    set strSecond=%movieTime:~6,2%
 
     rem 二桁目が「0」の場合
     if %strHour:~0,1%==0 (
@@ -83,23 +138,52 @@ echo ffmpegで画像から動画生成
     set /a second=%strSecond%
 
     rem 秒数変換
-    set /a   secHour=%hour%*3600
-    set /a secMinute=%minute%*60
-    set /a  startSec=%secHour%+%secMinute%+%second%
+    set /a      secHour=%hour%*3600
+    set /a    secMinute=%minute%*60
+    set /a movieTimeSec=%secHour%+%secMinute%+%second%
+
+  : 実行
+    rem ログフォルダ作成
+    if not exist %~dp0Log ( mkdir %~dp0Log )
+    rem ファイル名でログファイルパス設定
+    set logPath=%~dp0Log\%~n0.log
+    rem 実行前ログ出力
+    echo %date% %time%>>%logPath%
+    echo;>>%logPath%
+
+    rem 画像取得実行
+      : -y      :上書き
+      : -loop   :1
+      :            
+      : -i      :対象ファイル
+      : -c:v    :動画コーデック(「h264」指定)
+      : -pix_fmt:yuv420p
+      :            
+      : ^vf     :"scale=trunc(iw/2)*2:trunc(ih/2)*2"
+      :            
+      : -t      :対象期間(秒)
+      : -r      :フレームレート
+      : -video~ :tbn設定
+    %~dp0ffmpeg\win32\ffmpeg.exe -y -loop 1 -i %srcPath% -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -t %movieTimeSec%%movieTimeMilli% %codec:"=% -r %rate% -video_track_timescale %tbn% %outPath%
 
 
-: 画像取得実行
-  : -i :動画指定
-  : -ss:開始位置(秒)
-  : -t :対象期間(秒)
-  : -r :1秒あたり何枚抜き出すか
-  :     fps(フレームレート)の確認は「ffprobe.exe 対象動画」
-  : -f :「image2 %%06d.jpg」指定で「000001.jpg」から連番出力指定
-  ffmpeg\win32\ffmpeg.exe -loop 1 -i %sourcePath% -vcodec h264 -pix_fmt yuv420p -t %startSec%%startMilli% -r %rate% -video_track_timescale %tbn% %outPath%
-  pause
+:END
+  rem ログ出力
+  echo %srcPath:"=%>>%logPath%
+  echo %movieTime:"=%%movieTimeMilli%>>%logPath%
+  echo %codec:"=%>>%logPath%
+  echo %rate:"=%>>%logPath%
+  echo %tbn:"=%>>%logPath%
+  echo %outPath:"=%>>%logPath%
+  echo;>>%logPath%
+  echo %date% %time%>>%logPath%
+  echo;>>%logPath%
+  echo;>>%logPath%
 
+  rem 引数がない(ユーザ入力で実行した)場合、ポーズ
+  if %argc%==0 pause
 
-exit
+exit /b
 
 
 rem 時刻解体サブルーチン
