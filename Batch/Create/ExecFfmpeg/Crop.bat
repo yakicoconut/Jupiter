e046fb667ead17ae1788b23e3e44edd9bc651430
cal01 <abc@def.com>
2020-06-28 19:17:47 +0900

    Batch_ExecFfmpeg_Crop_修正_引数対応

--- Batch/Create/ExecFfmpeg/Crop.bat
+++ Batch/Create/ExecFfmpeg/Crop.bat
@@ -7,24 +7,44 @@ echo ffmpegで画面サイズ変更
 
 : 参照バッチ
   rem ユーザ入力バッチ
-  set call_UserInput="..\..\OwnLib\UserInput.bat"
+  set call_UserInput=%~dp0"..\..\OwnLib\UserInput.bat"
   rem 動画情報取得バッチ
-  set call_GetMpegInfo="GetMpegInfo.bat"
+  set call_GetMpegInfo=%~dp0"GetMpegInfo.bat"
+  rem 引数型判定バッチ
+  set call_ChkArgDataType=%~dp0"..\..\OwnLib\ChkArgDataType.bat"
 
 
-: ユーザ入力処理
+: 引数チェック
+  rem 引数カウント
+  set argc=0
+  for %%a in ( %* ) do set /a argc+=1
+
+  rem 引数がない場合、ユーザ入力へ
+  if %argc%==0 goto :USER_INPUT
+  rem 引数が定義通りの場合、引数判定へ
+  if %argc%==6 goto :CHK_ARG
+
+  echo 引数の数が定義と異なるため、終了します
+  echo 引数:%argc%
+  echo 定義:6
+  pause
+  exit /b
+
+
+rem ユーザ入力処理
+:USER_INPUT
   : 対象ファイルパス
     echo;
     rem ユーザ入力バッチ使用
     call %call_UserInput% 対象ファイルパス入力 TRUE PATH
     rem 入力値引継ぎ
-    set sourcePath=%return_UserInput1%
+    set srcPath=%return_UserInput1%
 
     echo;
     echo 対象画面サイズ
     rem 動画情報取得バッチ使用
     rem 画面サイズのみ取得オプション
-    call %call_GetMpegInfo% "-v error -select_streams v:0 -show_entries stream=height,width -of csv=s=x:p=0" %sourcePath%
+    call %call_GetMpegInfo% "-v error -select_streams v:0 -show_entries stream=height,width -of csv=s=x:p=0" %srcPath%
 
   : クロップ指定入力
     echo;
@@ -35,6 +55,29 @@ echo ffmpegで画面サイズ変更
     rem 入力値引継ぎ
     set crop=%return_UserInput1%
 
+  : コーデック
+    echo;
+    echo コーデック入力(-c:v 動画Codec -c:a 音声Codec)
+    rem ユーザ入力バッチ使用
+    call %call_UserInput% "" FALSE STR
+    rem 入力値引継ぎ
+    set codec=%return_UserInput1:"=%
+
+  : 1秒あたり何枚
+    echo;
+    rem ユーザ入力バッチ使用
+    call %call_UserInput% 1秒あたり枚数入力 TRUE NUM
+    rem 入力値引継ぎ
+    set rate=%return_UserInput1%
+
+  : tbn入力
+    echo;
+    echo tbn入力(数値)
+    rem ユーザ入力バッチ使用
+    call %call_UserInput% "" TRUE NUM
+    rem 入力値引継ぎ
+    set tbn=%return_UserInput1%
+
   : 出力ファイル名
     echo;
     echo 出力ファイル名入力(要拡張子)
@@ -43,13 +86,65 @@ echo ffmpegで画面サイズ変更
     rem 入力値引継ぎ
     set outPath=%return_UserInput1%
 
-: 実行
-  rem 音量調整
-  : -i                :元ファイル
-  : -vf crop=a:b:c:d  :クロップ設定
-  :                    a:出力Xサイズ
-  :                    b:出力Yサイズ
-  :                    c:対象Xオフセット
-  :                    d:対象Yオフセット
-  ffmpeg\win32\ffmpeg.exe -i %sourcePath% -vf crop=%crop% %outPath%
-  pause
+    rem 本処理へ
+    goto :RUN
+
+
+rem 引数判定
+:CHK_ARG
+  rem 引数型判定バッチ使用
+  call %call_ChkArgDataType% "PATH STR STR NUM NUM STR" %1 %2 %3 %4 %5 %6
+  rem 判定結果が失敗の場合、終了へ
+  if %ret_ChkArgDataType1%==0 goto :EOF
+
+  : 引数引継ぎ
+    set srcPath=%1
+    set    crop=%2
+    set   codec=%3
+    set    rate=%4
+    set     tbn=%5
+    set outPath=%6
+
+
+rem 本処理
+:RUN
+  : 実行
+    rem ログフォルダ作成
+    if not exist %~dp0Log ( mkdir %~dp0Log )
+    rem ファイル名でログファイルパス設定
+    set logPath=%~dp0Log\%~n0.log
+    rem 実行前ログ出力
+    echo %date% %time%>>%logPath%
+    echo;>>%logPath%
+
+    rem クロップ実行
+      : -y     :上書き
+      : -i     :対象ファイル
+      : -vf    :crop=a:b:c:d
+      :           クロップ設定
+      :           a:出力Xサイズ
+      :           b:出力Yサイズ
+      :           c:対象Xオフセット
+      :           d:対象Yオフセット
+      : -c:v   :動画コーデック
+      : -c:a   :音声コーデック
+      : -r     :フレームレート
+      : -video~:tbn設定
+   %~dp0ffmpeg\win32\ffmpeg.exe -y -i %srcPath% -vf crop=%crop% %codec:"=% -r %rate% -video_track_timescale %tbn% %outPath%
+
+
+:END
+  rem ログ出力
+  echo %srcPath:"=%>>%logPath%
+  echo %crop:"=%>>%logPath%
+  echo %codec:"=%>>%logPath%
+  echo %rate:"=%>>%logPath%
+  echo %tbn:"=%>>%logPath%
+  echo %outPath:"=%>>%logPath%
+  echo;>>%logPath%
+  echo %date% %time%>>%logPath%
+  echo;>>%logPath%
+  echo;>>%logPath%
+
+  rem 引数がない(ユーザ入力で実行した)場合、ポーズ
+  if %argc%==0 pause
