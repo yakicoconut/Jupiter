b335fa339f623b764dafa6d61f2c49e5a30d11c0
cal01 <abc@def.com>
2020-07-05 16:10:41 +0900

    Batch_Sample_Exec_ElapsedTime_修正_コンマ三桁差分パターン追加

--- Batch/Sample/Exec_ElapsedTime.bat
+++ Batch/Sample/Exec_ElapsedTime.bat
@@ -20,7 +20,6 @@ echo 経過時間計算バッチの使用例
   echo   %return_ElapsedTime%
   echo   00:11:19 ←期待結果
 
-
 : 差分計算_「h:mm:ss」パターン
   echo;
   set beforeTime=6:10:50
@@ -69,7 +68,6 @@ echo 経過時間計算バッチの使用例
   echo   %return_ElapsedTime%
   echo   08:30:46.430 ←期待結果
 
-
 : 差分計算_コンマ秒パターン
   echo;
   set beforeTime=01:59:19.40
@@ -82,8 +80,7 @@ echo 経過時間計算バッチの使用例
   echo   %return_ElapsedTime%
   echo   00:00:40.99 ←期待結果
 
-
-: 差分計算パターン
+: 差分計算_「hh:mm:ss」パターン
   echo;
   set beforeTime=00:13:30
   set  afterTime=00:14:00
@@ -94,6 +91,18 @@ echo 経過時間計算バッチの使用例
   echo   ------------
   echo   %return_ElapsedTime%
   echo   00:00:30 ←期待結果
+
+: 差分計算_コンマ三桁差分パターン
+  echo;
+  set beforeTime=00:00:34.600
+  set  afterTime=00:00:41.607
+  rem 経過時間計算バッチ使用
+  call %call_ElapsedTime% %beforeTime% %afterTime%
+  echo   %beforeTime%
+  echo - %afterTime%
+  echo   ------------
+  echo   %return_ElapsedTime%
+  echo   00:00:07.007 ←期待結果
 
 
 pause
