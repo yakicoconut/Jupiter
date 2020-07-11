94a8e27d9902259ca271fe25f6459365c82c589b
cal01 <abc@def.com>
2020-07-11 16:10:08 +0900

    PowerShell_LogicTemp_Xml_GetXml_NS_修正_リファクタリング

--- PowerShell/LogicTemp/Xml/GetXml_NS.ps1
+++ PowerShell/LogicTemp/Xml/GetXml_NS.ps1
@@ -5,31 +5,44 @@ echo XML取得_名前空間あり
 <# 設定 #>
   # 対象XMLパス
   $tgtXmlPath = ".\TestXml\TestXml02.xml"
+  # エンコ
+  $enc = "UTF8"
   # 名前空間変数設定
   $nsAlias = "ns"
   # 名前空間指定
   $nameSpace = "http://www.zzz.com/yyy/xxx.xsd"
+  # XPath指定
+  $xPath = "settings"
 
 
 <# Xml取得 #>
-  # XML読み込み
-  $tgtXml = [xml](Get-Content $tgtXmlPath)
+  # # XML読み込み
+    $tgtXml = [xml](Get-Content -Encoding $enc $tgtXmlPath)
 
-  # 名前空間オブジェクト生成
-  $ns = New-Object Xml.XmlNamespaceManager $tgtXml.NameTable
-  $ns.AddNamespace($nsAlias, $nameSpace)
+    # 名前空間オブジェクト生成
+    $ns = New-Object Xml.XmlNamespaceManager $tgtXml.NameTable
+    $ns.AddNamespace($nsAlias, $nameSpace)
 
-  # 要素名をxPathで検索
-  $xmlRoot = $tgtXml.SelectNodes("$($nsAlias):settings", $ns)
-  # # 別解
-  # $xmlRoot = $tgtXml.SelectNodes("$nsAlias" + ":settings", $ns)
+  # # ルート要素取得
+    # 要素名をxPathで検索
+    $tgtRoot = $tgtXml.SelectNodes("$nsAlias" + ":" + $xPath, $ns)
+    # # 別解
+    # $tgtRoot = $tgtXml.SelectNodes("$($nsAlias):settings", $ns)
+    # ねずみ返し
+    if ($tgtRoot -eq $null)
+    {
+      Write-Host 指定したXMLルートが取得できていません
+      Write-Host 終了します
+      return
+    }
 
-  # setting要素取得
-  $rootSetting = $xmlRoot.setting
-  # key属性取得
-  $rootSettingKey = $rootSetting.key
-  # value属性取得
-  $rootSettingValue = $rootSetting.value
+  # # 要素取得
+    # setting要素取得
+    $rootSetting = $tgtRoot.setting
+    # key属性取得
+    $rootSettingKey = $rootSetting.key
+    # value属性取得
+    $rootSettingValue = $rootSetting.value
 
 
 <# 値表示 #>
