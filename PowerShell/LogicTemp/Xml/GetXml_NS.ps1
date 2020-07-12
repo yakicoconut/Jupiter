a9ef58d2126347d6e5d9ae3cdd63283f3b6a10ec
cal01 <abc@def.com>
2020-07-13 00:49:55 +0900

    PowerShell_LogicTemp_Xml_GetXml_NS_修正_名前空間対応

--- PowerShell/LogicTemp/Xml/GetXml_NS.ps1
+++ PowerShell/LogicTemp/Xml/GetXml_NS.ps1
@@ -3,48 +3,62 @@ echo XML取得_名前空間あり
 
 
 <# 設定 #>
-  # 対象XMLパス
-  $tgtXmlPath = ".\TestXml\TestXml02.xml"
-  # エンコ
-  $enc = "UTF8"
-  # 名前空間変数設定
-  $nsAlias = "ns"
-  # 名前空間指定
-  $nameSpace = "http://www.zzz.com/yyy/xxx.xsd"
-  # XPath指定
-  $xPath = "settings"
+  # # 共通設定
+    # エンコ
+    $enc = "UTF8"
+    # 名前空間変数設定
+    $nsAlias = "ns"
+
+  # # エイリアスあり名前空間
+    # 対象XMLパス
+    $tgtXmlPath1 = ".\TestXml\TestXml04.xml"
+    # 名前空間指定
+    $nameSpace1 = "uri:namespace_uri"
+    # XPath指定
+    $xPath1 = "//$($nsAlias):book/$($nsAlias):title"
+    $xPath2 = "//book/title"
+
+  # # xmlns名前空間
+    # 対象XMLパス
+    $tgtXmlPath2 = ".\TestXml\TestXml02.xml"
+    # 名前空間指定
+    $nameSpace2 = "http://www.zzz.com/yyy/xxx.xsd"
+    # XPath指定
+    $xPath3 = "//$($nsAlias):settings"
+    $xPath4 = "//$($nsAlias):settings/$($nsAlias):setting"
 
 
 <# Xml取得 #>
-  # # XML読み込み
-    $tgtXml = [xml](Get-Content -Encoding $enc $tgtXmlPath)
+  # # エイリアスあり名前空間
+    $tgtXml1 = [xml](Get-Content -Encoding $enc $tgtXmlPath1)
 
     # 名前空間オブジェクト生成
-    $ns = New-Object Xml.XmlNamespaceManager $tgtXml.NameTable
-    $ns.AddNamespace($nsAlias, $nameSpace)
+    $ns1 = New-Object Xml.XmlNamespaceManager $tgtXml1.NameTable
+    $ns1.AddNamespace($nsAlias, $nameSpace1)
 
-  # # ルート要素取得
-    # 要素名をxPathで検索
-    $tgtRoot = $tgtXml.SelectNodes("$nsAlias" + ":" + $xPath, $ns)
-    # # 別解
-    # $tgtRoot = $tgtXml.SelectNodes("$($nsAlias):settings", $ns)
-    # ねずみ返し
-    if ($tgtRoot -eq $null)
-    {
-      Write-Host 指定したXMLルートが取得できていません
-      Write-Host 終了します
-      return
-    }
+    # XPathNavigatorオブジェクト生成
+    $xNavi1 = $tgtXml1.CreateNavigator()
 
-  # # 要素取得
-    # setting要素取得
-    $rootSetting = $tgtRoot.setting
-    # key属性取得
-    $rootSettingKey = $rootSetting.key
-    # value属性取得
-    $rootSettingValue = $rootSetting.value
+    # 名前空間要素XPath使用
+    $xml1 = $xNavi1.Select($xPath1, $ns1)
+    Write-Host "###########################"
+    $xml1
 
+    # 名前空間でない要素
+    $xml2 = $xNavi1.Select($xPath2, $ns1)
+    Write-Host "###########################"
+    $xml2
 
-<# 値表示 #>
-  Write-Host $rootSettingKey[1]
-  Write-Host $rootSettingValue[2]
+  # # xmlns名前空間
+    $tgtXml2 = [xml](Get-Content -Encoding $enc $tgtXmlPath2)
+    $ns2 = New-Object Xml.XmlNamespaceManager $tgtXml2.NameTable
+    $ns2.AddNamespace($nsAlias, $nameSpace2)
+    $xNavi2 = $tgtXml2.CreateNavigator()
+
+    $xml3 = $xNavi2.Select($xPath3, $ns2)
+    Write-Host "###########################"
+    $xml3
+
+    $xml4 = $xNavi2.Select($xPath4, $ns2)
+    Write-Host "###########################"
+    $xml4
