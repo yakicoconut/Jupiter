3130ef3f1e8ddabc459f9bbeb25f98c9314fedea
cal03 <ghi>
2020-08-03 17:52:09 +0900

    PowerShell_POWERSHELL_Kick_Input_修正_引数対応

--- PowerShell/Create/POWERSHELL_Kick_Input.bat
+++ PowerShell/Create/POWERSHELL_Kick_Input.bat
@@ -7,12 +7,43 @@ rem 遅延環境変数オフ
 SETLOCAL DISABLEDELAYEDEXPANSION
 
 
-: 対象ファイル入力
+: 引数チェック
+  rem 引数カウント
+  set argc=0
+  for %%a in ( %* ) do set /a argc+=1
+
+  rem 引数がない場合、ユーザ入力へ
+  if %argc%==0 goto :USER_INPUT
+  rem 引数が定義通りの場合、引数判定へ
+  if %argc%==1 goto :CHK_ARG
+
+  echo 引数の数が定義と異なるため、終了します
+  echo 引数:%argc%
+  echo 定義:1
+  pause
+  exit /b
+
+
+rem ユーザ入力処理
+:USER_INPUT
   echo 対象ファイルを指定してください
   set /P USR="入力してください:"
-  set pS1=%USR%
+  set tgtPath="%USR:"=%"
+  
+  rem 値検証ラベルへ
+  goto :CHK_VAL
+
+
+rem 引数判定
+:CHK_ARG
+  : 引数引継ぎ
+    set tgtPath=%1
+
+
+rem 値検証
+:CHK_VAL
   : ねずみ返し_ファイル存在確認
-    if not exist %pS1% (
+    if not exist %tgtPath% (
       echo;
       echo ファイルが存在しません
       echo 終了します
@@ -21,36 +52,12 @@ SETLOCAL DISABLEDELAYEDEXPANSION
       exit
     )
 
-  rem ダブルクォーテーション判断サブルーチン使用
-  set y=%pS1%
-  call :WQUOTATIONDECSION
-  set pS1=%y%
-
 
 : .ps1の実行
   echo;
   echo;
   rem .ps1ファイルを管理者実行
-  powershell -ExecutionPolicy Unrestricted %pS1%
+  powershell -ExecutionPolicy Unrestricted %tgtPath%
 
   pause
   exit
-
-
-rem ダブルクォーテーション判断サブルーチン
-:WQUOTATIONDECSION
-  rem 一文字目が「"」でない場合
-  rem If文のエスケープに「^」を使用
-  if not ^%y:~0,1%==^" (
-    rem 先頭にダブルクォートをつける
-    set x="%y%
-  )
-
-  rem 末尾が「"」でない場合
-  if not ^%y:~-1,1%==^" (
-    rem 末尾にダブルクォートをつける
-    set y=%y%"
-  )
-
-  rem サブルーチンを抜ける
-  exit /b
