25466301254b0a2adc651237b79f244347602ca8
cal03 <ghi>
2020-08-20 18:25:59 +0900

    PowerShell_Sample_Exec_UsrInpDecide_修正 ・軽微な修正(コメント) ・エラー時停止設定

--- PowerShell/Sample/Exec_UsrInpDecide.ps1
+++ PowerShell/Sample/Exec_UsrInpDecide.ps1
@@ -1,7 +1,11 @@
-$Host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
+# # 単体実行/呼び出し判別サンプル
 # 概要
 #   単体実行/呼び出し判別の
-#   呼び出し
+#   呼び出しパターン
+
+$Host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
+# 致命的でないエラーで処理終了設定
+$ErrorActionPreference = "Stop"
 
 
 <# ユーザ入力関数 #>
