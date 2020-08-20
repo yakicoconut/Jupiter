ef764e319b9eed4167badb2d96cf5bd0b7e746c7
cal03 <ghi>
2020-08-20 18:21:41 +0900

    PowerShell_Sample_Exec_UsrInp_修正 ・文字列サンプル追加 ・パスサンプル追加

--- PowerShell/Sample/Exec_UsrInp.ps1
+++ PowerShell/Sample/Exec_UsrInp.ps1
@@ -1,10 +1,27 @@
+using module "..\OwnLib\UsrInp.psm1"
+# # ユーザ入力関連関数クラスサンプル
+
+# タイトル設定
 $Host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
-# 概要
-#   ユーザ入力関連関数集サンプル
+# 致命的でないエラーで処理終了設定
+$ErrorActionPreference = "Stop"
 
 
-# サンプル対象ファイル読み込み
-. "..\OwnLib\UsrInp.ps1"
+# ユーザ入力関連関数クラスインスタンス生成
+$usrInpClass = [UsrInpClass]::new()
 
-# ユーザ入力関数使用
-UserInput "表示文言"
+Write-Host "--------------------"
+# ユーザ入力メソッド使用
+$inpData = $usrInpClass.UserInput("文字列-ループオン", $true, "STR")
+$inpData
+Write-Host
+
+Write-Host "--------------------"
+$inpData = $usrInpClass.UserInput("パス-ループオン", $true, "PATH")
+$inpData
+Write-Host
+
+Write-Host "--------------------"
+$inpData = $usrInpClass.UserInput("パス-ループオフ", $false, "PATH")
+$inpData
+Write-Host
