850c87bbbe9dfc06721a47102865191b67f5c86c
cal03 <ghi>
2020-08-20 18:36:31 +0900

    PowerShell_LogicTemp_UsrInpDecide_修正_呼び出し判断方法をコールスタックに変更

--- PowerShell/LogicTemp/UsrInpDecide.ps1
+++ PowerShell/LogicTemp/UsrInpDecide.ps1
@@ -6,33 +6,42 @@ $Host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
 #   ・引数あり呼び出しの場合、ユーザ入力なし
 
 
-<# ユーザ入力関数 #>
-  # # ユーザ入力関数
+<# 文字表示関数 #>
+  # # 文字表示
   #   引数01:表示文言
-  #   返り値:入力文字列
-  function UserInput($description)
+  #   返り値:なし
+  function EchoDesc($description)
   {
     # 文言表示
     Write-Host $description
-    $USR = (Read-Host 入力してください)
-    # 先頭文末ダブルクォーテーション削除
-    if($USR.Substring(0, 1) -eq "`""){ $USR = $USR.Substring(1, $USR.Length - 1) }
-    if($USR.Substring($USR.Length - 1, 1) -eq "`""){ $USR = $USR.Substring(0, $USR.Length - 1) }
-  
-    return $USR
   }
 
-
-<# 引数判断 #>
-  # 引数設定
-  $tgt = $Args[0]
-
-  # 引数がない場合
-  if($Args.Length -eq 0)
+<# 単体呼び出し処理関数 #>
+  # # 単体呼び出しでのみ実行する処理
+  #   引数01:なし
+  #   返り値:なし
+  function SingleExec()
   {
-    # ユーザ入力関数使用
-    $tgt = UserInput
+    Write-Host "単体実行"
+    Write-Host "表示文言入力"
+    $USR = (Read-Host 入力してください)
+    Write-Host
+    # スクリプト変数に値設定
+    $script:desc = $USR
   }
 
-  # 表示
-  Write-Host $tgt
+<# 呼び出し判断 #>
+  # 呼び出し元情報取得
+  $callstack = Get-PsCallStack
+  # 引数設定
+  $desc = $Args[0]
+
+  # 単体実行(本ファイル+スクリプトブロックの二つ)の場合
+  if($callstack.Length -eq 2)
+  {
+    # 単体呼び出し処理関数使用
+    SingleExec
+  }
+
+  # 文字表示関数使用
+  EchoDesc $desc
