963b389013dd8253cfcb96c8a6c9362290aae4b5
cal01 <abc@def.com>
2020-08-22 12:23:41 +0900

    PowerShell_POWERSHELL_Kick_Input_修正_引数カウント方法変更

--- PowerShell/Create/POWERSHELL_Kick_Input.bat
+++ PowerShell/Create/POWERSHELL_Kick_Input.bat
@@ -3,61 +3,67 @@ title %~nx0
 rem パワーシェル実行バッチ
 
 
-rem 遅延環境変数オフ
-SETLOCAL DISABLEDELAYEDEXPANSION
+rem 遅延環境変数オン
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 
 : 引数チェック
-  rem 引数カウント
-  set argc=0
-  for %%a in ( %* ) do set /a argc+=1
+  rem 引数数カウントサブルーチン使用
+  call :CNT_ARG %*
 
-  rem 引数がない場合、ユーザ入力へ
-  if %argc%==0 goto :USER_INPUT
-  rem 引数が定義通りの場合、引数判定へ
-  if %argc%==1 goto :CHK_ARG
+  rem 引数を実行ファイル名に設定
+  rem ※丸括弧対策として括弧外で設定
+  set pS1=%*
+  rem Wクォート対策
+  set pS1="!pS1:"=!"
 
-  echo 引数の数が定義と異なるため、終了します
-  echo 引数:%argc%
-  echo 定義:1
-  pause
-  exit /b
+  rem 引数が1つの場合、.ps1実行ラベルへ
+  if %argCt% == 1 goto :EXEC_SP1
+  rem ねずみ返し_引数が2つ以上の場合
+  if %argCt% geq 2 (
+    echo 引数が2つ以上存在するため
+    echo 終了します
+    echo 引数:%argCt%
+    pause
+    exit
+  )
 
 
-rem ユーザ入力処理
-:USER_INPUT
+: ユーザ入力処理
   echo 対象ファイルを指定してください
   set /P USR="入力してください:"
-  set tgtPath="%USR:"=%"
-  
-  rem 値検証ラベルへ
-  goto :CHK_VAL
+  set pS1="!USR:"=!"
 
 
-rem 引数判定
-:CHK_ARG
-  : 引数引継ぎ
-    set tgtPath=%1
+rem .ps1実行ラベル
+:EXEC_SP1
+  rem 対象PSファイル存在確認
+  if not exist !pS1! (
+    echo 対象.ps1ファイルが存在しません
+    echo !pS1!
+    pause
+    exit
+  )
 
-
-rem 値検証
-:CHK_VAL
-  : ねずみ返し_ファイル存在確認
-    if not exist %tgtPath% (
-      echo;
-      echo ファイルが存在しません
-      echo 終了します
-
-      pause
-      exit
-    )
-
-
-: .ps1の実行
   echo;
   echo;
   rem .ps1ファイルを管理者実行
-  powershell -ExecutionPolicy Unrestricted %tgtPath%
+  powershell -ExecutionPolicy Unrestricted !pS1!
 
   pause
   exit
+
+
+rem 引数数カウントサブルーチン
+:CNT_ARG
+  rem 全引数変数化
+  set argCmd=%*
+
+  rem カウンタ初期化
+  set /a argCt=0
+  for %%a in (!argCmd!) do (
+    rem カウンタインクリメント
+    set /a argCt+=1
+  )
+
+  exit /b
