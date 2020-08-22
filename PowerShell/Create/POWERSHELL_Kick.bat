42f0e82dbc9deb6084f3ee85839202b85589b8f3
cal01 <abc@def.com>
2020-08-22 12:24:45 +0900

    PowerShell_POWERSHELL_Kick.bat_修正_引数対応

--- PowerShell/Create/POWERSHELL_Kick.bat
+++ PowerShell/Create/POWERSHELL_Kick.bat
@@ -7,34 +7,64 @@ rem 遅延環境変数オン
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 
-: 自身のファイル名から実行対象の.ps1ファイル名を取得
-  rem 自身のファイル名取得
-  set myName=%~nx0
+: 引数チェック
+  rem 引数数カウントサブルーチン使用
+  call :CNT_ARG %*
 
-  rem パス設定
-  rem ※引数にパスを使用したい場合、以下コマンド内の「%~dp0\」を置換
-  set pS1=%~dp0%myName%
-  rem ファイル名から「_Kick.bat」を「.ps1」に置換え
-  set pS1=%pS1:_Kick.bat=.ps1%
+  rem 引数を実行ファイル名に設定
+  rem ※丸括弧対策として括弧外で設定
+  set pS1=%*
+  rem Wクォート対策
+  set pS1="!pS1:"=!"
 
-
-: 対象PSファイル存在確認
-  if not exist %pS1% (
-    rem 本ファイル名の置き換え対象部分取得
-    set fileNmae=%myName:_Kick.bat=%
-
-    echo 対象.ps1ファイルが存在しません
-    echo;
-    echo 本ファイルの「!fileNmae!」部分を
-    echo 実行対象の.ps1ファイル名に変更してから再度実行してください
-
+  rem 引数が1つの場合、.ps1実行ラベルへ
+  if %argCt% == 1 goto :EXEC_SP1
+  rem 引数が2つ以上の場合、
+  if %argCt% geq 2 (
+    echo 引数が2つ以上存在するため
+    echo 終了します
+    echo 引数:%argCt%
     pause
     exit
   )
 
 
-: .ps1の実行
+: 自身のファイル名から実行対象の.ps1ファイル名を取得
+  rem 自身のファイルパス取得
+  set pS1=%0
+  rem ファイル名から「_Kick.bat」を「.ps1」に置換え
+  set pS1=%pS1:_Kick.bat=.ps1%
+
+
+rem .ps1実行ラベル
+:EXEC_SP1
+  rem 対象PSファイル存在確認
+  if not exist !pS1! (
+    echo 対象.ps1ファイルが存在しません
+    echo !pS1!
+    pause
+    exit
+  )
+
+  echo;
+  echo;
   rem .ps1ファイルを管理者実行
-  powershell -ExecutionPolicy Unrestricted %pS1%
+  powershell -ExecutionPolicy Unrestricted !pS1!
 
 pause
+exit
+
+
+rem 引数数カウントサブルーチン
+:CNT_ARG
+  rem 全引数変数化
+  set argCmd=%*
+
+  rem カウンタ初期化
+  set /a argCt=0
+  for %%a in (!argCmd!) do (
+    rem カウンタインクリメント
+    set /a argCt+=1
+  )
+
+  exit /b
