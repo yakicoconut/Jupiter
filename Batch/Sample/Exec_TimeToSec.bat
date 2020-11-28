6a94d86fd5ee716b04a13e21f915eb56e0f3c989
Cal01 <abd@def>
2020-11-28 17:56:41 +0900

    Batch_Sample_Exec_TimeToSec_修正_ミリ秒パターン追加

--- Batch/Sample/Exec_TimeToSec.bat
+++ Batch/Sample/Exec_TimeToSec.bat
@@ -8,8 +8,15 @@ echo 時間秒変換バッチの使用例
   set call_TimeToSec="..\OwnLib\TimeToSec.bat"
 
 
-: 処理
+: パターン
   rem 時間秒変換バッチ使用
   call %call_TimeToSec% 01:23:45.678
-  echo %return_TimeToSec%
+  echo %return_TimeToSec1%
+  echo %return_TimeToSec2%
+
+: パターン
+  rem 時間秒変換バッチ使用
+  call %call_TimeToSec% 1:23:45.67
+  echo %return_TimeToSec1%
+  echo %return_TimeToSec2%
 pause
