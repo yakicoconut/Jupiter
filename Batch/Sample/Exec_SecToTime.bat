426e18e9c567e983ce256bb5c257f4d07af47cfd
Cal01 <abd@def>
2020-11-28 13:44:40 +0900

    Batch_Sample_Exec_SecToTime_修正_軽微な修正(コメント)

--- Batch/Sample/Exec_SecToTime.bat
+++ Batch/Sample/Exec_SecToTime.bat
@@ -1,15 +1,15 @@
 @echo off
 title %~nx0
-echo 時間秒変換バッチの使用例
+echo 秒時間変換バッチの使用例
 
 
 : 参照バッチ
-  rem 時間秒変換バッチ
+  rem 秒時間変換バッチ
   set call_SecToTime="..\OwnLib\SecToTime.bat"
 
 
 : 処理
-  rem 時間秒変換バッチ使用
+  rem 秒時間変換バッチ使用
   call %call_SecToTime% 5025
   echo %return_SecToTime%
 pause
