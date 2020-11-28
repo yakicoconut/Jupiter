adecceab3027c960ae0f31a30a114493d78b5e62
Cal01 <abd@def>
2020-11-28 16:00:56 +0900

    Batch_OwnLib_CngOctalNum_修正_先頭0除去関数として仕様拡張

--- Batch/OwnLib/CngOctalNum.bat
+++ Batch/OwnLib/CngOctalNum.bat
@@ -1,10 +1,11 @@
 @echo off
 title %~nx0
 rem 8進数数値変換バッチ
-rem   DOSコマンドでは0埋め二桁の数値は8進数として扱われる
-rem   そのため、「08」と「09」はオーバーフローとなる
-rem 引数01:対象値(二桁)
-rem 戻値:変換後数値
+rem   DOSコマンドでは0埋めの数値は8進数として扱われるため、
+rem   0埋めされている8と9はオーバーフローとなるので
+rem   先頭の「0」を除去する
+rem 引数01:対象値
+rem 戻値:変換後数値文字列
 
 
 rem 変数ローカル化
@@ -13,24 +14,30 @@ SETLOCAL
     rem 対象値
     set value=%1
 
-
   : 実行
     rem 返却用数値
-    set returnNumeric=%value%
+    set retNum="%value%"
 
-    rem 「08」の場合
-    if %value% == 08 (
-      rem 「8」に変換
-      set returnNumeric=8
-    )
+    :LOOP
+      : ねずみ返し_事後作業へ
+        rem 対象値が空の場合
+        if %retNum%=="" (
+          rem すべて0のパターンのため「0」を設定
+          set retNum="0"
+          goto :END_BAT
+        )
+        rem 一文字目が0でない場合
+        if not %retNum:~1,1%==0 ( goto :END_BAT )
 
-    rem 「09」の場合
-    if %value% == 09 (
-      rem 「9」に変換
-      set returnNumeric=9
-    )
+      rem 二文字目以降ループ変数格納
+      set retNum="%retNum:~2,-1%"
+      goto :LOOP
 
+  rem 事後作業
+  :END_BAT
+    rem Wクォート削除
+    set retNum=%retNum:"=%
 
 rem 戻り値
-ENDLOCAL && set return_CngOctalNum=%returnNumeric%
+ENDLOCAL && set return_CngOctalNum=%retNum%
 exit /b
