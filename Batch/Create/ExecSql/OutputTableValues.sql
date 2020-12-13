4da48ee57aac1f24b484951d2b9eefaaf8aa8b57
Cal03 <abc@def>
2020-12-13 13:55:43 +0900

    Batch_ExecSql_OutputTableValues_修正_一般化

--- Batch/Create/ExecSql/OutputTableValues.sql
+++ Batch/Create/ExecSql/OutputTableValues.sql
@@ -1,45 +1,17 @@
-/* テーブルデータ確認用スクリプト */
 /*
- * 「POS-VersionUp_ServerSqlDiffList.bat」から実行する
+ * テーブルデータ出力スクリプト
+ *   ・バッチファイルから実行する
+ *   ・対象テーブル等を見出しとして表示する場合は「SELECT 見出し」を使用する
  */
 
 /* 設定 */
 -- 「n件処理されました。」の非表示設定
 set nocount on
 
-
 /* データ取得 */
--- PaymentServiceMaster
-SELECT 'PaymentServiceMaster'
+-- hogeMaster
+SELECT 'hogeMaster'
 SELECT *
-FROM   PaymentServiceMaster
-WHERE  CATCode            = 'g-pfm10' AND
-       PaymentServiceCode = '06'
-
-
--- SystemParameter_JMBポイントイシュアID
-SELECT 'SystemParameter_JMBポイントイシュアID'
-SELECT *
-FROM   SystemParameter
-WHERE  ParameterCode = 'WaonJmbPointIssuerId'
-
-
--- #28082:【仕様変更】【POS】【決済端末連動(PFM-10)】交通系新UIガイドライン対応
-SELECT '#28082:【仕様変更】【POS】【決済端末連動(PFM-10)】交通系新UIガイドライン対応'
-SELECT *
-FROM   PaymentServiceReceiptLayoutMaster
-WHERE  CATCode            = 'g-pfm10' AND
-       PaymentServiceCode = '07'      AND
-       ColumnID           = 'Balance'
-
-
--- #28121:【決済端末連動(PFM-10)】WAON決済での取引完了後に電子マネー控えレシートが出力されない
-SELECT '#28121:【決済端末連動(PFM-10)】WAON決済での取引完了後に電子マネー控えレシートが出力されない'
-SELECT *
-FROM   PaymentServiceReceiptLayoutMaster
-WHERE  CATCode            = 'g-pfm10' AND
-       PaymentServiceCode = '06'
-
-/*
- * ・対象テーブル等を見出しとして表示する場合は「SELECT 見出し」を使用する
- */
+FROM   hogeMaster
+WHERE  ABC = 'abc' AND
+       DEF = 'def'
