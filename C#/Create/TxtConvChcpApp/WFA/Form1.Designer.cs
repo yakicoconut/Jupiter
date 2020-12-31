bac3870a7ff169d275cdeb016da41116b309db6b
Cal03 <abc@def>
2020-12-31 19:10:06 +0900

    C#_TxtConvChcpApp_追加

---
+++ C#/Create/TxtConvChcpApp/WFA/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WFA
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows フォーム デザイナーで生成されたコード
+
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btConvert = new System.Windows.Forms.Button();
+      this.tbTgtPath = new System.Windows.Forms.TextBox();
+      this.cbChcp = new System.Windows.Forms.ComboBox();
+      this.tbOutPath = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // btConvert
+      // 
+      this.btConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.btConvert.Location = new System.Drawing.Point(529, 114);
+      this.btConvert.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.btConvert.Name = "btConvert";
+      this.btConvert.Size = new System.Drawing.Size(163, 46);
+      this.btConvert.TabIndex = 1;
+      this.btConvert.Text = "変換";
+      this.btConvert.UseVisualStyleBackColor = true;
+      this.btConvert.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // tbTgtPath
+      // 
+      this.tbTgtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.tbTgtPath.Location = new System.Drawing.Point(95, 18);
+      this.tbTgtPath.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.tbTgtPath.Name = "tbTgtPath";
+      this.tbTgtPath.Size = new System.Drawing.Size(591, 31);
+      this.tbTgtPath.TabIndex = 2;
+      // 
+      // cbChcp
+      // 
+      this.cbChcp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.cbChcp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.cbChcp.FormattingEnabled = true;
+      this.cbChcp.Location = new System.Drawing.Point(20, 118);
+      this.cbChcp.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.cbChcp.Name = "cbChcp";
+      this.cbChcp.Size = new System.Drawing.Size(167, 32);
+      this.cbChcp.TabIndex = 3;
+      this.cbChcp.Validated += new System.EventHandler(this.cbChcp_Validated);
+      // 
+      // tbOutPath
+      // 
+      this.tbOutPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.tbOutPath.Location = new System.Drawing.Point(95, 68);
+      this.tbOutPath.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.tbOutPath.Name = "tbOutPath";
+      this.tbOutPath.Size = new System.Drawing.Size(591, 31);
+      this.tbOutPath.TabIndex = 4;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(15, 24);
+      this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(63, 24);
+      this.label1.TabIndex = 5;
+      this.label1.Text = "対象:";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(15, 74);
+      this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(63, 24);
+      this.label2.TabIndex = 6;
+      this.label2.Text = "出力:";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(724, 174);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.tbOutPath);
+      this.Controls.Add(this.cbChcp);
+      this.Controls.Add(this.tbTgtPath);
+      this.Controls.Add(this.btConvert);
+      this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.MinimumSize = new System.Drawing.Size(728, 179);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btConvert;
+    private System.Windows.Forms.TextBox tbTgtPath;
+    private System.Windows.Forms.ComboBox cbChcp;
+    private System.Windows.Forms.TextBox tbOutPath;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+  }
+}
+
---
+++ C#/Create/TxtConvChcpApp/WFA/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Configuration;
+using System.Drawing.Imaging;
+
+namespace WFA
+{
+  /// <summary>
+  /// メインフォーム
+  /// </summary>
+  public partial class Form1 : Form
+  {
+    #region コンストラクタ
+    public Form1()
+    {
+      InitializeComponent();
+
+      #region 【論理雛形】
+      WFAComLogic WFACL = new WFAComLogic();
+      // アプリ名設定
+      Text = WFACL.GetAppName();
+      #endregion
+
+      // コンフィグ取得メソッド使用
+      GetConfig();
+    }
+    #endregion
+
+    #region コンフィグ取得メソッド
+    public void GetConfig()
+    {
+      string hoge01 = ConfigurationManager.AppSettings["Hoge01"];
+    }
+    #endregion
+
+
+    #region 宣言
+
+    // 共通ロジッククラスインスタンス
+    MCSComLogic _comLogic = new MCSComLogic();
+
+    // エンコードフォーマット
+    Encoding encFmt;
+
+    #endregion
+
+
+    #region メインフォーム初期設定メソッド
+    private void MainFormInitSeting()
+    {
+      // 変更後拡張子コンボボックス設定
+      cbChcp.DataSource = new string[] { "UTF7", "BigEndianUnicode", "Unicode", "Default", "ASCII", "UTF8", "UTF32" };
+      // 変更後拡張子コンボボックス選択
+      cbChcp.SelectedItem = 0;
+      // エンコードフォーマット
+      encFmt = Encoding.UTF8;
+    }
+    #endregion
+
+
+    #region ロードイベント
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      // メインフォーム初期設定メソッド使用
+      MainFormInitSeting();
+    }
+    #endregion
+
+    #region コンボボックス値変更イベント
+    private void cbChcp_Validated(object sender, EventArgs e)
+    {
+      // 拡張子コンボボックスからフォーマットを選判定する
+      switch (cbChcp.Text)
+      {
+        case "UTF7":
+          encFmt = Encoding.UTF7;
+          break;
+
+        case "BigEndianUnicode":
+          encFmt = Encoding.BigEndianUnicode;
+          break;
+
+        case "Unicode":
+          encFmt = Encoding.Unicode;
+          break;
+
+        case "Default":
+          encFmt = Encoding.Default;
+          break;
+
+        case "ASCII":
+          encFmt = Encoding.ASCII;
+          break;
+
+        case "UTF8":
+          encFmt = Encoding.UTF8;
+          break;
+
+        case "UTF32":
+          encFmt = Encoding.UTF32;
+          break;
+      }
+    }
+    #endregion
+
+    #region ボタン1押下イベント
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // 対象パス取得
+      string tgtPath = _comLogic.ExclIniEndWQuot(tbTgtPath.Text);
+      // 出力パス取得
+      string outPath = tbOutPath.Text;
+      // 変換後文字コード取得
+      string chcp = cbChcp.Text;
+
+      // ねずみ返し
+      if (tgtPath == string.Empty)
+      {
+        MessageBox.Show("対象パスがありません");
+        return;
+      }
+
+      // 出力パスが設定されている場合
+      if (outPath != string.Empty)
+      {
+        // 最後一文字が「\」でない場合
+        if (outPath.Substring(outPath.Length - 1, 1) != @"\")
+          outPath += @"\";
+      }
+
+      // ファイルの場合
+      if (File.Exists(tgtPath))
+      {
+        // 文字コード変換メソッド使用
+        ChcpConversion(tgtPath, outPath, chcp);
+      }
+      else if (Directory.Exists(tgtPath))
+      {
+        // フォルダからXMLファイルのパスだけ取得
+        string[] targetFolder = Directory.GetFiles(tgtPath, "*", SearchOption.TopDirectoryOnly);
+
+        // ループ
+        foreach (string x in targetFolder)
+        {
+          // 文字コード変換メソッド使用
+          ChcpConversion(x, outPath, chcp);
+        }
+      }
+      else
+      {
+        MessageBox.Show("対象が存在しません");
+        return;
+      }
+
+      MessageBox.Show("完了しました");
+    }
+    #endregion
+
+    #region ボタン2押下イベント
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+    #endregion
+
+
+    #region 文字コード変換メソッド
+    private void ChcpConversion(string tgtPath, string outPath, string chcp)
+    {
+      // ファイル名取得
+      string tgtName = Path.GetFileName(tgtPath);
+
+      // ファイル読み込み
+      StreamReader sr = new StreamReader(tgtPath);
+      string str = sr.ReadToEnd();
+      sr.Close();
+
+      // エンコードセット
+      Encoding srcEnc = Encoding.Unicode;
+      Encoding destEnc = Encoding.UTF8;
+
+      // バイナリ変換
+      byte[] binaryStr = srcEnc.GetBytes(str);
+      // 文字コード変換
+      byte[] convBinary = Encoding.Convert(srcEnc, destEnc, binaryStr);
+      // 文字列変換
+      string retStr = destEnc.GetString(convBinary);
+
+      // ファイルを開く
+      StreamWriter writer = new StreamWriter(tgtName, false);
+      // テキストを書き込む
+      writer.WriteLine(retStr);
+      // ファイルを閉じる
+      writer.Close();
+    }
+    #endregion
+
+
+    #region 雛形メソッド
+    private void template()
+    {
+
+    }
+    #endregion
+  }
+}
