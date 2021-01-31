d71cfe5433e22aed906ca9a20f6b626a335e4510
Cal03 <abc@def>
2021-01-31 14:18:50 +0900

    C#_LogicTemp_ItemDropInForm_修正_アイテムドロップ対応

--- C#/LogicTemp/Form/ItemDropInForm/WFA/FrmItemDropIn.cs
+++ C#/LogicTemp/Form/ItemDropInForm/WFA/FrmItemDropIn.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -29,17 +30,43 @@ namespace WFA
     }
     #endregion
 
-    #region グループボックスドラッグドロップイベント
-    private void groupBox_DragDrop(object sender, DragEventArgs e)
-    {
-
-    }
-    #endregion
 
     #region グループボックスドラッグエンターイベント
     private void groupBox_DragEnter(object sender, DragEventArgs e)
     {
+      // ねずみ返し_マウスがファイルを持っていない場合、イベント・ハンドラを抜ける
+      if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+      {
+        return;
+      }
 
+      // ドラッグ中アイテムの取得
+      string[] drags = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+      // ねずみ返し_最初の一つ目がフォルダでもファイルでもない場合
+      if (!Directory.Exists(drags[0]) && !File.Exists(drags[0]))
+      {
+        return;
+      }
+
+      // マウスの表示を「+」に変更する
+      e.Effect = DragDropEffects.Copy;
+    }
+    #endregion
+
+    #region グループボックスドラッグドロップイベント
+    private void groupBox_DragDrop(object sender, DragEventArgs e)
+    {
+      // ドラッグ&ドロップされたアイテムの取得
+      string[] drags = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+      // アイテム表示
+      string outStr = string.Empty;
+      foreach (string x in drags)
+      {
+        outStr += x + Environment.NewLine;
+      }
+      MessageBox.Show(outStr); 
     }
     #endregion
   }
