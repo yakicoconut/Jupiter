43a2b922b5119cb594f2536076c6572e33b4c767
Cal03 <abc@def>
2021-07-24 18:50:05 +0900

    C#_Function_Found_Static_追加

---
+++ C#/Function/Found/Static/001/WCA/Program.cs
@@ -0,0 +1,202 @@
+﻿#define PATTERN01
+#define PATTERN02
+//#define PATTERN03
+#define PATTERN04
+//#define PATTERN05
+#define PATTERN06
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#region ヘッダ
+/*
+ * staticメソッド内でstaticでないメンバを参照できない理由 - プログラミングとかブログ
+ *  http://shirakamisauto.hatenablog.com/entry/2015/06/15/181428
+ *    ・static(静的)修飾子を使用して宣言されたメンバはアプリの実行時にメモリが確保される
+ *      そのためインスタンス化して始めてメモリ展開される動的なメンバを含んでいる場合、
+ *      staticメンバの使用時に動的メンバがインスタンス化(メモリ展開)されていない可能性があるためエラーとなる
+ * 
+ * 連載! とことん C#: 第 2 回 静的 (static) なクラスとその意義 言語: C#
+ *	https://code.msdn.microsoft.com/windowsdesktop/2-static-8c0cc826
+ *	  ・動的メンバはインスタンス化によってメモリを確保できる分の実体を生成することができる
+ *	    静的メンバはインスタンス化できないため唯一一つの実体しかもてない(データの整合性が取りやすい)
+ */
+#endregion
+namespace WCA
+{
+  /// <summary>
+  /// メインプログラム
+  /// </summary>
+  class Program
+  {
+
+    #region 宣言
+
+    // 共通ロジッククラスインスタンス
+    static MCSComLogic _comLogic = new MCSComLogic();
+
+    #endregion
+
+    static void Main(string[] args)
+    {
+      #region パターン01_静的メソッド
+      /*
+       * 単純に静的クラスの静的メソッドを呼び出す
+       */
+#if PATTERN01
+      // 静的クラスの静的メソッド使用
+      TestClass01.Test01();
+#endif
+      #endregion
+
+
+      #region パターン02_動的メソッド
+      /*
+       * 動的クラスでも静的メソッドであれば
+       * インスタンス化せずに呼び出すことが可能
+       */
+#if PATTERN02
+      // 動的クラスの静的メソッド使用
+      TestClass02.Test02();
+#endif
+      #endregion
+
+
+      #region パターン03_動的メソッドエラー
+#if PATTERN03
+      /*
+       * エラーパターン
+       * 動的クラスの動的メソッドをインスタンス化しないで
+       * 使用しようとする
+       */
+
+      // 動的クラスの動的メソッド使用
+      TestClass02.Test03();
+#endif
+      #endregion
+
+
+      #region パターン04_動的メソッド
+      /*
+       * 動的クラスの動的メソッドを使用するにはインスタンス化が必須
+       */
+#if PATTERN04
+      // 動的クラスインスタンス化
+      TestClass02 tearClass02 = new TestClass02();
+
+      // 動的クラスの動的メソッド使用
+      tearClass02.Test03();
+#endif
+      #endregion
+
+
+      #region パターン05_動的メソッドエラー
+#if PATTERN05
+      /*
+       * コンパイルエラーパターン
+       * 静的メソッド内で動的変数を使用しようとする
+       */
+#endif
+      #endregion
+
+
+      #region パターン06_静的メソッド内動的変数
+      /*
+       * 静的メソッド内で動的変数のを含むクラスのインスタンス化が
+       * されていれば動的変数のメモリが確保されているため問題なく呼び出せる
+       */
+#if PATTERN06
+      // 動的クラスの動的メソッド使用
+      TestClass02.Test05();
+#endif
+      #endregion
+
+
+      Console.ReadKey();
+    }
+  }
+
+
+  #region 静的クラス
+  /// <summary>
+  /// 静的クラス
+  /// </summary>
+  static class TestClass01
+  {
+    #region 静的メソッド
+    /// <summary>
+    /// 静的メソッド
+    /// </summary>
+    public static void Test01()
+    {
+      Console.WriteLine("TEST001");
+    }
+    #endregion
+  }
+  #endregion
+
+
+  #region 動的クラス
+  /// <summary>
+  /// 動的クラス
+  /// </summary>
+  class TestClass02
+  {
+    #region 静的メソッド
+    /// <summary>
+    /// 静的メソッド
+    /// </summary>
+    public static void Test02()
+    {
+      Console.WriteLine("TEST002");
+    }
+    #endregion
+
+    #region 動的メソッド
+    /// <summary>
+    /// 動的メソッド
+    /// </summary>
+    public void Test03()
+    {
+      Console.WriteLine("TEST003");
+    }
+    #endregion
+
+    #region パターン05_静的メソッド内動的変数エラー
+#if PATTERN05
+    string str01;
+
+    /// <summary>
+    /// 静的メソッド
+    /// </summary>
+    public static void Test04()
+    {
+      str01 = "TEST004";
+      Console.WriteLine(str01);
+    }
+#endif
+    #endregion
+
+    #region パターン06_静的メソッド内動的変数
+#if PATTERN06
+    string str02;
+
+    /// <summary>
+    /// 静的メソッド内動的変数
+    /// </summary>
+    public static void Test05()
+    {
+      // 本クラスのインスタンス生成
+      // これにより動的変数str02のメモリが確保される
+      TestClass02 testClass02 = new TestClass02();
+
+      // インスタンスが生成され、動的変数のメモリが確保されているためエラーにならない
+      testClass02.str02 = "TEST005";
+      Console.WriteLine(testClass02.str02);
+    }
+#endif
+    #endregion
+  }
+  #endregion
+}
---
+++ C#/Function/Found/Stract/001/WFA/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Configuration;
+
+#region ヘッダ
+/*
+ * 構造体
+ *   ・クラスとの違いは値型か参照型か
+ */
+#endregion
+namespace WFA
+{
+  /// <summary>
+  /// メインフォーム
+  /// </summary>
+  public partial class Form1 : Form
+  {
+    #region コンストラクタ
+    public Form1()
+    {
+      InitializeComponent();
+
+      #region 【論理雛形】
+      WFAComLogic WFACL = new WFAComLogic();
+      // アプリ名設定
+      Text = WFACL.GetAppName();
+      #endregion
+
+      // コンフィグ取得メソッド使用
+      GetConfig();
+    }
+    #endregion
+
+    #region コンフィグ取得メソッド
+    public void GetConfig()
+    {
+      string hoge01 = _comLogic.GetConfigValue("Key01", "DefaultValue");
+    }
+    #endregion
+
+
+    #region 宣言
+
+    // 共通ロジッククラスインスタンス
+    MCSComLogic _comLogic = new MCSComLogic();
+
+    #endregion
+
+
+    #region ボタン1押下イベント
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // 返却クラス返却メソッド使用
+      ReturnClass clss = Reclass();
+      // 返却構造体返却メソッド使用
+      ReturnStruct strct = ReStruct();
+
+
+      /* インスタンスから値取得 */
+      // 返却クラス型の値表示
+      textBox1.AppendText(clss.var01 + Environment.NewLine);
+      textBox1.AppendText(clss.var02.ToString() + Environment.NewLine);
+      foreach (int x in clss.array03)
+      {
+        textBox1.AppendText(x.ToString() + Environment.NewLine);
+      }
+      // 返却構造体型の値表示
+      textBox1.AppendText(strct.var04 + Environment.NewLine);
+      textBox1.AppendText(strct.var05.ToString() + Environment.NewLine);
+      foreach (int x in strct.array06)
+      {
+        textBox1.AppendText(x.ToString() + Environment.NewLine);
+      }
+
+
+      /* インスタンスを引数にメソッド内で値変更 */
+      textBox1.AppendText("-------------------------------------" + Environment.NewLine);
+      // インスタンス値変更メソッド使用
+      ChangeIns(clss, strct);
+      // 値表示
+      textBox1.AppendText(clss.var01 + Environment.NewLine);
+      textBox1.AppendText(strct.var04 + Environment.NewLine);
+      textBox1.AppendText(strct.array06[1] + Environment.NewLine);
+
+
+      /* 変数を引数にメソッド内で値変更 */
+      textBox1.AppendText("-------------------------------------" + Environment.NewLine);
+      // 変数値変更メソッド使用
+      ChangeVar(clss.var01, clss.array03, strct.var04, strct.array06);
+      // 返却クラス型の値表示
+      textBox1.AppendText(clss.var01 + Environment.NewLine);
+      foreach (int x in clss.array03)
+      {
+        textBox1.AppendText(x.ToString() + Environment.NewLine);
+      }
+      // 返却構造体型の値表示
+      textBox1.AppendText(strct.var04 + Environment.NewLine);
+      foreach (int x in strct.array06)
+      {
+        textBox1.AppendText(x.ToString() + Environment.NewLine);
+      }
+    }
+    #endregion
+
+    #region ボタン2押下イベント
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+    #endregion
+
+
+    #region 返却クラス返却メソッド
+    public ReturnClass Reclass()
+    {
+      // 返却クラスインスタンス生成
+      ReturnClass rClass = new ReturnClass();
+
+      // 返却クラスの変数に値を格納
+      rClass.var01 = "1";
+      rClass.var02 = 2;
+      rClass.array03 = new int[] { 3, 33, 333 };
+
+      // 返却クラス返却
+      return rClass;
+    }
+    #endregion
+
+    #region 返却構造体返却メソッド
+    public ReturnStruct ReStruct()
+    {
+      // 返却構造体インスタンス生成
+      ReturnStruct rStruct = new ReturnStruct();
+
+      // 返却構造体の変数に値を格納
+      rStruct.var04 = "4";
+      rStruct.var05 = 5;
+      rStruct.array06 = new int[] { 6, 66, 666 };
+
+      // 返却構造体返却
+      return rStruct;
+    }
+    #endregion
+
+
+    #region インスタンス値変更メソッド
+    public void ChangeIns(ReturnClass clss, ReturnStruct strct)
+    {
+      // 値変更
+      clss.var01 = "a";
+      strct.var04 = "b";
+      strct.array06[1] = 10000;
+    }
+    #endregion
+
+    #region 変数値変更メソッド
+    public void ChangeVar(string clssStr, int[] clssArray, string strctStr, int[] strctArray)
+    {
+      // 値変更
+      clssStr = "z";
+      clssArray[1] = 99;
+      strctStr = "y";
+      strctArray[1] = 98;
+    }
+    #endregion
+  }
+
+
+  #region 返却クラス
+  /// <summary>
+  /// 返却クラス
+  /// </summary>
+  public class ReturnClass
+  {
+    public string var01;
+    public int var02;
+    public int[] array03;
+  }
+  #endregion
+
+
+  #region 返却構造体
+  /// <summary>
+  /// 返却構造体
+  /// </summary>
+  public struct ReturnStruct
+  {
+    public string var04;
+    public int var05;
+    public int[] array06;
+  }
+  #endregion
+}
