f9b3fabda6339c1e2d87e415b0bb443a6c6cd910
Cal03 <abc@def>
2021-07-24 18:50:38 +0900

    C#_Function_Found_UsingDir_追加

---
+++ C#/Function/Found/UsingDir/001/WCA/Program.cs
@@ -0,0 +1,21 @@
+﻿#region ヘッダ
+/*
+ * Usingディレクティブ
+ *   ・名前空間で型の使用を許可する
+ *     これにより、その名前空間内では型を修飾せずに使用可能となる
+ */
+#endregion
+namespace WCA
+{
+  /// <summary>
+  /// メインプログラム
+  /// </summary>
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine();
+      System.Console.ReadKey();
+    }
+  }
+}
