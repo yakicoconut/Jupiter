b862adff00042e6656c05d82fdd93d91bc06dc3f
Cal03 <abc@def>
2021-07-24 18:53:28 +0900

    C#_Function_OOP_This_追加

---
+++ C#/Function/OOP/This/001/WFA/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+
+#region ヘッダ
+/*
+ * Thisキーワード
+ * ・「this」キーワードは記述されているクラスを返す
+ * ・宣言されているメンバの名称に同一のものがある場合(クラスメンバ。メソッドメンバ等)も
+ *   別の変数として扱われ「this」を接頭辞とした変数をクラスメンバとして扱う
+ *   →「this」を使用したものはクラスメンバを指定、使用しないものはメソッドメンバを指定
+ */
+#endregion
+namespace WFA
+{
+  public partial class Form1 : Form
+  {
+    #region コンストラクタ
+    public Form1()
+    {
+      InitializeComponent();
+
+      #region 【論理雛形】
+      WFAComLogic WFACL = new WFAComLogic();
+      // アプリ名設定
+      Text = WFACL.GetAppName();
+      #endregion
+    }
+    #endregion
+
+
+    #region クラスメンバ
+
+    // クラスのメンバとして宣言した「var」変数
+    public string var;
+
+    #endregion
+
+
+    #region ボタン1押下イベント
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // thisキーワード検証メソッド使用
+      ThisTestMeth();
+    }
+    #endregion
+
+    #region ボタン2押下イベント
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+    #endregion
+
+    #region thisキーワード検証メソッド
+    /// <summary>
+    /// thisキーワード検証メソッド
+    /// </summary>
+    private void ThisTestMeth()
+    {
+      // メソッド内で宣言した「var」変数
+      string var;
+
+      // メソッド内
+      var = "abc";
+      // クラスメンバ
+      this.var = "def";
+
+      // メソッド内の変数を使用
+      textBox1.AppendText(var);
+      textBox1.AppendText("\r\n");
+
+      // クラスメンバの変数を使用
+      textBox1.AppendText(this.var);
+
+      // 「this」は本クラスを指す
+      textBox1.AppendText("\r\n\r\n");
+      textBox1.AppendText(this.ToString());
+    }
+    #endregion
+  }
+}
---
+++ C#/Function/OOP/This/002/WFA/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+
+#region ヘッダ
+/*
+ * Thisキーワード
+ * ・デバッグ時の「this」の紛らわしさ
+ *   継承先クラスで再定義していないメンバを使用すると
+ *   継承元クラスのものが処理される
+ *   そのためデバッグでブレークすると継承元クラスのメンバにステップインされる
+ *   このとき、this修飾子を使用して継承先で再定義しているメンバを呼び出すと紛らわしい
+ *   →下記のDerivate01Class.Exec()でブレークしBaseClass.Vir01Meth()にステップイン、「this.Vir02Meth()」の呼び出しを行うと
+ *     Derivate01Class.Vir02Meth()にステップインする
+ *     ⇒この挙動は処理の途中のBaseClass.Vir01Meth()からブレークしていると
+ *       thisを使用しているのに別クラスのメソッドを呼び出すため混乱しやすいので注意
+ */
+#endregion
+namespace WFA
+{
+  public partial class Form1 : Form
+  {
+    #region コンストラクタ
+    public Form1()
+    {
+      InitializeComponent();
+
+      #region 【論理雛形】
+      WFAComLogic WFACL = new WFAComLogic();
+      // アプリ名設定
+      Text = WFACL.GetAppName();
+      #endregion
+    }
+    #endregion
+
+
+    #region ボタン1押下イベント
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // 継承先クラスの実行メソッド呼び出し
+      Derivate01Class d01c = new Derivate01Class();
+      d01c.Exec();
+    }
+    #endregion
+
+    #region ボタン2押下イベント
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+    #endregion
+  }
+
+
+  #region 基底クラス
+  /// <summary>
+  /// 基底クラス
+  /// </summary>
+  public class BaseClass
+  {
+    /// <summary>
+    /// 継承元メソッド01
+    /// </summary>
+    /// <returns></returns>
+    protected virtual void Vir01Meth()
+    {
+      MessageBox.Show("Base01");
+
+      // 「this」を使用しているが実際に呼び出すのは
+      // 継承先クラスのメソッド
+      this.Vir02Meth();
+    }
+
+    /// <summary>
+    /// 継承元メソッド02
+    /// </summary>
+    /// <returns></returns>
+    protected virtual void Vir02Meth()
+    {
+      MessageBox.Show("Base02");
+    }
+  }
+  #endregion
+
+
+  #region 継承先クラス01
+  /// <summary>
+  /// 継承先クラス01
+  /// </summary>
+  public class Derivate01Class : BaseClass
+  {
+    /// <summary>
+    /// 実行メソッド
+    /// </summary>
+    public void Exec()
+    {
+      // 本クラスにメソッドが存在しない(コメントアウト中の)ため
+      // 継承元クラスのメソッドを使用する       
+      this.Vir01Meth();
+    }
+
+    ///// <summary>
+    ///// 継承先メソッド01
+    ///// </summary>
+    //protected override void Vir01Meth()
+    //{
+    //  MessageBox.Show("Derivate01");
+    //  this.Vir02Meth();
+    //}
+
+    /// <summary>
+    /// 継承先メソッド02
+    /// </summary>
+    protected override void Vir02Meth()
+    {
+      MessageBox.Show("Derivate02");
+    }
+  }
+  #endregion
+}
