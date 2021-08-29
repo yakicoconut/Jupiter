dfeeaceea3facaf46bddf284cbd087bbf3da4e98
Cal03 <abc@def>
2021-08-29 18:10:08 +0900

    C#_Create_XmlToCsv_追加

---
+++ C#/Create/XmlToCsv/WFA/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WFA
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows フォーム デザイナーで生成されたコード
+
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button2 = new System.Windows.Forms.Button();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.button1.Location = new System.Drawing.Point(26, 219);
+      this.button1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(163, 46);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBox1.Location = new System.Drawing.Point(26, 24);
+      this.textBox1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.textBox1.Size = new System.Drawing.Size(667, 31);
+      this.textBox1.TabIndex = 2;
+      // 
+      // button2
+      // 
+      this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.button2.Location = new System.Drawing.Point(202, 219);
+      this.button2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(163, 46);
+      this.button2.TabIndex = 3;
+      this.button2.Text = "button2";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // textBox2
+      // 
+      this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBox2.Location = new System.Drawing.Point(26, 67);
+      this.textBox2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.textBox2.Multiline = true;
+      this.textBox2.Name = "textBox2";
+      this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.textBox2.Size = new System.Drawing.Size(667, 140);
+      this.textBox2.TabIndex = 4;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(724, 289);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.button1);
+      this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.TextBox textBox2;
+  }
+}
+
---
+++ C#/Create/XmlToCsv/WFA/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WFA
+{
+  /// <summary>
+  /// メインフォーム
+  /// </summary>
+  public partial class Form1 : Form
+  {
+    #region コンストラクタ
+    public Form1()
+    {
+      InitializeComponent();
+
+      #region 【論理雛形】
+      WFAComLogic WFACL = new WFAComLogic();
+      // アプリ名設定
+      Text = WFACL.GetAppName();
+      #endregion
+
+      // コンフィグ取得メソッド使用
+      GetConfig();
+    }
+    #endregion
+
+    #region コンフィグ取得メソッド
+    public void GetConfig()
+    {
+      string hoge01 = _comLogic.GetConfigValue("Key01", "DefaultValue");
+    }
+    #endregion
+
+
+    #region 宣言
+
+    // 共通ロジッククラスインスタンス
+    MCSComLogic _comLogic = new MCSComLogic();
+
+    // StringReader設定
+    XmlReaderSettings xmlSet;
+
+    #endregion
+
+
+    #region 初期設定メソッド
+    private void SetInit()
+    {
+      /* StringReader設定 */
+      xmlSet = new XmlReaderSettings();
+      // コメントを無視するかどうか
+      // ※デフォルトもfalseだが明示的に設定
+      xmlSet.IgnoreComments = false;
+      // 処理命令(スタイルシートの宣言等)を無視するかどうか
+      // ※デフォルトもfalseだが明示的に設定
+      xmlSet.IgnoreProcessingInstructions = false;
+      // 意味のない空白を無視するかどうか
+      xmlSet.IgnoreWhitespace = true;
+    }
+    #endregion
+
+
+    #region フォームロードイベント
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      // 初期設定メソッド使用
+      SetInit();
+    }
+    #endregion
+
+    #region ボタン1押下イベント
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // XmlCsv変換メソッド使用
+      List<string> retList = ConvXmlToCsv(textBox1.Text);
+
+      // CSV出力
+      using (StreamWriter sw = new StreamWriter(@"test.csv", false, Encoding.UTF8))
+      {
+        foreach (string x in retList)
+        {
+          sw.WriteLine(x);
+        }
+      }
+    }
+    #endregion
+
+    #region ボタン2押下イベント
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+    #endregion
+
+
+    #region XmlCsv変換メソッド
+    private List<string> ConvXmlToCsv(string tgtPath)
+    {
+      // 返り値変数
+      List<string> retList = new List<string>();
+
+      // フルパスディクショナリ
+      Dictionary<int, string> fullPathDic = new Dictionary<int, string>();
+      // 属性ディクショナリ
+      Dictionary<int, string> attrDic= new Dictionary<int, string>();
+      // 値ディクショナリ
+      Dictionary<int, string> valDic = new Dictionary<int, string>();
+
+      // 累計パスリスト
+      List<string> cmlPathList = new List<string>();
+
+      // 階層順要素リスト
+      List<string> depthElemList = new List<string>();
+
+      // 最大属性数
+      int attrMaxDepth = 0;
+      // タグ行数
+      int tagRow = 0;
+      // 前回階層
+      int preDepth = 0;
+
+      // ファイルからXMLを取得
+      // インスタンスを生成する全てのクラスをusing化(しないとファイルが開放されない)
+      using (StreamReader strmRdr = new StreamReader(tgtPath))
+      using (XmlReader xmlRdr = XmlReader.Create(strmRdr, xmlSet))
+      {
+        // XmlReader.Readメソッド使用パターン
+        while (xmlRdr.Read())
+        {
+          // ノードタイプで分岐
+          switch (xmlRdr.NodeType)
+          {
+            case XmlNodeType.Element: // 開始タグ
+              // タグ行数インクリメント
+              tagRow++;
+
+              // 現在階層取得
+              int dep = xmlRdr.Depth;
+
+              // 前回階層との差を取得
+              int calcI = dep - preDepth;
+              // 前回より深くなった場合は「1」を設定
+              int depI = calcI <= 0 ? calcI : 1;
+
+              // 前回階層差ループ
+              for (int i = depI; i <= 0; i++)
+              {
+                // 累計パスリストに要素がない場合
+                int cnt = cmlPathList.Count;
+                if(cnt == 0)
+                {
+                  break;
+                }
+
+                // 累計パスリストの後ろ1要素を削除
+                cmlPathList.RemoveAt(cnt - 1);
+              }
+
+              // 要素名取得
+              string pathName = xmlRdr.Name;
+
+              // 累計パスリストに追加
+              cmlPathList.Add(pathName);
+
+              // 階層数分カンマを頭につけて累計パスリストに追加
+              depthElemList.Add(new string(',', dep) + pathName);
+
+              // 累計パスリストを「/」で結合
+              string fullPathStr = string.Join("/", cmlPathList);
+
+              // フルパスディクショナリ追加
+              fullPathDic.Add(tagRow, string.Format("\"{0}\"", fullPathStr));
+
+              // 現在階層を前回階層変数に引継ぎ
+              preDepth = dep;
+
+              // 属性がない場合
+              if (!xmlRdr.HasAttributes)
+              {
+                break;
+              }
+
+              // 最大属性数を更新
+              int attrCnt = xmlRdr.AttributeCount;
+              if (attrMaxDepth < attrCnt)
+              {
+                attrMaxDepth = attrCnt;
+              }
+
+              // 属性をループ
+              string attrStr = string.Empty;
+              string attrStrFmt = "\"{0}\",\"{1}\"";
+              for (int i = 0; i < attrCnt; i++)
+              {
+                // 属性へリーダを移動
+                xmlRdr.MoveToAttribute(i);
+
+                // 属性文字列設定
+                attrStr += string.Format(attrStrFmt, xmlRdr.Name, xmlRdr.Value);
+                // 次のフォーマット作成
+                attrStrFmt = ",\"{0}\",\"{1}\"";
+              }
+
+              // 属性ディクショナリ追加
+              attrDic.Add(tagRow, attrStr);
+              break;
+
+            case XmlNodeType.Text: // 値
+              // 値ディクショナリ追加
+              valDic.Add(tagRow, string.Format("\"{0}\"", xmlRdr.Value));
+              break;
+
+            case XmlNodeType.Comment: // コメントタグ
+            case XmlNodeType.Attribute: // 属性
+            case XmlNodeType.XmlDeclaration: // XML宣言
+            case XmlNodeType.EndElement: // 終了タグ
+            case XmlNodeType.None:
+            default:
+              continue;
+          }
+        }
+
+        // ヘッダ初期値
+        string headerStr = "ID,フルパス,値";
+        // 属性ヘッダ作成
+        for (int i = 1; i <= attrMaxDepth; i++)
+        {
+          // 名称+二桁階層数
+          headerStr += string.Format(",属性名{0,2},属性値{0,2}", i.ToString());
+        }
+
+        // 返り値リストにヘッダ追加
+        retList.Add(headerStr);
+        // タグ行数ループ
+        for (int i = 1; i <= tagRow; i++)
+        {
+          // 値ディクショナリに該当キーがある場合
+          string valStr = string.Empty;
+          if (valDic.ContainsKey(i))
+          {
+            // 値を設定
+            valStr = valDic[i];
+          }
+          string attrStr = string.Empty;
+          if (attrDic.ContainsKey(i))
+          {
+            attrStr = attrDic[i];
+          }
+
+          // 返り値リスト追加
+          retList.Add(string.Format("{0},{1},{2},{3}", i.ToString(), fullPathDic[i], valStr, attrStr));
+        }
+      }
+
+      // 階層順要素ループ
+      foreach (string x in depthElemList)
+      {
+        // テキストボックスに出力
+        textBox2.AppendText(x + Environment.NewLine);
+      }
+
+      return retList;
+    }
+    #endregion
+  }
+}
