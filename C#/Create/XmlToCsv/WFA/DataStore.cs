a3e79062f434629c85e75212ae80ff7683843af1
Cal03 <abc@def>
2021-09-05 00:20:52 +0900

    C#_Create_XmlToCsv_修正_リファクタリング

---
+++ C#/Create/XmlToCsv/WFA/DataStore.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFA
+{
+  public class DataStore
+  {
+    #region プロパティ
+
+    /// <summary>
+    /// CSV変換後リスト
+    /// </summary>
+    public List<string> ConvdCsvList { get; set; }
+    /// <summary>
+    /// 階層順要素リスト
+    /// </summary>
+    public List<string> DepthElemList { get; set; }
+
+    /// <summary>
+    /// フルパスカラムディクショナリ
+    /// </summary>
+    public Dictionary<int, string> FullPathColDic { get; set; }
+    /// <summary>
+    /// 属性カラムディクショナリ
+    /// </summary>
+    public Dictionary<int, string> AttrColDic { get; set; }
+    /// <summary>
+    /// 値カラムディクショナリ
+    /// </summary>
+    public Dictionary<int, string> ValColDic { get; set; }
+
+    /// <summary>
+    /// 累計パスリスト
+    /// </summary>
+    public List<string> CmlPathList { get; set; }
+
+    /// <summary>
+    /// 最大属性数
+    /// </summary>
+    public int AttrMaxDepthNum { get; set; }
+
+    /// <summary>
+    /// 現在階層数
+    /// </summary>
+    public int CrDepthNum { get; set; }
+
+    /// <summary>
+    /// 前回階層数
+    /// </summary>
+    public int PreDepthNum { get; set; }
+
+    #endregion
+
+
+    #region コンストラクタ
+    public DataStore()
+    {
+      // プロパティ初期化
+      ConvdCsvList = new List<string>();
+      DepthElemList = new List<string>();
+      FullPathColDic = new Dictionary<int, string>();
+      AttrColDic = new Dictionary<int, string>();
+      ValColDic = new Dictionary<int, string>();
+      CmlPathList = new List<string>();
+      AttrMaxDepthNum = 0;
+      CrDepthNum = 0;
+      PreDepthNum = 0;
+    }
+    #endregion
+  }
+}
--- C#/Create/XmlToCsv/WFA/Form1.cs
+++ C#/Create/XmlToCsv/WFA/Form1.cs
@@ -75,15 +75,23 @@ namespace WFA
     #region ボタン1押下イベント
     private void button1_Click(object sender, EventArgs e)
     {
-      // XmlCsv変換メソッド使用
-      List<string> retList = ConvXmlToCsv(textBox1.Text);
+      // データセットクラスインスタンス生成
+      DataStore ds = new DataStore();
+
+      // Xml要素探索メソッド使用
+      DigXmlElem(ds, textBox1.Text);
 
       // CSV出力
-      using (StreamWriter sw = new StreamWriter(@"test.csv", false, Encoding.UTF8))
+      using (StreamWriter swMain = new StreamWriter(@"test.csv", false, Encoding.UTF8))
+      using (StreamWriter swSub = new StreamWriter(@"test_ElemNm.csv", false, Encoding.UTF8))
       {
-        foreach (string x in retList)
+        // 階層数分ループ
+        for (int i = 0; i < ds.CrDepthNum - 1; i++)
         {
-          sw.WriteLine(x);
+          // CSV変換後値
+          swMain.WriteLine(ds.ConvdCsvList[i]);
+          // 階層順要素
+          swSub.WriteLine(ds.DepthElemList[i]);
         }
       }
     }
@@ -97,38 +105,20 @@ namespace WFA
     #endregion
 
 
-    #region XmlCsv変換メソッド
-    private List<string> ConvXmlToCsv(string tgtPath)
+    #region Xml要素探索メソッド
+    /// <summary>
+    /// Xml要素探索メソッド
+    /// </summary>
+    /// <param name="ds">データストア</param>
+    /// <param name="tgtPath">対象パス</param>
+    private void DigXmlElem(DataStore ds, string tgtPath)
     {
-      // 返り値変数
-      List<string> retList = new List<string>();
-
-      // フルパスディクショナリ
-      Dictionary<int, string> fullPathDic = new Dictionary<int, string>();
-      // 属性ディクショナリ
-      Dictionary<int, string> attrDic= new Dictionary<int, string>();
-      // 値ディクショナリ
-      Dictionary<int, string> valDic = new Dictionary<int, string>();
-
-      // 累計パスリスト
-      List<string> cmlPathList = new List<string>();
-
-      // 階層順要素リスト
-      List<string> depthElemList = new List<string>();
-
-      // 最大属性数
-      int attrMaxDepth = 0;
-      // タグ行数
-      int tagRow = 0;
-      // 前回階層
-      int preDepth = 0;
-
       // ファイルからXMLを取得
       // インスタンスを生成する全てのクラスをusing化(しないとファイルが開放されない)
       using (StreamReader strmRdr = new StreamReader(tgtPath))
       using (XmlReader xmlRdr = XmlReader.Create(strmRdr, xmlSet))
       {
-        // XmlReader.Readメソッド使用パターン
+        // ノードループ
         while (xmlRdr.Read())
         {
           // ノードタイプで分岐
@@ -136,82 +126,15 @@ namespace WFA
           {
             case XmlNodeType.Element: // 開始タグ
               // タグ行数インクリメント
-              tagRow++;
+              ds.CrDepthNum++;
 
-              // 現在階層取得
-              int dep = xmlRdr.Depth;
-
-              // 前回階層との差を取得
-              int calcI = dep - preDepth;
-              // 前回より深くなった場合は「1」を設定
-              int depI = calcI <= 0 ? calcI : 1;
-
-              // 前回階層差ループ
-              for (int i = depI; i <= 0; i++)
-              {
-                // 累計パスリストに要素がない場合
-                int cnt = cmlPathList.Count;
-                if(cnt == 0)
-                {
-                  break;
-                }
-
-                // 累計パスリストの後ろ1要素を削除
-                cmlPathList.RemoveAt(cnt - 1);
-              }
-
-              // 要素名取得
-              string pathName = xmlRdr.Name;
-
-              // 累計パスリストに追加
-              cmlPathList.Add(pathName);
-
-              // 階層数分カンマを頭につけて累計パスリストに追加
-              depthElemList.Add(new string(',', dep) + pathName);
-
-              // 累計パスリストを「/」で結合
-              string fullPathStr = string.Join("/", cmlPathList);
-
-              // フルパスディクショナリ追加
-              fullPathDic.Add(tagRow, string.Format("\"{0}\"", fullPathStr));
-
-              // 現在階層を前回階層変数に引継ぎ
-              preDepth = dep;
-
-              // 属性がない場合
-              if (!xmlRdr.HasAttributes)
-              {
-                break;
-              }
-
-              // 最大属性数を更新
-              int attrCnt = xmlRdr.AttributeCount;
-              if (attrMaxDepth < attrCnt)
-              {
-                attrMaxDepth = attrCnt;
-              }
-
-              // 属性をループ
-              string attrStr = string.Empty;
-              string attrStrFmt = "\"{0}\",\"{1}\"";
-              for (int i = 0; i < attrCnt; i++)
-              {
-                // 属性へリーダを移動
-                xmlRdr.MoveToAttribute(i);
-
-                // 属性文字列設定
-                attrStr += string.Format(attrStrFmt, xmlRdr.Name, xmlRdr.Value);
-                // 次のフォーマット作成
-                attrStrFmt = ",\"{0}\",\"{1}\"";
-              }
-
-              // 属性ディクショナリ追加
-              attrDic.Add(tagRow, attrStr);
+              // 要素解析メソッド使用
+              AnlXmlElem(ds, xmlRdr);
               break;
 
             case XmlNodeType.Text: // 値
-              // 値ディクショナリ追加
-              valDic.Add(tagRow, string.Format("\"{0}\"", xmlRdr.Value));
+              // 値カラムディクショナリ追加
+              ds.ValColDic.Add(ds.CrDepthNum, string.Format("\"{0}\"", xmlRdr.Value));
               break;
 
             case XmlNodeType.Comment: // コメントタグ
@@ -223,47 +146,143 @@ namespace WFA
               continue;
           }
         }
-
-        // ヘッダ初期値
-        string headerStr = "ID,フルパス,値";
-        // 属性ヘッダ作成
-        for (int i = 1; i <= attrMaxDepth; i++)
-        {
-          // 名称+二桁階層数
-          headerStr += string.Format(",属性名{0,2},属性値{0,2}", i.ToString());
-        }
-
-        // 返り値リストにヘッダ追加
-        retList.Add(headerStr);
-        // タグ行数ループ
-        for (int i = 1; i <= tagRow; i++)
-        {
-          // 値ディクショナリに該当キーがある場合
-          string valStr = string.Empty;
-          if (valDic.ContainsKey(i))
-          {
-            // 値を設定
-            valStr = valDic[i];
-          }
-          string attrStr = string.Empty;
-          if (attrDic.ContainsKey(i))
-          {
-            attrStr = attrDic[i];
-          }
-
-          // 返り値リスト追加
-          retList.Add(string.Format("{0},{1},{2},{3}", i.ToString(), fullPathDic[i], valStr, attrStr));
-        }
       }
 
-      // 階層順要素ループ
-      foreach (string x in depthElemList)
+      // CSV変換後リスト追加メソッド使用
+      AddConvdCsvList(ds);
+    }
+    #endregion
+
+    #region 要素解析メソッド
+    /// <summary>
+    /// 要素解析メソッド
+    /// </summary>
+    /// <param name="ds">データストア</param>
+    /// <param name="xmlRdr">リーダ</param>
+    private void AnlXmlElem(DataStore ds, XmlReader xmlRdr)
+    {
+      // 現在階層取得
+      int depNum = xmlRdr.Depth;
+
+      // 前回階層との差を取得
+      int calcI = depNum - ds.PreDepthNum;
+      // 前回より深くなった場合は「1」を設定
+      int depI = calcI <= 0 ? calcI : 1;
+
+      // 前回階層差ループ
+      for (int i = depI; i <= 0; i++)
       {
-        // テキストボックスに出力
-        textBox2.AppendText(x + Environment.NewLine);
+        // 累計パスリストに要素がない場合
+        int cnt = ds.CmlPathList.Count;
+        if (cnt == 0)
+        {
+          break;
+        }
+
+        // 累計パスリストの後ろ1要素を削除
+        ds.CmlPathList.RemoveAt(cnt - 1);
       }
 
-      return retList;
+      // 要素名取得
+      string elemName = xmlRdr.Name;
+
+      // 累計パスリストに追加
+      ds.CmlPathList.Add(elemName);
+
+      // 累計パスリストを「/」で結合
+      string fullPathStr = string.Join("/", ds.CmlPathList);
+      // フルパスカラムディクショナリ追加
+      ds.FullPathColDic.Add(ds.CrDepthNum, string.Format("\"{0}\"", fullPathStr));
+
+      // 階層数分カンマを頭につけて累計パスリストに追加
+      ds.DepthElemList.Add(new string(',', depNum) + elemName);
+
+      // 現在階層を前回階層変数に引継ぎ
+      ds.PreDepthNum = depNum;
+
+      // 属性がない場合
+      if (!xmlRdr.HasAttributes)
+      {
+        return;
+      }
+
+      // 属性解析メソッド使用
+      AnlXmlAttr(ds, xmlRdr);
+    }
+    #endregion
+
+    #region 属性解析メソッド
+    /// <summary>
+    /// 属性解析メソッド
+    /// </summary>
+    /// <param name="ds">データストア</param>
+    /// <param name="xmlRdr">リーダ</param>
+    private void AnlXmlAttr(DataStore ds, XmlReader xmlRdr)
+    {
+      // 最大属性数を更新
+      int attrCnt = xmlRdr.AttributeCount;
+      if (ds.AttrMaxDepthNum < attrCnt)
+      {
+        ds.AttrMaxDepthNum = attrCnt;
+      }
+
+      // 属性をループ
+      string attrStr = string.Empty;
+      string attrStrFmt = "\"{0}\",\"{1}\"";
+      for (int i = 0; i < attrCnt; i++)
+      {
+        // 属性へリーダを移動
+        xmlRdr.MoveToAttribute(i);
+
+        // 属性文字列設定
+        attrStr += string.Format(attrStrFmt, xmlRdr.Name, xmlRdr.Value);
+        // 次のフォーマット作成
+        attrStrFmt = ",\"{0}\",\"{1}\"";
+      }
+
+      // 属性ディクショナリ追加
+      ds.AttrColDic.Add(ds.CrDepthNum, attrStr);
+    }
+    #endregion
+
+    #region CSV変換後リスト追加メソッド
+    /// <summary>
+    /// CSV変換後リスト追加メソッド
+    /// </summary>
+    /// <param name="ds">データストア</param>
+    private void AddConvdCsvList(DataStore ds)
+    {
+      // ヘッダ初期値
+      string headerStr = "No,フルパス,値";
+      // 属性ヘッダ作成
+      for (int i = 1; i <= ds.AttrMaxDepthNum; i++)
+      {
+        // 名称+二桁階層数
+        headerStr += string.Format(",属性名{0,2},属性値{0,2}", i.ToString());
+      }
+      // CSV変換後リストにヘッダ追加
+      ds.ConvdCsvList.Add(headerStr);
+
+      // タグ行数ループ
+      for (int i = 1; i <= ds.CrDepthNum; i++)
+      {
+        string valStr = string.Empty;
+        string attrStr = string.Empty;
+
+        // 値カラムディクショナリに該当キーがある場合
+        if (ds.ValColDic.ContainsKey(i))
+        {
+          // 値を設定
+          valStr = ds.ValColDic[i];
+        }
+        if (ds.AttrColDic.ContainsKey(i))
+        {
+          attrStr = ds.AttrColDic[i];
+        }
+
+        // CSV変換後リスト追加
+        ds.ConvdCsvList.Add(string.Format("{0},{1},{2},{3}", i.ToString(), ds.FullPathColDic[i], valStr, attrStr));
+      }
     }
     #endregion
   }
